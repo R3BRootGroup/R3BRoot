3847d4d3c58514b0dd590a3d3c250daf28c2e281
denis <denis>
2009-04-21 12:08:18 +0000

    r3b calorimeter geo2(barrel) geo5(ecap)
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@5315 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ cal/CaloLinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+ 
+#pragma link C++ class R3BGeoCalo+;
+#pragma link C++ class R3BGeoCaloPar;
+#pragma link C++ class R3BCalo+;
+#pragma link C++ class R3BCaloContFact;
+
+#endif
+
---
+++ cal/R3BCalo.h
@@ -0,0 +1,168 @@
+// -------------------------------------------------------------------------
+// -----                        R3BCalo header file                     -----
+// -----                  Created 26/03/09  by D.Bertini               -----
+// -------------------------------------------------------------------------
+
+/**  R3BCalo.h
+ **/
+
+
+#ifndef R3BCALO_H
+#define R3BCALO_H
+
+#include "FairDetector.h"
+
+#include "TLorentzVector.h"
+
+class TClonesArray;
+class R3BCaloPoint;
+class FairVolume;
+class TGeoRotation;
+
+
+class R3BCalo : public FairDetector
+{
+
+ public:
+
+  /** Default constructor **/
+  R3BCalo();
+
+
+  /** Standard constructor.
+   *@param name    detetcor name
+   *@param active  sensitivity flag
+   **/
+  R3BCalo(const char* name, Bool_t active);
+
+
+  /** Destructor **/
+  virtual ~R3BCalo();
+
+
+  /** Virtual method ProcessHits
+   **
+   ** Defines the action to be taken when a step is inside the
+   ** active volume. Creates a R3BCaloPoint and adds it to the
+   ** collection.
+   *@param vol  Pointer to the active volume
+   **/
+  virtual Bool_t ProcessHits(FairVolume* vol = 0);
+
+
+  /** Virtual method BeginEvent
+   **
+   ** If verbosity level is set, print hit collection at the
+   ** end of the event and resets it afterwards.
+   **/
+
+  virtual void BeginEvent();
+
+  /** Virtual method EndOfEvent
+   **
+   ** If verbosity level is set, print hit collection at the
+   ** end of the event and resets it afterwards.
+   **/
+
+  virtual void EndOfEvent();
+
+
+
+  /** Virtual method Register
+   **
+   ** Registers the hit collection in the ROOT manager.
+   **/
+  virtual void Register();
+
+
+  /** Accessor to the hit collection **/
+  virtual TClonesArray* GetCollection(Int_t iColl) const;
+
+
+  /** Virtual method Print
+   **
+   ** Screen output of hit collection.
+   **/
+  virtual void Print() const;
+
+
+  /** Virtual method Reset
+   **
+   ** Clears the hit collection
+   **/
+  virtual void Reset();
+
+
+  /** Virtual method CopyClones
+   **
+   ** Copies the hit collection with a given track index offset
+   *@param cl1     Origin
+   *@param cl2     Target
+   *@param offset  Index offset
+   **/
+  virtual void CopyClones(TClonesArray* cl1, TClonesArray* cl2,
+			  Int_t offset);
+
+
+  /** Virtaul method Construct geometry
+   **
+   ** Constructs the STS geometry
+   **/
+  virtual void ConstructGeometry();
+
+
+//  void SaveGeoParams();
+
+  private:
+
+
+    /** Track information to be stored until the track leaves the
+	active volume. **/
+    Int_t          fTrackID;           //!  track index
+    Int_t          fVolumeID;          //!  volume id
+    TLorentzVector fPosIn, fPosOut;    //!  position
+    TLorentzVector fMomIn, fMomOut;    //!  momentum
+    Double32_t     fTime;              //!  time
+    Double32_t     fLength;            //!  length
+    Double32_t     fELoss;             //!  energy loss
+
+    Int_t          fPosIndex;          //!
+    TClonesArray*  fCaloCollection;     //!  The hit collection
+    Bool_t         kGeoSaved;          //!
+    TList *flGeoPar; //!
+
+    /** Private method AddHit
+     **
+     ** Adds a CaloPoint to the HitCollection
+     **/
+    R3BCaloPoint* AddHit(Int_t trackID, Int_t detID, TVector3 posIn,
+			TVector3 pos_out, TVector3 momIn, 
+			TVector3 momOut, Double_t time, 
+			Double_t length, Double_t eLoss);
+
+
+    /** Private method ResetParameters
+     **
+     ** Resets the private members for the track parameters
+     **/
+    void ResetParameters();
+   TGeoRotation* createMatrix( Double_t phi, Double_t theta, Double_t psi);
+
+
+
+    ClassDef(R3BCalo,1);
+};
+
+
+inline void R3BCalo::ResetParameters() {
+  fTrackID = fVolumeID = 0;
+  fPosIn.SetXYZM(0.0, 0.0, 0.0, 0.0);
+  fPosOut.SetXYZM(0.0, 0.0, 0.0, 0.0);
+  fMomIn.SetXYZM(0.0, 0.0, 0.0, 0.0);
+  fMomOut.SetXYZM(0.0, 0.0, 0.0, 0.0);
+  fTime = fLength = fELoss = 0;
+  fPosIndex = 0;
+};
+
+
+#endif 
