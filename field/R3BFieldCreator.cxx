31c67ca95427441abc7e558ae690668b71ebfcb2
denis <denis>
2009-05-19 11:47:35 +0000

    corrections on field
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@5535 0381ead4-6506-0410-b988-94b70fbc4730

--- field/R3BFieldCreator.cxx
+++ field/R3BFieldCreator.cxx
@@ -2,11 +2,6 @@
 
 #include "R3BFieldConst.h"
 #include "R3BFieldMap.h"
-#include "R3BFieldMapSym2.h"
-#include "R3BFieldMapSym3.h"
-#include "R3BBsField.h"
-#include "R3BFieldMapDistorted.h"
-#include "R3BFieldMapSym1.h"
 
 #include "FairRunAna.h"
 #include "FairRuntimeDb.h"
@@ -46,11 +41,6 @@ FairField* R3BFieldCreator::createFairField()
 	Int_t fType = fFieldPar->GetType();
 	if      ( fType == 0 ) fMagneticField = new R3BFieldConst(fFieldPar);
 	else if ( fType == 1 ) fMagneticField = new R3BFieldMap(fFieldPar);
-	else if ( fType == 2 ) fMagneticField = new R3BFieldMapSym2(fFieldPar);
-	else if ( fType == 3 ) fMagneticField = new R3BFieldMapSym3(fFieldPar);
-	else if ( fType == kTypeDistorted ) fMagneticField = new R3BFieldMapDistorted(fFieldPar); 
-	else if ( fType == 5 ) fMagneticField = new R3BFieldMapSym1(fFieldPar);
-	else if ( fType == 6 ) fMagneticField = new R3BBsField(fFieldPar);
 	else cerr << "-W- FairRunAna::GetField: Unknown field type " << fType
 		<< endl;
 	cout << "New field at " << fMagneticField << ", type " << fType << endl;
