9be0d5927abd918336a1ff7bbb0b20f54100e7a2
denis <denis>
2009-06-09 15:08:16 +0000

    aladin/r3b  map added
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@5785 0381ead4-6506-0410-b988-94b70fbc4730

--- field/FieldLinkDef.h
+++ field/FieldLinkDef.h
@@ -10,7 +10,7 @@
 #pragma link C++ class R3BFieldConst+;
 #pragma link C++ class R3BFieldContFact+;
 #pragma link C++ class R3BFieldMap+;
-#pragma link C++ class R3BFieldMapData+;
+//#pragma link C++ class R3BFieldMapData+;
 #pragma link C++ class R3BFieldPar+;
 #pragma link C++ class R3BFieldCreator+;
 
