91ea579d09009ebcaac7dc97711c80d0b88d7c6b
denis <denis>
2009-07-09 10:01:19 +0000

    MC Hits activated
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@5981 0381ead4-6506-0410-b988-94b70fbc4730

--- r3bdata/gfiData/R3BGfiPoint.cxx
+++ r3bdata/gfiData/R3BGfiPoint.cxx
@@ -15,13 +15,14 @@ using std::flush;
 R3BGfiPoint::R3BGfiPoint() : FairMCPoint() {
   fX_out      = fY_out  = fZ_out  = 0.;
   fPx_out     = fPy_out = fPz_out = 0.;
+  fModule     = -1;
 }
 // -------------------------------------------------------------------------
 
 
 
 // -----   Standard constructor   ------------------------------------------
-R3BGfiPoint::R3BGfiPoint(Int_t trackID, Int_t detID, TVector3 posIn, 
+R3BGfiPoint::R3BGfiPoint(Int_t trackID, Int_t detID, Int_t plane, TVector3 posIn,
 			 TVector3 posOut, TVector3 momIn, TVector3 momOut,
 			 Double_t tof, Double_t length, Double_t eLoss) 
   : FairMCPoint(trackID, detID, posIn, momIn, tof, length, eLoss) {
@@ -31,6 +32,7 @@ R3BGfiPoint::R3BGfiPoint(Int_t trackID, Int_t detID, TVector3 posIn,
   fPx_out = momOut.Px();
   fPy_out = momOut.Py();
   fPz_out = momOut.Pz();
+  fModule = plane;
 }
 // -------------------------------------------------------------------------
 
