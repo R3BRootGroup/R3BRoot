683ac5f3e523280553a503ee5597696123ea9722
denis <denis>
2009-07-15 09:49:52 +0000

    customized Phy. Cuts
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@6023 0381ead4-6506-0410-b988-94b70fbc4730

--- tracker/R3BTra.h
+++ tracker/R3BTra.h
@@ -110,7 +110,9 @@ class R3BTra : public FairDetector
    **/
   virtual void ConstructGeometry();
   virtual void Initialize();
-
+  virtual void SetSpecialPhysicsCuts();
+  void SetEnergyCutOff( Double_t cutE ){fCutE = cutE;}
+  Double_t  GetEnergyCutOff ( ) {return fCutE;}
 
 //  void SaveGeoParams();
 
@@ -127,7 +129,7 @@ class R3BTra : public FairDetector
     Double32_t     fTime;              //!  time
     Double32_t     fLength;            //!  length
     Double32_t     fELoss;             //!  energy loss
-
+    Double32_t     fCutE;              //!  Sec. Energy Cut-Off
     Int_t          fPosIndex;          //!
     TClonesArray*  fTraCollection;     //!  The hit collection
     Bool_t         kGeoSaved;          //!
