4664a1ad0f04e49f29ce53c547f8d7537f2ff684
denis <denis>
2009-07-23 09:29:11 +0000

    Completed CAL Crystal MCPoint version
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@6120 0381ead4-6506-0410-b988-94b70fbc4730

--- r3bdata/calData/R3BCalPoint.h
+++ r3bdata/calData/R3BCalPoint.h
@@ -38,7 +38,7 @@ class R3BCalPoint : public FairMCPoint
    *@param length   Track length since creation [cm]
    *@param eLoss    Energy deposit [GeV]
    **/
-  R3BCalPoint(Int_t trackID, Int_t detID, TVector3 posIn, 
+  R3BCalPoint(Int_t trackID, Int_t detID, Int_t type, Int_t cp, TVector3 posIn,
 	      TVector3 posOut, TVector3 momIn, TVector3 momOut,
 	      Double_t tof, Double_t length, Double_t eLoss);
 
@@ -89,7 +89,8 @@ class R3BCalPoint : public FairMCPoint
 
   Double32_t fX_out,  fY_out,  fZ_out;
   Double32_t fPx_out, fPy_out, fPz_out;
-
+  Int_t fCrystalType;
+  Int_t fCrystalNb;
 
 
   ClassDef(R3BCalPoint,1)
