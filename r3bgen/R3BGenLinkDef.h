3e36aee5de5bad07839e15814e54d820385fca24
denis <denis>
2009-08-05 10:21:20 +0000

    R3B Event Generators
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@6222 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ r3bgen/R3BGenLinkDef.h
@@ -0,0 +1,17 @@
+// $Id: GenLinkDef.h,v 1.2 2006/09/15 16:53:34 friese Exp $
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class  R3BPrimaryGenerator+;
+#pragma link C++ class  R3BReadKinematics+;
+#pragma link C++ class  R3BCDGenerator+;
+#pragma link C++ class  R3BBeamInfo+;
+#pragma link C++ class  R3BBackTracking+;
+#pragma link C++ class  R3BBackTrackingStorageState+;
+
+
+#endif
