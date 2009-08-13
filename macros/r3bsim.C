037c9d6ed2e4bc333baa69f0e18a23829d4de30f
denis <denis>
2009-08-13 05:49:00 +0000

    Macros comments added
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@6262 0381ead4-6506-0410-b988-94b70fbc4730

--- macros/r3bsim.C
+++ macros/r3bsim.C
@@ -1,19 +1,31 @@
+//--------------------------------------------------------------------
+//
+// Define the R3B simulation Setup
+// Steering macros to main r3ball.C
+// Author: <D.Bertini@gsi.de>
+//
+// Last Update: 13/08/09
+// Comments:
+//         - 13/08/09 : adding flag for User Physics List
+//
+//
+//--------------------------------------------------------------------
+
 
 Int_t r3bsim(){
 
    // Load the Main Simulation macro
    gROOT->LoadMacro("r3ball.C");
 
-   //---------------------------------
-   // Define the R3B simulation Setup
-   //---------------------------------
 
-   // Number of Simulated Events
+   //- N# of Sim. Events   |    nEvents     (Int_t)
+   //-------------------------------------------------
    Int_t nEvents = 1;
 
-   // Calorimeter type
-   //  0 --> CALIFA
-   //  1 --> CrystalBall   
+   //- Calorimeter type    |    fCalVersion (Int_t)
+   //-------------------------------------------------
+   //   CALIFA:                 0
+   //   CrystalBall:            1
    Int_t fCalVersion = 0;
 
    // Target type
@@ -22,25 +34,31 @@ Int_t r3bsim(){
    TString target3="Para45";
    TString target4="LiH";
 
-   // EventDisplay        fEventDisplay
-   //
-   //       connected       kTRUE
-   //   not connected       kFALSE
+   //- EventDisplay        |    fEventDisplay (Bool_t)
+   //-------------------------------------------------
+   //   connected:              kTRUE
+   //   not connected:          kFALSE
 
    Bool_t fEventDisplay=kTRUE;
 
-   // Monte Carlo engine type
+   // Monte Carlo type     |    fMC        (TString)
+   //-------------------------------------------------
+   //   Geant3:                 "TGeant3"
+   //   Geant4:                 "TGeant4"
+   //   Fluka :                 "TFluka"
+
    TString fMC ="TGeant4";
    
-   // Event Generator Type
-   //        "box " : box generator
-   //        "r3b"  : R3B specific generator  
+   // Event Generator Type |   fGene       (TString)
+   //-------------------------------------------------
+   // Box generator:             "box"
+   // R3B spec. generator:       "r3b"
    TString fGene="box";
 
-   // Case Geant4 : loading R3B Special Physic List
-   //
-   //     VMC Standard  -->  kFALSE
-   //     R3B Special   -->  kTRUE;
+   // R3B Spec. PhysicList |     fUserPList (Bool_t)
+   // ----------------------------------------------
+   //     VMC Standard           kFALSE
+   //     R3B Special            kTRUE;
 
    Bool_t fUserPList= kTRUE;
 
