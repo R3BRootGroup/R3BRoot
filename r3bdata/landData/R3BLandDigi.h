9926381235d0b34742850719d6d4471676931506
mheil <mheil>
2010-02-16 06:57:38 +0000

    new constructor for R3BLandDigi
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@7826 0381ead4-6506-0410-b988-94b70fbc4730

--- r3bdata/landData/R3BLandDigi.h
+++ r3bdata/landData/R3BLandDigi.h
@@ -24,9 +24,11 @@ class R3BLandDigi : public TObject
 
   /** Default constructor **/
   R3BLandDigi();
-  R3BLandDigi(Int_t paddle, Double_t tdcR, Double_t tdcL,
-	      Double_t qdcR, Double_t qdcL);
-
+  R3BLandDigi(Int_t paddle, Double_t tdcL, Double_t tdcR,
+   Double_t tdc, Double_t qdcL, Double_t qdcR, Double_t qdc,
+   Double_t xx, Double_t yy, Double_t zz,
+   Double_t x0, Double_t y0, Double_t z0, Double_t T0);
+   
   /** Copy constructor **/
   R3BLandDigi(const R3BLandDigi& point) { *this = point; };
 
@@ -43,6 +45,9 @@ class R3BLandDigi : public TObject
 
   void SetTdcL(Double_t time){fTdcL = time;}
   Double_t GetTdcL(){return fTdcL;}
+  
+  void SetTdc(Double_t time){fTdc = time;}
+  Double_t GetTdc(){return fTdc;}
 
   void SetQdcR(Double_t charge){fQdcR = charge;}
   Double_t GetQdcR(){return fQdcR;}
@@ -50,9 +55,32 @@ class R3BLandDigi : public TObject
   void SetQdcL(Double_t charge){fQdcL = charge;}
   Double_t GetQdcL(){return fQdcL;}
 
+  void SetQdc(Double_t charge){fQdc = charge;}
+  Double_t GetQdc(){return fQdc;}
+
+  void SetXX(Double_t pos){fxx = pos;}
+  Double_t GetXX(){return fxx;}
+
+  void SetYY(Double_t pos){fyy = pos;}
+  Double_t GetYY(){return fyy;}
+
+  void SetZZ(Double_t pos){fzz = pos;}
+  Double_t GetZZ(){return fzz;}
+
   void SetPaddleNr(Int_t paddleNb){fPaddleNb = paddleNb;}
   Int_t GetPaddleNr(){return fPaddleNb;}
 
+  void SetX0(Double_t pos){fx0 = pos;}
+  Double_t GetX0(){return fx0;}
+
+  void SetY0(Double_t pos){fy0 = pos;}
+  Double_t GetY0(){return fy0;}
+
+  void SetZ0(Double_t pos){fz0 = pos;}
+  Double_t GetZ0(){return fz0;}
+
+  void SetT0(Double_t time){fT0 = time;}
+  Double_t GetT0(){return fT0;}
 
 
  protected:
@@ -60,8 +88,17 @@ class R3BLandDigi : public TObject
   Int_t fPaddleNb;
   Double32_t fTdcL;
   Double32_t fTdcR;
+  Double32_t fTdc;
   Double32_t fQdcL;
   Double32_t fQdcR;
+  Double32_t fQdc;
+  Double32_t fxx;
+  Double32_t fyy;
+  Double32_t fzz;
+  Double32_t fx0;
+  Double32_t fy0;
+  Double32_t fz0;
+  Double32_t fT0;
 
   ClassDef(R3BLandDigi,1)
 
