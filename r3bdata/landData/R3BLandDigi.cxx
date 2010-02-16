aa189403b826d0cc954fc232f9ca9ad03b8c8c21
mheil <mheil>
2010-02-16 06:57:29 +0000

    new constructor for R3BLandDigi
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@7825 0381ead4-6506-0410-b988-94b70fbc4730

--- r3bdata/landData/R3BLandDigi.cxx
+++ r3bdata/landData/R3BLandDigi.cxx
@@ -13,15 +13,25 @@ using std::flush;
 
 // -----   Default constructor   -------------------------------------------
 R3BLandDigi::R3BLandDigi() {
-  fTdcL     = fTdcR    =  fQdcR  = fQdcL = 0.;
+  fTdcL = fTdcR = fQdcR = fQdcL = fTdc = fQdc = fxx = fyy = fzz = fx0 = fy0 = fz0 = fT0 = 0.;
   fPaddleNb = -1;
 }
-R3BLandDigi::R3BLandDigi(Int_t paddle, Double_t tdcR, Double_t tdcL,
-			               Double_t qdcR, Double_t qdcL){
-    fTdcL    = tdcR;
-    fTdcR    = tdcL;
-    fQdcL    = qdcR;
-    fQdcR    = qdcL;
+R3BLandDigi::R3BLandDigi(Int_t paddle, Double_t tdcL, Double_t tdcR,
+   Double_t tdc, Double_t qdcL, Double_t qdcR, Double_t qdc,
+   Double_t xx,Double_t yy, Double_t zz, Double_t x0,Double_t y0, Double_t z0,Double_t T0 ){
+    fTdcL    = tdcL;
+    fTdcR    = tdcR;
+    fTdc    = tdc;    
+    fQdcL    = qdcL;
+    fQdcR    = qdcR;
+    fQdc    = qdc;
+    fxx    = xx;
+    fyy    = yy;
+    fzz    = zz;
+    fx0    = x0;
+    fy0    = y0;
+    fz0    = z0;
+    fT0    = T0;
 
     fPaddleNb = paddle;
 }
