b144b1fb293b680b9027b02c8c33b6e9a6a9a363
hapol <hapol>
2011-01-03 10:29:05 +0000

    Improving the hit documentation.
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@10576 0381ead4-6506-0410-b988-94b70fbc4730

--- r3bdata/caloData/R3BCaloCrystalHit.h
+++ r3bdata/caloData/R3BCaloCrystalHit.h
@@ -3,31 +3,23 @@
 // -----               Created 08/07/10  by H. Alvarez Pol             -----
 // -------------------------------------------------------------------------
 
-
 /**  R3BCaloCrystalHit.h
  **  A calorimeter crystal hit is the representation of the 
  **  information obtained from each crystal in the calorimeter.
- **
  **/
-
 
 #ifndef R3BCALOCRYSTALHIT_H
 #define R3BCALOCRYSTALHIT_H
 
-
 #include "TObject.h"
-
 #include "FairMultiLinkedData.h"
-
 
 class R3BCaloCrystalHit : public FairMultiLinkedData 
 {
-
- public:
+public:
 
   /** Default constructor **/
   R3BCaloCrystalHit();
-
 
   /** Constructor with arguments
    *@param fCrystalId		Crystal unique identifier
@@ -39,14 +31,11 @@ class R3BCaloCrystalHit : public FairMultiLinkedData
 	R3BCaloCrystalHit(Int_t type, Int_t copy, Int_t ident,
 					  Double_t energy, Double_t time);
 
-
   /** Copy constructor **/
   R3BCaloCrystalHit(const R3BCaloCrystalHit& hit) { *this = hit; };
 
-
   /** Destructor **/
   virtual ~R3BCaloCrystalHit();
-
 
   /** Accessors **/
   Double_t GetCrystalType()   const { return fCrystalType; }
@@ -55,7 +44,6 @@ class R3BCaloCrystalHit : public FairMultiLinkedData
   Double_t GetEnergy()   const { return fEnergy; }
   Double_t GetTime()  const { return fTime; }
 	
-
   /** Modifiers **/
   void SetCrystalType(Int_t type){fCrystalType = type;}
   void SetCrystalCopy(Int_t copy){fCrystalCopy = copy;}
@@ -64,20 +52,16 @@ class R3BCaloCrystalHit : public FairMultiLinkedData
   void AddMoreEnergy(Double32_t moreEnergy){fEnergy += moreEnergy;}
   void SetTime(Double32_t time){fTime = time;}
 
-
   /** Output to screen **/
   virtual void Print(const Option_t* opt) const;
 
+protected:
 
-
- protected:
-
-  Double32_t fEnergy;
-  Double32_t fTime;
-  Int_t fCrystalType;
-  Int_t fCrystalCopy;
-  Int_t fCrystalId;
-
+  Double32_t fEnergy;  //total energy in the crystal
+  Double32_t fTime;    //time of the interaction
+  Int_t fCrystalType;  //crystal type
+  Int_t fCrystalCopy;  //crystal copy
+  Int_t fCrystalId;    //crystal unique identifier
 
   ClassDef(R3BCaloCrystalHit,1)
 
