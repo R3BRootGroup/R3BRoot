745bce593283aa86a0ef4208d0cea6f4c953e2db
mheil <mheil>
2011-01-18 07:47:25 +0000

    Neutron Tracker is now using total energy to determine number of neutrons and has a routine to detect elastic scattered neutrons
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@10652 0381ead4-6506-0410-b988-94b70fbc4730

--- land/R3BNeutronTracker.h
+++ land/R3BNeutronTracker.h
@@ -7,6 +7,7 @@
 #include <string>
 #include "R3BLandDigiPar.h"
 #include "R3BLandDigi.h"
+#include "TLorentzVector.h"
 
 class TClonesArray;
 class TObjectArray;
@@ -29,6 +30,10 @@ struct NEUT1_HIT
 struct NEUT2_HIT
 {
   Double_t x,y,z,t,px,py,pz,p;
+};
+struct CLUSTER
+{
+  Double_t xStart,yStart,zStart,xEnd,yEnd,zEnd,tStart,tStop,e,size;
 };
 
 class R3BNeutronTracker : public FairTask
@@ -54,9 +59,16 @@ class R3BNeutronTracker : public FairTask
   virtual void Finish();
   virtual void Reset();
 
+  void UseBeam(Double_t _beam_energy,Double_t _beam_beta);
 
-  R3BNeutronTrack* AddHit(Int_t paddleNr, Double_t tdcR, Double_t tdcL,
-        Double_t qdcR, Double_t qdcL );
+  private:
+  
+  TLorentzVector fPosIn, fPosOut;    //!  position
+  TLorentzVector fMomIn, fMomOut;    //!  momentum
+  Double32_t     fTime;              //!  time
+
+  R3BNeutronTrack* AddHit(TVector3 pos_in, TVector3 pos_out,
+                          TVector3 momOut, Double_t time);
 
 
   protected:
@@ -76,6 +88,10 @@ class R3BNeutronTracker : public FairTask
   // Add here control hist for tracker
   TH1F *hNeutmult1;
   TH1F *hNeutmult2;
+  TH1F *hNeutmult3;
+  TH1F *hNeutmult4;
+  TH1F *hNeutmult5;
+  TH1F *hNeutmult6;
   TH1F *hMinv;
   TH1F *hMinv0;
   TH1F *hDeltaPx1;
@@ -84,20 +100,30 @@ class R3BNeutronTracker : public FairTask
   TH1F *hDeltaPx2;
   TH1F *hDeltaPy2;
   TH1F *hDeltaPz2;
-
+  TH1F *hClusterSize;
+  TH1F *hClusterEnergy;
+  TH1F *hClusters;
+  TH1F *hHits;
+  TH2F *hClusterNo_vs_Size;
   Int_t npaddles;
   Int_t nplanes;
 
+  Double_t beamEnergy;
+  Double_t beamBeta;
   Double_t plength; // half length of paddle
   Double_t att; // light attenuation factor [1/cm]
   Double_t amu; //atomic mass unit in MeV/c**2
+  Double_t mNeutron; //mass of neutron in amu
   Double_t c;
   Double_t cMedia; // speed of light in material in cm/ns
   Double_t calFactor; //calibration factor energy of LAND paddles
+  Int_t eventNo;
   PM_HIT *PM_hit;
   PRIM_PART *PRIM_part;
+  PRIM_PART *PRIM_frag;
   NEUT1_HIT *NEUT1_hit;
   NEUT2_HIT *NEUT2_hit;
+  CLUSTER *Cluster;
   
   private:
   virtual void SetParContainers();
