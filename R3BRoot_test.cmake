d91e1b18099eeb2bd338843f1abe12874f69fbc2
uhlig <uhlig>
2011-02-03 14:29:42 +0000

    Allow to define a revision number for the Continuous tests. Now it is possible to test a single revision. Before there was the possibility that two commits to SVN which happens shortly after each other have been updated at once. There have been also changes in the post-commit hook of the svn repository and the Continuous test script.
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@10762 0381ead4-6506-0410-b988-94b70fbc4730

--- R3BRoot_test.cmake
+++ R3BRoot_test.cmake
@@ -6,11 +6,17 @@ SET (CTEST_CMAKE_GENERATOR "Unix Makefiles")
 SET (CTEST_PROJECT_NAME "R3BROOT")
 
 SET (CTEST_UPDATE_COMMAND "svn")
-SET (CTEST_UPDATE_CHECKOUT  " ${CTEST_UPDATE_COMMAND} update ")
+If($ENV{ctest_model} MATCHES Continuous)
+  Set(CTEST_SVN_UPDATE_OPTIONS "$ENV{REVISION}")
+EndIf($ENV{ctest_model} MATCHES Continuous)
+
 SET (BUILD_COMMAND "make")
 SET (CTEST_BUILD_COMMAND "${BUILD_COMMAND} -j$ENV{number_of_processors}")
 
 if($ENV{ctest_model} MATCHES Nightly)
+
+Set (CTEST_CONFIGURE_COMMAND " \"${CMAKE_EXECUTABLE_NAME}\" \"-G${CTEST_CMAKE_GENERATOR}\" \"${CTEST_SOURCE_DIRECTORY}\" \"-DCMAKE_BUILD_TYPE=NIGHTLY\" ")
+
   # get the information about conflicting or localy modified files
   # from svn, extract the relavant information about the file name
   # and put the result in the output variable
@@ -27,14 +33,18 @@ if($ENV{ctest_model} MATCHES Nightly)
   STRING(REGEX REPLACE "\n" ";" _result "${FILELIST}")
 
   FOREACH(_file ${_result})
-    SET (CTEST_NOTES_FILES ${CTEST_NOTES_FILES} "${CTEST_SOURCE_DIRECTORY}/${_file}")
+    STRING(STRIP "${_file}" _file1)
+    SET (CTEST_NOTES_FILES ${CTEST_NOTES_FILES} "${CTEST_SOURCE_DIRECTORY}/${_file1}")
   ENDFOREACH(_file ${_result})
+
+  CTEST_EMPTY_BINARY_DIRECTORY(${CTEST_BINARY_DIRECTORY})
+
 endif($ENV{ctest_model} MATCHES Nightly)
 
-
-if($ENV{ctest_model} MATCHES Experimental OR $ENV{ctest_model} MATCHES Nightly)
-  CTEST_EMPTY_BINARY_DIRECTORY(${CTEST_BINARY_DIRECTORY})
-endif($ENV{ctest_model} MATCHES Experimental OR $ENV{ctest_model} MATCHES Nightly)
+configure_file(${CTEST_SOURCE_DIRECTORY}/CTestCustom.cmake
+               ${CTEST_BINARY_DIRECTORY}/CTestCustom.cmake
+              )
+ctest_read_custom_files("${CTEST_BINARY_DIRECTORY}")
 
 CTEST_START ($ENV{ctest_model})
 CTEST_UPDATE (SOURCE "${CTEST_SOURCE_DIRECTORY}")
