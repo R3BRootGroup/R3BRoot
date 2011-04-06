a803a61f7a8f87ff0d38e03cb6efa4385001a984
mcherciu <mcherciu>
2011-04-06 12:46:22 +0000

    example macro of how to use reading class LMD->ROOT
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@11411 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ macros/r3b/land/r3b_sim_and_rclass.C
@@ -0,0 +1,164 @@
+//--------------------------------------------------------------------
+//
+// Define the R3B simulation Setup
+// Steering macros to main r3ball.C
+// Author: <D.Bertini@gsi.de>
+//Modified by M.I.Cherciu
+// Last Update: 6/04/11
+// Comments:
+//         - 06/04/11 : reading class R3BLandData.cxx implementation for working with data from LMD->ROOT files
+//         - 17/08/09 : Adding detector list
+//         - 13/08/09 : adding flag for User Physics List
+//
+//
+//--------------------------------------------------------------------
+
+
+Int_t r3b_sim_and_rclass(){
+
+   // Load the Main Simulation macro
+   gROOT->LoadMacro("r3ball.C");
+
+   //-------------------------------------------------
+   // Monte Carlo type     |    fMC        (TString)
+   //-------------------------------------------------
+   //   Geant3:                 "TGeant3"
+   //   Geant4:                 "TGeant4"
+   //   Fluka :                 "TFluka"
+
+   TString fMC ="TGeant3";
+
+   //-------------------------------------------------
+   // Primaries generation
+   // Event Generator Type |   fGene       (TString)
+   //-------------------------------------------------
+   // Box generator:             "box"
+   // Ascii generator:           "ascii"
+   // R3B spec. generator:       "r3b"
+
+   TString fGene="box";
+
+   //-------------------------------------------------
+   // Secondaries  generation (G4 only)
+   // R3B Spec. PhysicList |     fUserPList (Bool_t)
+   // ----------------------------------------------
+   //     VMC Standard           kFALSE
+   //     R3B Special            kTRUE;
+
+   Bool_t fUserPList= kTRUE;
+
+   // Target type
+   TString target1="LeadTarget";
+   TString target2="Para";
+   TString target3="Para45";
+   TString target4="LiH";
+
+   //-------------------------------------------------
+   //- Geometrical Setup Definition
+   //-  Non Sensitiv        |    fDetName (String)
+   //-------------------------------------------------
+   //   Target:                  TARGET
+   //   Magnet:                  ALADIN
+   //
+   //-------------------------------------------------
+   //-  Sensitiv            |    fDetName
+   //-------------------------------------------------
+   //   Calorimeter:             CALIFA
+   //                            CRYSTALBALL
+   //
+   //   Tof:                     TOF
+   //                            MTOF
+   //
+   //   Tracker:                 DCH
+   //                            TRACKER
+   //                            GFI
+   //
+   //   Neutron Detector
+   //   Plastic                  LAND
+   //   RPC                      
+   //                            RPCFLAND
+   //                            RPCMLAND
+
+    TObjString det1("TARGET");
+    TObjString det2("ALADIN");
+    TObjString det3("CALIFA");
+    TObjString det4("CRYSTALBALL");
+    TObjString det5("TOF");
+    TObjString det6("MTOF");
+    TObjString det7("DCH");
+    TObjString det8("TRACKER");
+    TObjString det9("GFI");
+    TObjString det10("LAND");
+    TObjString det11("RPCMLAND");
+    TObjString det12("RPCFLAND");
+    TObjString det13("SCINTNEULAND");
+
+    TObjArray fDetList;
+//    fDetList.Add(&det1);
+    fDetList.Add(&det2);
+//    fDetList.Add(&det4);
+//    fDetList.Add(&det5);
+//    fDetList.Add(&det6);
+//    fDetList.Add(&det7);
+//    fDetList.Add(&det8);
+//    fDetList.Add(&det9);
+    fDetList.Add(&det10);
+//    fDetList.Add(&det11);
+
+
+   //-------------------------------------------------
+   //- N# of Sim. Events   |    nEvents     (Int_t)
+   //-------------------------------------------------
+
+   Int_t nEvents = 1;
+
+   //-------------------------------------------------
+   //- EventDisplay        |    fEventDisplay (Bool_t)
+   //-------------------------------------------------
+   //   connected:              kTRUE
+   //   not connected:          kFALSE
+
+   Bool_t fEventDisplay=kTRUE;
+
+    // Magnet Field definition
+   Bool_t fR3BMagnet = kTRUE;
+
+   // Main Sim function call
+   r3ball(  nEvents,
+            fDetList,
+            target1,
+	    fEventDisplay,
+	    fMC,
+	    fGene,
+	    fUserPList,
+            fR3BMagnet
+          );
+//------------------- Read Data from LMD-ROOT file -------------------------------------
+   R3BLandData* LandData = new R3BLandData("s318_172.root", "h309"); // file input and tree name
+   int entries = LandData->GetEntries();                             // entries number
+   TLeaf* leaf1;                                                     // define one leaf...
+   TLeaf* leaf2;                                                     // define another leaf
+   cout<<"Entries number in TTree = "<<entries<<endl;
+
+   for (int i=0; i < entries; i++)
+   {  
+      leaf1 = LandData->GetLeaf("Nhe",i);                                 // Accessing TLeaf informations
+      leaf2 = LandData->GetLeaf("Nht",i);                              // Accessing TLeaf informations
+   //   LandData->FillHisto1D(leaf1);                                  // Plotting 1D histogram ... automatical bin width
+   //   LandData->FillHisto1D(leaf1, bin, binmin, binmax);             // Plotting 1D histogram ... specified bin width
+      LandData->FillHisto2D(leaf1, leaf2);                           // Plotting 2D histogram ... automatical bin width
+   //   LandData->FillHisto2D(leaf1, leaf2, bin, binmin , binmax, bin, binmin, binmax); // Plotting 2D histogram ... specified bin width
+   //####### for diferent analysis ###############
+/*
+   Int_t len = leaf1->GetLen();
+   for(Int_t j=0; j<len ; j++)
+      {
+       leaf1->GetValue(j);                                              // TLeaf Value for different analysis
+      }
+*/
+   }
+
+    //   LandData->Draw1D();                                            // Draw 1D histograms
+       LandData->Draw2D();                                            // Draw 2D histograms
+}
+
