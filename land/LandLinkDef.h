43a608d7dbdc87b8b3e2bf39a4ddf58e9034ee55
mcherciu <mcherciu>
2011-04-06 16:00:50 +0000

    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@11418 0381ead4-6506-0410-b988-94b70fbc4730

--- land/LandLinkDef.h
+++ land/LandLinkDef.h
@@ -12,6 +12,7 @@
 #pragma link C++ class R3BLandDigitizer+;
 #pragma link C++ class R3BNeuLandDigitizer+;
 #pragma link C++ class R3BLandDigiPar+;
+#pragma link C++ class R3BLandData+;
 #pragma link C++ class R3BNeutronTracker+;
 #pragma link C++ class R3BConstantFraction+;
 #pragma link C++ class R3BLandDigitizer_CFD+;
