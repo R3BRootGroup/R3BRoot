cf8927b7b82d0dd3ed91202846fd81db19ab3f7d
mcherciu <mcherciu>
2011-05-10 12:24:14 +0000

    example macro of how to use reading class LMD->ROOT
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@11848 0381ead4-6506-0410-b988-94b70fbc4730

--- macros/r3b/land/r3b_sim_and_rclass.C
+++ macros/r3b/land/r3b_sim_and_rclass.C
@@ -133,22 +133,49 @@ Int_t r3b_sim_and_rclass(){
 	    fUserPList,
             fR3BMagnet
           );
-//------------------- Read Data from LMD-ROOT file -------------------------------------
-   R3BLandData* LandData = new R3BLandData("s318_172.root", "h309"); // file input and tree name
-   int entries = LandData->GetEntries();                             // entries number
-   TLeaf* leaf1;                                                     // define one leaf...
-   TLeaf* leaf2;                                                     // define another leaf
-   cout<<"Entries number in TTree = "<<entries<<endl;
 
-   for (int i=0; i < entries; i++)
-   {  
-      leaf1 = LandData->GetLeaf("Nhe",i);                                 // Accessing TLeaf informations
-      leaf2 = LandData->GetLeaf("Nht",i);                              // Accessing TLeaf informations
-   //   LandData->FillHisto1D(leaf1);                                  // Plotting 1D histogram ... automatical bin width
-   //   LandData->FillHisto1D(leaf1, bin, binmin, binmax);             // Plotting 1D histogram ... specified bin width
-      LandData->FillHisto2D(leaf1, leaf2);                           // Plotting 2D histogram ... automatical bin width
-   //   LandData->FillHisto2D(leaf1, leaf2, bin, binmin , binmax, bin, binmin, binmax); // Plotting 2D histogram ... specified bin width
-   //####### for diferent analysis ###############
+//------------------- Read Data from LMD-ROOT file -------------------------------------
+// the ROOT files (simulated and experimental) are the same but, for testing purpose, show how to use two quantities for comparison
+   R3BLandData* LandData1 = new R3BLandData("s318_172.root", "h309"); // file input and tree name
+   R3BLandData* LandData2 = new R3BLandData("s318_172.root", "h309"); // file input and tree name
+// Define diferent options for TCanvas
+   TCanvas* c = new TCanvas("Compare quantities from ROOT files","ROOT Canvas");
+   c->Divide(2,1);                                                                 // Divide a canvas in two ...or more
+   int entries1 = LandData1->GetEntries();                                         // entries number of first TTree
+   int entries2 = LandData2->GetEntries();                                         // entries number of first TTree
+   TLeaf* leaf1;
+//   TLeaf* leaf11;                                                                  // define one leaf...
+   TLeaf* leaf2;
+//   TLeaf* leaf22;                                                                  // define another leaf
+   cout<<"Entries number in  first TTree = "<<entries1<<endl;
+   cout<<"Entries number in second TTree = "<<entries2<<endl;
+//  Reprezentation of 1D histogram from ROOT files
+   for (int i=0; i < entries1; i++)
+      {
+         leaf1 = LandData1->GetLeaf("Nte",i);                                      // Accessing TLeaf informations
+//         leaf11 = LandData1->GetLeaf("Nhe",i);                                   // Accessing TLeaf informations
+       LandData1->FillHisto1D(leaf1);                                              // Fill 1D histogram ... automatical bin width
+//         LandData1->FillHisto2D(leaf1,leaf11);                                   // Fill 2D histogram ... automatical bin width
+      }
+      c->cd(1);
+      LandData1->Draw1D();                                                         // Plotting 1D histogram ... automatical bin width
+//      LandData1->Draw2D();                                                         // Plotting 2D histogram ... automatical bin width
+
+   for (int i=0; i < entries2; i++)
+      {
+         leaf2 = LandData2->GetLeaf("Nhe",i);                                      // Accessing TLeaf informations
+//         leaf22 = LandData2->GetLeaf("Nte",i);                                     // Accessing TLeaf informations
+         LandData2->FillHisto1D(leaf2);                                            // Fill 1D histogram ... automatical bin width
+//         LandData2->FillHisto2D(leaf2,leaf22);                                     // Fill 2D histogram ... automatical bin width
+      }
+      c->cd(2);
+      LandData2->Draw1D();                                                         // Plotting 1D histogram ... automatical bin width
+//      LandData2->Draw2D();                                                        // Plotting 2D histogram ... automatical bin width
+      // ... or you can plot on the same histogram by comment the precedent two line and comment out the next line
+//      LandData2->Draw1Dsame();                                                        // Plotting 1D histogram ... automatical bin width
+// ... The same things for 2D histograming
+
+   //####### for diferent TLeaf analysis ###############
 /*
    Int_t len = leaf1->GetLen();
    for(Int_t j=0; j<len ; j++)
@@ -156,9 +183,6 @@ Int_t r3b_sim_and_rclass(){
        leaf1->GetValue(j);                                              // TLeaf Value for different analysis
       }
 */
-   }
-
-    //   LandData->Draw1D();                                            // Draw 1D histograms
-       LandData->Draw2D();                                            // Draw 2D histograms
+    
 }
 
