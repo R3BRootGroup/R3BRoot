2c7be124a4b1ea9e40d51f855dbf536c54bd10a3
jmargan <jmargan>
2011-07-07 11:45:25 +0000

    file for digitizer for proton tof wall
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@12600 0381ead4-6506-0410-b988-94b70fbc4730

---
+++ r3bdata/tofData/R3BTofDigi.cxx
@@ -0,0 +1,40 @@
+// -------------------------------------------------------------------------
+// -----                      R3BTofPoint source file                  -----
+// -------------------------------------------------------------------------
+
+#include "R3BTofDigi.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::flush;
+
+
+// -----   Default constructor   -------------------------------------------
+R3BTofDigi::R3BTofDigi() {
+   
+   Tfmul=0;
+   Tfx_p1=0.;
+   Tfy_p1=0.;
+   Tft_p1=0.;
+}
+
+R3BTofDigi::R3BTofDigi(Int_t tfmul,Double_t tfwx_p1,Double_t tfwy_p1,Double_t tfwt_p1) {
+   
+   Tfmul = tfmul;
+   Tfx_p1 = tfwx_p1;
+   Tfy_p1 = tfwy_p1;
+   Tft_p1 = tfwt_p1;
+}
+
+// -----   Destructor   ----------------------------------------------------
+R3BTofDigi::~R3BTofDigi() { }
+
+// -----   Public method Print   -------------------------------------------
+void R3BTofDigi::Print(const Option_t* opt) const {
+
+}
+// -------------------------------------------------------------------------
+
+ClassImp(R3BTofDigi)
