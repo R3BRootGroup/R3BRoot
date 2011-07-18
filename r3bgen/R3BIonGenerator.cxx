55eae828f1d189eade62ab46a629a018a7425a72
labiche <labiche>
2011-07-18 08:12:35 +0000

    reimplement ion generators
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@12721 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 6ad7046ef2627c2de334a4dd48a458aec965f941

---
+++ r3bgen/R3BIonGenerator.cxx
@@ -0,0 +1,195 @@
+// -------------------------------------------------------------------------
+// -----            Based on FairIonGenerator source file              -----
+// -----            Created 18/03/11  by M. Labiche                    -----
+// -------------------------------------------------------------------------
+#include "R3BIonGenerator.h"
+
+#include "FairPrimaryGenerator.h"
+
+#include "FairIon.h"
+#include "FairRunSim.h"
+
+#include "TDatabasePDG.h"
+#include "TParticlePDG.h"
+#include "TObjArray.h"
+                    
+#include "TRandom.h"
+#include "TMath.h"
+  
+#include "FairRunSim.h"
+#include "FairIon.h"
+#include <iostream>
+#include "TParticle.h"
+using std::cout;
+using std::endl;
+
+// -----   Initialsisation of static variables   --------------------------
+Int_t R3BIonGenerator::fgNIon = 0;
+// ------------------------------------------------------------------------
+
+
+
+// -----   Default constructor   ------------------------------------------
+R3BIonGenerator::R3BIonGenerator() {
+//  cout << "-W- R3BIonGenerator: "
+//      << " Please do not use the default constructor! " << endl;
+}
+// ------------------------------------------------------------------------
+
+R3BIonGenerator::R3BIonGenerator(const Char_t* ionName, Int_t mult,
+				 Double_t px, Double_t py, Double_t pz)
+ :fMult(0),          
+  fPx(0), fPy(0), fPz(0),
+  fR(0), fz(0), fOffset(0),
+  fIon(0),  fQ(0), fBeamSpotIsSet(0)		
+
+ {
+
+  FairRunSim *fRun=FairRunSim::Instance();
+  TObjArray *UserIons=fRun->GetUserDefIons();
+  TObjArray *UserParticles=fRun->GetUserDefParticles();
+  FairParticle *part=0;
+  fIon =(FairIon *) UserIons->FindObject(ionName);
+  if(fIon){
+     fgNIon++;
+     fMult = mult;
+     fPx   = Double_t(fIon->GetA()) * px;
+     fPy   = Double_t(fIon->GetA()) * py;
+     fPz   = Double_t(fIon->GetA()) * pz;
+     //fVx   = vx; 
+     //fVy   = vy; 
+     //fVz   = vz;
+       //}
+
+  }else{
+     part= (FairParticle *)UserParticles->FindObject(ionName);
+     if(part){
+        fgNIon++;
+        TParticle *particle=part->GetParticle();
+        fMult = mult;
+        fPx   = Double_t(particle->GetMass()/0.92827231) * px;
+        fPy   = Double_t(particle->GetMass()/0.92827231) * py;
+        fPz   = Double_t(particle->GetMass()/0.92827231) * pz;
+	//fVx   = vx;
+	//fVy   = vy;
+	//fVz   = vz;
+     }
+  }
+  if(fIon==0 && part==0 ){
+     cout << "-E- R3BIonGenerator: Ion or Particle is not defined !" << endl;
+     Fatal("R3BIonGenerator", "No FairRun instantised!");      
+  }
+    
+}
+// ------------------------------------------------------------------------
+
+
+
+// -----   Default constructor   ------------------------------------------
+R3BIonGenerator::R3BIonGenerator(Int_t z, Int_t a, Int_t q, Int_t mult,
+				 Double_t px, Double_t py, Double_t pz)
+ :fMult(0),          
+  fPx(0), fPy(0), fPz(0),
+  fR(0), fz(0), fOffset(0),
+  fIon(0),  fQ(0), fBeamSpotIsSet(0)
+
+ {
+  fgNIon++;
+  fMult = mult;
+  fPx   = Double_t(a) * px;
+  fPy   = Double_t(a) * py;
+  fPz   = Double_t(a) * pz;
+  //fVx   = vx; 
+  //fVy   = vy; 
+  //fVz   = vz; 
+  char buffer[20];
+  sprintf(buffer, "FairIon%d", fgNIon);
+  fIon= new FairIon(buffer, z, a, q);
+  FairRunSim* run = FairRunSim::Instance();
+  if ( ! run ) {
+    cout << "-E- FairIonGenerator: No FairRun instantised!" << endl;
+    Fatal("FairIonGenerator", "No FairRun instantised!");
+  }
+  run->AddNewIon(fIon);
+}
+//_________________________________________________________________________
+
+
+
+// -----   Destructor   ---------------------------------------------------
+R3BIonGenerator::~R3BIonGenerator() {
+ // if (fIon) delete fIon;
+}
+//_________________________________________________________________________
+
+
+
+// -----   Public method SetExcitationEnergy   ----------------------------
+void R3BIonGenerator::SetExcitationEnergy(Double_t eExc) {
+  fIon->SetExcEnergy(eExc);
+}
+//_________________________________________________________________________
+
+
+
+// -----   Public method SetMass   ----------------------------------------
+void R3BIonGenerator::SetMass(Double_t mass) {
+  fIon->SetMass(mass);
+}
+//_________________________________________________________________________
+
+
+
+// -----   Public method ReadEvent   --------------------------------------
+Bool_t R3BIonGenerator::ReadEvent(FairPrimaryGenerator* primGen) {
+
+  Double_t Phi, SpotR;
+
+ // if ( ! fIon ) {
+ //   cout << "-W- FairIonGenerator: No ion defined! " << endl;
+ //   return kFALSE;
+ // }
+
+  TParticlePDG* thisPart = 
+    TDatabasePDG::Instance()->GetParticle(fIon->GetName());
+  if ( ! thisPart ) {
+    cout << "-W- FairIonGenerator: Ion " << fIon->GetName()
+	 << " not found in database!" << endl;
+    return kFALSE;
+  }
+
+  int pdgType = thisPart->PdgCode();
+
+  //cout << "fR=" << fR << " fz=" << fz <<endl;
+
+  Phi= gRandom->Uniform(0,360)*TMath::DegToRad();
+  SpotR=gRandom->Uniform(0,fR);
+
+  if(fBeamSpotIsSet) {
+    fVx   = SpotR*cos(Phi); //gRandom->Uniform(-fx,fx);
+    fVy   = fOffset + SpotR*sin(Phi); //gRandom->Uniform(-fy,fy);
+    fVz   = fz;
+  }else
+    {
+      fVx=0.0;
+      fVy=0.0;
+      fVz=0.0;
+    }
+
+  cout << "-I- FairIonGenerator: Generating " << fMult << " ions of type "
+       << fIon->GetName() << " (PDG code " << pdgType << ")" << endl;
+  cout << "    Momentum (" << fPx << ", " << fPy << ", " << fPz 
+       << ") Gev from vertex (" << fVx << ", " << fVy
+       << ", " << fVz << ") cm" << endl;
+
+  for(Int_t i=0; i<fMult; i++)
+    primGen->AddTrack(pdgType, fPx, fPy, fPz, fVx, fVy, fVz);
+  
+  return kTRUE;
+
+}
+
+//_____________________________________________________________________________
+
+
+ClassImp(R3BIonGenerator)
---
+++ r3bgen/R3BIonGenerator.h
@@ -0,0 +1,97 @@
+// -------------------------------------------------------------------------
+// -----                   R3BIonGenerator header file                 -----
+// -----                    Based on FairIonGenerator                  -----
+// -----                 Created 18/03/11  by M. Labiche               -----
+// -------------------------------------------------------------------------
+
+/** Include beam spot size **/
+
+#ifndef R3BIONGENERATOR_H
+#define R3BIONGENERATOR_H
+
+
+#include "FairGenerator.h"
+#include "FairIon.h"
+
+#include <iostream>
+
+class FairPrimaryGenerator;
+
+class R3BIonGenerator : public FairGenerator
+{
+
+ public:
+
+  /** Default constructor **/
+  R3BIonGenerator();
+
+
+  /** Constructor with ion name    
+    ** For the generation of ions with pre-defined FairIon 
+   ** By default, the  excitation energy is zero. This can be changed with the
+   ** respective modifiers.
+   **@param ionName  Ion name
+   **@param mult      Number of ions per event
+   **@param px,py,pz  Momentum components [GeV] per nucleon!
+   **@param vx,vy,vz  Vertex coordinates [cm]
+   **/
+  R3BIonGenerator(const Char_t* ionName, Int_t mult, Double_t px, 
+		  Double_t py, Double_t pz);
+
+
+  /** Default constructor 
+   ** For the generation of ions with atomic number z and mass number a.
+   ** By default, the mass equals a times the proton mass and the 
+   ** excitation energy is zero. This can be changed with the
+   ** respective modifiers.
+   **@param z         Atomic number
+   **@param a         Atomic mass
+   **@param q         Electric charge [e]
+   **@param mult      Number of ions per event
+   **@param px,py,pz  Momentum components [GeV] per nucleon!
+   **@param vx,vy,vz  Vertex coordinates [cm]
+   **/
+  R3BIonGenerator(Int_t z, Int_t a, Int_t q, Int_t mult, Double_t px, 
+		  Double_t py, Double_t pz);
+
+
+  /** Destructor **/
+  virtual ~R3BIonGenerator();
+
+
+  /** Modifiers **/
+  void SetCharge(Int_t charge){fQ=charge;}
+  void SetExcitationEnergy(Double_t eExc);
+  void SetMass(Double_t mass);
+
+  void SetSpotRadius   (Double32_t r=0, Double32_t z=0, Double32_t off=0) {
+    fR=r; fz=z; fOffset=off; fBeamSpotIsSet=kTRUE;}
+
+
+  /** Method ReadEvent 
+   ** Generates <mult> of the specified ions and hands hem to the
+   ** FairPrimaryGenerator. 
+   **/
+  virtual Bool_t ReadEvent(FairPrimaryGenerator* primGen);
+
+
+private:
+
+  static Int_t fgNIon;      //! Number of the instance of this class
+  Int_t    fMult;           // Multiplicity per event
+  Double_t fPx, fPy, fPz;   // Momentum components [GeV] per nucleon
+  Double32_t fR, fz, fOffset;  // beam Spot radius [cm], z source, y source
+  Double_t fVx, fVy, fVz;   // Vertex coordinates [cm]
+  FairIon   *fIon;          // Pointer to the FairIon to be generated
+  Int_t    fQ;		    // Electric charge [e]
+
+  Bool_t fBeamSpotIsSet;    // True if point beamspot is set
+    
+  ClassDef(R3BIonGenerator,1)
+
+}; 
+
+
+#endif
+   
+
