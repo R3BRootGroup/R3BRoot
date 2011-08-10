3d3068c231e0c7b6fa87da673e2007d585e333fd
jmargan <jmargan>
2011-08-10 08:58:57 +0000

    new digitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13046 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 0a689d0b9f5e0a02130e5ae4e48057fb2bf533d5

---
+++ mtof/R3BmTofDigiPar.cxx
@@ -0,0 +1,38 @@
+#include <iostream>
+#include "R3BmTofDigiPar.h"
+#include "TMath.h"
+
+R3BmTofDigiPar::R3BmTofDigiPar(const char* name, const char* title, const char* context)
+  : FairParGenericSet(name,title,context)
+{
+  clear();
+}
+
+void R3BmTofDigiPar::putParams(FairParamList* list)
+{
+  std::cout<<"-I- R3BmTofDigiPar::putParams() called"<<std::endl;
+  if(!list) return;
+//  list->add("max_paddle", (Int_t)nMaxPaddle);
+//  list->add("max_plane", (Int_t)nMaxPlane);
+}
+
+Bool_t R3BmTofDigiPar::getParams(FairParamList* list)
+{
+  std::cout<<"-I- R3BmTofDigiPar::getParams() called"<<std::endl;
+  if (!list) return kFALSE;
+  std::cout<<"-I- R3BmTofDigiPar::getParams() 1 ";
+  
+//  if (!list->fill("max_paddle", &nMaxPaddle)) return kFALSE;
+//  if (!list->fill("max_plane", &nMaxPlane)) return kFALSE;
+  return kTRUE;
+}
+
+
+void R3BmTofDigiPar::Print()
+{
+    std::cout<<"-I- mTof Digi Parameters:"<<std::endl;
+//    std::cout<<"   Max Paddle   = "<<nMaxPaddle<<std::endl;
+//    std::cout<<"   Max Plane   = "<<nMaxPlane<<std::endl;
+}
+
+ClassImp(R3BmTofDigiPar);
