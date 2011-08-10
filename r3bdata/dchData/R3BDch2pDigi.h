d95a5fa0892723b48591d10c45ed2e3d9cfd59fa
jmargan <jmargan>
2011-08-10 09:40:06 +0000

    new digitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13086 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: b4cb1b35b305bf2614477a1bd209cec70848b71e

---
+++ r3bdata/dchData/R3BDch2pDigi.h
@@ -0,0 +1,93 @@
+// -------------------------------------------------------------------------
+// -----                      R3BDch2pDigi header file                  -----
+// -----                  Created 26/03/09  by D.Bertini               -----
+// -------------------------------------------------------------------------
+
+
+/**  R3BDch2pDigi.h
+ **/
+
+
+#ifndef R3BDCH2PDIGI_H
+#define R3BDCH2PDDIGI_H
+
+
+
+#include "TObject.h"
+#include "TVector3.h"
+
+
+
+class R3BDch2pDigi : public TObject
+{
+
+ public:
+
+  /** Default constructor **/
+  R3BDch2pDigi();
+  R3BDch2pDigi(Int_t pd1mul,Double_t Pdx1_p1,Double_t Pdy1_p1,Double_t Pdx1_p2,Double_t Pdy1_p2,
+Int_t pd2mul,Double_t Pdx2_p1,Double_t Pdy2_p1,Double_t Pdx2_p2,Double_t Pdy2_p2);
+  
+  /** Copy constructor **/
+  R3BDch2pDigi(const R3BDch2pDigi& point) { *this = point; };
+
+  /** Destructor **/
+  virtual ~R3BDch2pDigi();
+
+  /** Output to screen **/
+  virtual void Print(const Option_t* opt) const;
+
+
+  void Setpd1mul(Int_t mul){Pd1mul = mul;}
+  Double_t Getpd1mul(){return Pd1mul;}
+
+  void SetPdx1_p1(Double_t pos){Pd1x_p1 = pos;}
+  Double_t GetPdx1_p1(){return Pd1x_p1;}
+  
+  void SetPdy1_p1(Double_t pos){Pd1y_p1 = pos;}
+  Double_t GetPdy1_p1(){return Pd1y_p1;}
+  
+  void SetPdx1_p2(Double_t pos){Pd1x_p2 = pos;}
+  Double_t GetPdx1_p2(){return Pd1x_p2;}
+  
+  void SetPdy1_p2(Double_t pos){Pd1y_p2 = pos;}
+  Double_t GetPdy1_p2(){return Pd1y_p2;}
+  
+      
+  
+  void Setpd2mul(Int_t mul){Pd2mul = mul;}
+  Double_t Getpd2mul(){return Pd2mul;}
+  
+  void SetPdx2_p1(Double_t pos){Pd2x_p1 = pos;}
+  Double_t GetPdx2_p1(){return Pd2x_p1;}
+  
+  void SetPdy2_p1(Double_t pos){Pd2y_p1 = pos;}
+  Double_t GetPdy2_p1(){return Pd2y_p1;}
+  
+  void SetPdx2_p2(Double_t pos){Pd2x_p2 = pos;}
+  Double_t GetPdx2_p2(){return Pd2x_p2;}
+  
+  void SetPdy2_p2(Double_t pos){Pd2y_p2 = pos;}
+  Double_t GetPdy2_p2(){return Pd2y_p2;}
+  
+
+
+ protected:
+ 
+   Int_t Pd1mul;
+   Double32_t Pd1x_p1;
+   Double32_t Pd1y_p1;
+   Double32_t Pd1x_p2;
+   Double32_t Pd1y_p2;
+   
+   Int_t Pd2mul;
+   Double32_t Pd2x_p1;
+   Double32_t Pd2y_p1;
+   Double32_t Pd2x_p2;
+   Double32_t Pd2y_p2;
+
+  ClassDef(R3BDch2pDigi,1)
+
+};
+
+#endif
