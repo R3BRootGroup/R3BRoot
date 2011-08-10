3810a6bea7e001fdde2ce3c9b4722011a7cbebd4
jmargan <jmargan>
2011-08-10 09:13:15 +0000

    new digitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13063 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: cc78ef433accadfc617e9aee933007eed998239a

---
+++ tracker/R3BTraDigiPar.h
@@ -0,0 +1,32 @@
+#ifndef R3BTRADIGIPAR_H
+#define R3BTRADIGIPAR_H 1
+
+#include <TVector3.h>
+#include <TObjString.h>
+
+#include "FairParGenericSet.h"
+#include "FairParamList.h"
+
+class R3BTraDigiPar : public FairParGenericSet
+{
+    public :
+	R3BTraDigiPar (const char* name="R3BTraDigiParTest",
+			const char* title="Tutorial  parameter",
+			const char* context="TestDefaultContext");
+	~R3BTraDigiPar(void){};
+	void clear(void){};
+	void putParams(FairParamList* list);
+	Bool_t getParams(FairParamList* list);
+
+	void Print();
+	/** Accessor functions **/
+
+  private:
+  // Digi. Parameters
+
+
+   ClassDef(R3BTraDigiPar,1); //
+};
+
+#endif /* !R3BTRADIGIPAR_H*/
+
