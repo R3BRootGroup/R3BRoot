42c6842fae51471c94379b432e346a1294971407
jmargan <jmargan>
2011-08-10 09:40:29 +0000

    new digitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13087 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: b0ff756b6af443f79427f12be3de3757e080dfc7

--- r3bdata/dchData/R3BDchDigi.h
+++ r3bdata/dchData/R3BDchDigi.h
@@ -12,6 +12,7 @@
 #define R3BDCHDDIGI_H
 
 
+
 #include "TObject.h"
 #include "TVector3.h"
 
@@ -24,6 +25,8 @@ class R3BDchDigi : public TObject
 
   /** Default constructor **/
   R3BDchDigi();
+  R3BDchDigi(Int_t pd1mul,Double_t Pdx1_p1,Double_t Pdy1_p1,Int_t pd2mul,Double_t Pdx2_p1,Double_t Pdy2_p1);
+  
   /** Copy constructor **/
   R3BDchDigi(const R3BDchDigi& point) { *this = point; };
 
@@ -34,7 +37,34 @@ class R3BDchDigi : public TObject
   virtual void Print(const Option_t* opt) const;
 
 
+  void Setpd1mul(Int_t mul){Pd1mul = mul;}
+  Double_t Getpd1mul(){return Pd1mul;}
+
+  void SetPdx1_p1(Double_t pos){Pd1x_p1 = pos;}
+  Double_t GetPdx1_p1(){return Pd1x_p1;}
+  
+  void SetPdy1_p1(Double_t pos){Pd1y_p1 = pos;}
+  Double_t GetPdy1_p1(){return Pd1y_p1;}
+  
+  void Setpd2mul(Int_t mul){Pd2mul = mul;}
+  Double_t Getpd2mul(){return Pd2mul;}
+  
+  void SetPdx2_p1(Double_t pos){Pd2x_p1 = pos;}
+  Double_t GetPdx2_p1(){return Pd2x_p1;}
+  
+  void SetPdy2_p1(Double_t pos){Pd2y_p1 = pos;}
+  Double_t GetPdy2_p1(){return Pd2y_p1;}
+
+
  protected:
+ 
+   Int_t Pd1mul;
+   Double32_t Pd1x_p1;
+   Double32_t Pd1y_p1;
+   
+   Int_t Pd2mul;
+   Double32_t Pd2x_p1;
+   Double32_t Pd2y_p1;
 
   ClassDef(R3BDchDigi,1)
 
