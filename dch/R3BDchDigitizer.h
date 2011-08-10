603b484e92a343f1df6869fc79ae036ec50a05a1
jmargan <jmargan>
2011-08-10 08:49:45 +0000

    new digitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13034 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: eeac4d9f4c2366103d46376a68308f387a386564

--- dch/R3BDchDigitizer.h
+++ dch/R3BDchDigitizer.h
@@ -39,7 +39,7 @@ class R3BDchDigitizer : public FairTask
   virtual void Reset();
 
 
-  R3BDchDigi* AddHit();
+  R3BDchDigi* AddHit(Int_t pd1mul,Double_t Pdx1_p1,Double_t Pdy1_p1,Int_t pd2mul,Double_t Pdx2_p1,Double_t Pdy2_p1);
 
 
   protected:
@@ -51,6 +51,33 @@ class R3BDchDigitizer : public FairTask
   R3BDchDigiPar* fDchDigiPar;
 
   //- Control Hitograms
+  TH1F * DCH1Px;
+  TH1F * DCH2Px;
+  
+  TH1F * DCH1Py;
+  TH1F * DCH2Py;
+  
+  TH1F * DCH1Pz;
+  TH1F * DCH2Pz;
+  
+  TH1F * DCH1X;
+  TH1F * DCH2X;
+  
+  TH1F * DCH1Y;
+  TH1F * DCH2Y;
+  
+  TH1F * DCH1elosshis;
+  TH1F * DCH2elosshis;
+  
+  TH1F * TrackPx;
+  TH1F * TrackPy;
+  TH1F * TrackPz;
+  
+  TH2F * TrackPxVSDCH1Px;
+  
+  Int_t eventNoDch;
+  
+  
   private:
   virtual void SetParContainers();
 
