98e9c65f7f467445aa86ab279d7da7a79f2b3400
jmargan <jmargan>
2011-08-10 09:44:54 +0000

    new digitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13095 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 3298aabcb6e1244f634ff49973f4fc72fa926409

--- r3bdata/tofData/R3BTofDigi.cxx
+++ r3bdata/tofData/R3BTofDigi.cxx
@@ -18,14 +18,16 @@ R3BTofDigi::R3BTofDigi() {
    Tfx_p1=0.;
    Tfy_p1=0.;
    Tft_p1=0.;
+   Tfpath_p1=0.;
 }
 
-R3BTofDigi::R3BTofDigi(Int_t tfmul,Double_t tfwx_p1,Double_t tfwy_p1,Double_t tfwt_p1) {
+R3BTofDigi::R3BTofDigi(Int_t tfmul,Double_t tfwx_p1,Double_t tfwy_p1,Double_t tfwt_p1,Double_t tfwpath_p1) {
    
    Tfmul = tfmul;
    Tfx_p1 = tfwx_p1;
    Tfy_p1 = tfwy_p1;
    Tft_p1 = tfwt_p1;
+   Tfpath_p1 = tfwpath_p1;
 }
 
 // -----   Destructor   ----------------------------------------------------
