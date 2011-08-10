82e321cc0a91574e7cbd99ff0842398eb827b42f
jmargan <jmargan>
2011-08-10 08:59:43 +0000

    new digitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13048 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 107a27252780a4ba4bc6661e398272804cdd131f

---
+++ mtof/R3BmTofDigitizer.h
@@ -0,0 +1,69 @@
+#ifndef R3BMTOFDIGITIZER_H
+#define R3BMTOFDDIGITISER_H 1
+
+
+#include "FairTask.h"
+#include <map>
+#include <string>
+#include "R3BmTofDigiPar.h"
+#include "R3BmTofDigi.h"
+
+class TClonesArray;
+class TObjectArray;
+class TH1F;
+class TH2F;
+
+
+
+class R3BmTofDigitizer : public FairTask
+{
+
+ public:
+
+  /** Default constructor **/  
+  R3BmTofDigitizer();
+
+
+  /** Destructor **/
+  ~R3BmTofDigitizer();
+
+
+  /** Virtual method Init **/
+  virtual InitStatus Init();
+
+
+  /** Virtual method Exec **/
+  virtual void Exec(Option_t* opt);
+
+  virtual void Finish();
+  virtual void Reset();
+
+
+  R3BmTofDigi* AddHit(Int_t ntmul,Double_t ntfx,Double_t ntfy,Double_t ntft,Double_t ntfpath);
+
+
+  protected:
+  TClonesArray* fmTofPoints;
+  TClonesArray* fmTofMCTrack; 
+  TClonesArray* fmTofDigi;
+
+
+  // Parameter class
+  R3BmTofDigiPar* fmTofDigiPar;
+
+  //- Control Hitograms
+  
+  TH1F * NtfXhis;
+  
+  Int_t eventNomTof;
+  
+  
+  private:
+  virtual void SetParContainers();
+
+ 
+  ClassDef(R3BmTofDigitizer,1);
+  
+};
+
+#endif
