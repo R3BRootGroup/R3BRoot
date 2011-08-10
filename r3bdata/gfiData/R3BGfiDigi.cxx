bba26f8aee70de58ece4ec3d7d8ede79f9f4ef4f
jmargan <jmargan>
2011-08-10 09:42:19 +0000

    new digitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13090 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 2851913a38343bccfcda891feebc541cb9e383f2

---
+++ r3bdata/gfiData/R3BGfiDigi.cxx
@@ -0,0 +1,42 @@
+// -------------------------------------------------------------------------
+// -----                      R3BGfiPoint source file                  -----
+// -------------------------------------------------------------------------
+
+#include "R3BGfiDigi.h"
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::flush;
+
+
+// -----   Default constructor   -------------------------------------------
+R3BGfiDigi::R3BGfiDigi() {
+   
+   Gf1mul=0;
+   Gf1_01x=0.;
+   Gf2mul=0;
+   Gf2_01x=0.;
+   
+}
+
+R3BGfiDigi::R3BGfiDigi(Int_t gf1mul,Double_t gf1_01x,Int_t gf2mul,Double_t gf2_01x) {
+   
+   Gf1mul=gf1mul;
+   Gf1_01x=gf1_01x;
+   Gf2mul=gf2mul;
+   Gf2_01x=gf2_01x;
+   
+}
+
+// -----   Destructor   ----------------------------------------------------
+R3BGfiDigi::~R3BGfiDigi() { }
+
+// -----   Public method Print   -------------------------------------------
+void R3BGfiDigi::Print(const Option_t* opt) const {
+
+}
+// -------------------------------------------------------------------------
+
+ClassImp(R3BGfiDigi)
