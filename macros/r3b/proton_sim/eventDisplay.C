68283e2e1127969a1f22c1aaa72ac2f57ee4bacc
fwamers <fwamers>
2011-08-11 11:03:28 +0000

    11.08.2011: "LoadGeometry()" uncommented. F.Wamers.
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13146 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: f0a7f899d1eebaae335f334dcece186dc29be0a1

--- macros/r3b/proton_sim/eventDisplay.C
+++ macros/r3b/proton_sim/eventDisplay.C
@@ -36,7 +36,8 @@ eventDisplay()
  
 
 //  TFile* file = new TFile("r3bpar.root");
-  TFile* file = new TFile("/lustre/land/jmargan/simulation/Ox_1p/r3bpar_bug.root");
+  //TFile* file = new TFile("/lustre/land/jmargan/simulation/Ox_1p/r3bpar_bug.root");
+  TFile* file = new TFile("/u/fwamers/Promotion/L_Analysis/s318/qfs_analysis/ne17/sim_r3broot/output/r3bpar.root");
   file->Get("FairBaseParSet"); 
 
  // -----   Runtime database   ---------------------------------------------
@@ -44,20 +45,23 @@ eventDisplay()
   FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
   FairParRootFileIo* parIn = new FairParRootFileIo();
 //  parIn->open("r3bpar.root");
-  parIn->open("/lustre/land/jmargan/simulation/Ox_1p/r3bpar_bug.root");
+  //parIn->open("/lustre/land/jmargan/simulation/Ox_1p/r3bpar_bug.root");
+  parIn->open("/u/fwamers/Promotion/L_Analysis/s318/qfs_analysis/ne17/sim_r3broot/output/r3bpar.root");
   rtdb->print();
 
 
  //-------
  
 // fRun->SetInputFile("r3bsim.root");
- fRun->SetInputFile("/lustre/land/jmargan/simulation/Ox_1p/r3bsim_bug.root");
+ //fRun->SetInputFile("/lustre/land/jmargan/simulation/Ox_1p/r3bsim_bug.root");
+ fRun->SetInputFile("/u/fwamers/Promotion/L_Analysis/s318/qfs_analysis/ne17/sim_r3broot/output/r3bsim.root");
  
 // fRun->SetInputFile("r3bsim_1000ev_01-30en.root");
 //  fRun->AddFriend("land_digi.root"); // 2nd file where LandHits or whatever are stored
 //  fRun->SetOutputFile("test.root");
-  fRun->SetOutputFile("/lustre/land/jmargan/simulation/Ox_1p/test.root");
-  fRun->LoadGeometry();
+  //fRun->SetOutputFile("/lustre/land/jmargan/simulation/Ox_1p/test.root");
+  fRun->SetOutputFile("/u/fwamers/Promotion/L_Analysis/s318/qfs_analysis/ne17/sim_r3broot/output/test.root");
+  //fRun->LoadGeometry();
 
   R3BEventManager *fMan= new R3BEventManager();
   R3BMCTracks *Track =  new R3BMCTracks ("Monte-Carlo Tracks");
