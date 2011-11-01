1974c17bbc4603f64e48684f28b83b360455c32a
jmargan <jmargan>
2011-11-01 12:35:43 +0000

    Add new variable
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13603 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 4e969ce18463e2592af91625a328f3687fe66416

--- r3bdata/trackerData/R3BTarget2pDigi.cxx
+++ r3bdata/trackerData/R3BTarget2pDigi.cxx
@@ -25,11 +25,31 @@ R3BTarget2pDigi::R3BTarget2pDigi() {
      
      Estar=0;
      
+     pxf=0;
+     pyf=0;
+     pzf=0;
+     pf_tot=0;
+     f_Beta=0;
+     
+     pxp1=0;
+     pyp1=0;
+     pzp1=0;
+     pp1_tot=0;
+     p1_Beta=0;
+     
+     pxp2=0;
+     pyp2=0;
+     pzp2=0;
+     pp2_tot=0;
+     p2_Beta=0;
+     
    
 }
 
 R3BTarget2pDigi::R3BTarget2pDigi(Double_t ss03_smul,Double_t ss03_kmul,Double_t ss06_smul,Double_t ss06_kmul,
-Double_t x0,Double_t y0,Double_t t0,Double_t estar) {
+Double_t x0,Double_t y0,Double_t t0,Double_t estar,Double_t Pxf,Double_t Pyf,Double_t Pzf,Double_t Pf_tot,Double_t f_beta,
+Double_t Pxp1,Double_t Pyp1,Double_t Pzp1,Double_t Pp1_tot,Double_t p1_beta,Double_t Pxp2,Double_t Pyp2,Double_t Pzp2,
+Double_t Pp2_tot,Double_t p2_beta) {
 
 
      Ss03_smul=ss03_smul;
@@ -42,6 +62,24 @@ Double_t x0,Double_t y0,Double_t t0,Double_t estar) {
      T0=t0;
      
      Estar=estar;
+     
+     pxf=Pxf;
+     pyf=Pyf;
+     pzf=Pzf;
+     pf_tot=Pf_tot;
+     f_Beta=f_beta;
+     
+     pxp1=Pxp1;
+     pyp1=Pyp1;
+     pzp1=Pzp1;
+     pp1_tot=Pp1_tot;
+     p1_Beta=p1_beta;
+     
+     pxp2=Pxp2;
+     pyp2=Pyp2;
+     pzp2=Pzp2;
+     pp2_tot=Pp2_tot;
+     p2_Beta=p2_beta;
      
 
 
