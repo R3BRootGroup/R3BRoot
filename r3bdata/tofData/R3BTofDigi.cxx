16a628801c09481cf2ac4ca3b47336c82613d9a9
jmargan <jmargan>
2011-11-01 12:59:37 +0000

    Add new variable
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13612 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 5b4d66c09751daabc55a86289195fcd0d9b5d357

--- r3bdata/tofData/R3BTofDigi.cxx
+++ r3bdata/tofData/R3BTofDigi.cxx
@@ -19,15 +19,22 @@ R3BTofDigi::R3BTofDigi() {
    Tfy_p1=0.;
    Tft_p1=0.;
    Tfpath_p1=0.;
+   Tfwpx_p1=0.;
+   Tfwpy_p1=0.;
+   Tfwpz_p1=0.;
 }
 
-R3BTofDigi::R3BTofDigi(Int_t tfmul,Double_t tfwx_p1,Double_t tfwy_p1,Double_t tfwt_p1,Double_t tfwpath_p1) {
+R3BTofDigi::R3BTofDigi(Int_t tfmul,Double_t tfwx_p1,Double_t tfwy_p1,Double_t tfwt_p1,Double_t tfwpath_p1,
+Double_t tfwpx_p1,Double_t tfwpy_p1,Double_t tfwpz_p1) {
    
    Tfmul = tfmul;
    Tfx_p1 = tfwx_p1;
    Tfy_p1 = tfwy_p1;
    Tft_p1 = tfwt_p1;
    Tfpath_p1 = tfwpath_p1;
+   Tfwpx_p1 = tfwpx_p1;
+   Tfwpy_p1 = tfwpy_p1;
+   Tfwpz_p1 = tfwpz_p1;
 }
 
 // -----   Destructor   ----------------------------------------------------
