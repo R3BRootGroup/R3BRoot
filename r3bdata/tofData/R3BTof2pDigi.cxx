292e6f397966ccf2a7cef1f0c33a9966eb3d64f9
jmargan <jmargan>
2011-11-01 13:00:20 +0000

    Add new variable
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13614 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 9e6a6b5f495570e30edba4488a379a80a0a2c3dd

--- r3bdata/tofData/R3BTof2pDigi.cxx
+++ r3bdata/tofData/R3BTof2pDigi.cxx
@@ -19,24 +19,39 @@ R3BTof2pDigi::R3BTof2pDigi() {
    Tfy_p1=0.;
    Tft_p1=0.;
    Tfpath_p1=0.;
+   Tfwpx_p1=0.;
+   Tfwpy_p1=0.;
+   Tfwpz_p1=0.;
+   
    Tfx_p2=0.;
    Tfy_p2=0.;
    Tft_p2=0.;
    Tfpath_p2=0.;
+   Tfwpx_p2=0.;
+   Tfwpy_p2=0.;
+   Tfwpz_p2=0.;
 }
 
 R3BTof2pDigi::R3BTof2pDigi(Int_t tfmul,Double_t tfwx_p1,Double_t tfwy_p1,Double_t tfwt_p1,
-Double_t tfwx_p2,Double_t tfwy_p2,Double_t tfwt_p2,Double_t tfwpath_p1,Double_t tfwpath_p2) {
+Double_t tfwx_p2,Double_t tfwy_p2,Double_t tfwt_p2,Double_t tfwpath_p1,Double_t tfwpath_p2,Double_t tfwpx_p1,
+Double_t tfwpy_p1,Double_t tfwpz_p1,Double_t tfwpx_p2,Double_t tfwpy_p2,Double_t tfwpz_p2) {
    
    Tfmul = tfmul;
    Tfx_p1 = tfwx_p1;
    Tfy_p1 = tfwy_p1;
    Tft_p1 = tfwt_p1;
+   Tfpath_p1 = tfwpath_p1;
+   Tfwpx_p1 = tfwpx_p1;
+   Tfwpy_p1 = tfwpy_p1;
+   Tfwpz_p1 = tfwpz_p1;
+   
    Tfx_p2 = tfwx_p2;
    Tfy_p2 = tfwy_p2;
    Tft_p2 = tfwt_p2;
-   Tfpath_p1 = tfwpath_p1;
    Tfpath_p2 = tfwpath_p2;
+   Tfwpx_p2 = tfwpx_p2;
+   Tfwpy_p2 = tfwpy_p2;
+   Tfwpz_p2 = tfwpz_p2;
 }
 
 // -----   Destructor   ----------------------------------------------------
