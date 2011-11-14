c47b87d2180cd38e1e3965407ce8c9a91ef34a2e
uhlig <uhlig>
2011-11-14 09:25:36 +0000

    New URL for CDash.
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@13681 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: c7947be509f31ef43c0db3c4abfb7de55b05cb0a

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -4,7 +4,7 @@ set(CTEST_PROJECT_NAME "R3BRoot")
 set(CTEST_NIGHTLY_START_TIME "00:00:00 CEST")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "fairroot.gsi.de")
+set(CTEST_DROP_SITE "cbmroot.gsi.de")
 set(CTEST_DROP_LOCATION "/CDash/submit.php?project=R3BRoot")
 set(CTEST_DROP_SITE_CDASH TRUE)
 
