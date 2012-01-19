4b62cc61e47ba3aff836fde705995c6b3e845fff
denis <denis>
2012-01-19 13:02:23 +0000

    new Geant4 Plists
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@14187 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 7310e5b856e29fb33d83eae505ecbd55ff6138b8

--- plists/G4PenelopeQEDBuilder.cxx
+++ plists/G4PenelopeQEDBuilder.cxx
@@ -43,24 +43,62 @@
 #include "G4ParticleDefinition.hh"
 #include "G4ProcessManager.hh"
 
-#include "G4PenelopeCompton.hh"
-#include "G4PenelopeGammaConversion.hh"
-#include "G4PenelopePhotoElectric.hh"
-#include "G4PenelopeRayleigh.hh"
-
 #if G4VERSION_NUMBER > 922
 #include "G4eMultipleScattering.hh"
 #else
 #include "G4MultipleScattering.hh"
 #endif
 
+#include "G4Gamma.hh"
+#include "G4Electron.hh"
+#include "G4Positron.hh"
+
+// ----- G4 version lower than 9.5 ------ 
+#if G4VERSION_NUMBER < 950
+#include "G4PenelopeCompton.hh"
+#include "G4PenelopeGammaConversion.hh"
+#include "G4PenelopePhotoElectric.hh"
+#include "G4PenelopeRayleigh.hh"
+
 #include "G4PenelopeIonisation.hh"
 #include "G4PenelopeBremsstrahlung.hh"
 #include "G4PenelopeAnnihilation.hh"
 
-#include "G4Gamma.hh"
-#include "G4Electron.hh"
-#include "G4Positron.hh"
+
+// ----- G4 version 9.5 ------ 
+#else
+
+#include "G4PhysicsListHelper.hh"
+
+// gamma
+#include "G4PhotoElectricEffect.hh"
+#include "G4PenelopePhotoElectricModel.hh"
+
+#include "G4ComptonScattering.hh"
+#include "G4PenelopeComptonModel.hh"
+
+#include "G4GammaConversion.hh"
+#include "G4PenelopeGammaConversionModel.hh"
+
+#include "G4RayleighScattering.hh" 
+#include "G4PenelopeRayleighModel.hh"
+
+// e-
+#include "G4eMultipleScattering.hh"
+#include "G4UniversalFluctuation.hh"
+
+#include "G4eIonisation.hh"
+#include "G4PenelopeIonisationModel.hh"
+
+#include "G4eBremsstrahlung.hh"
+#include "G4PenelopeBremsstrahlungModel.hh"
+
+// e+ only
+
+#include "G4eplusAnnihilation.hh"
+#include "G4PenelopeAnnihilationModel.hh"
+
+#endif
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
@@ -86,6 +124,10 @@ void G4PenelopeQEDBuilder::ConstructParticle()
 
 void G4PenelopeQEDBuilder::ConstructProcess()
 {
+
+// ----- G4 version lower than 9.5 ------ 
+#if G4VERSION_NUMBER < 950
+
   // Add standard EM Processes for gamma
   G4ParticleDefinition* particle = G4Gamma::Gamma();
   G4ProcessManager* pmanager = particle->GetProcessManager();
@@ -123,6 +165,67 @@ void G4PenelopeQEDBuilder::ConstructProcess()
   pmanager->AddProcess(new G4PenelopeIonisation,    -1, 2,2);
   pmanager->AddProcess(new G4PenelopeBremsstrahlung,-1,-1,3);
   pmanager->AddProcess(new G4PenelopeAnnihilation,   0,-1,4);
+ 
+
+// ----- G4 version 9.5 ------ 
+#else
+
+  G4PhysicsListHelper* ph = G4PhysicsListHelper::GetPhysicsListHelper();
+
+  // Add standard EM Processes for gamma
+  G4ParticleDefinition* particle = G4Gamma::Gamma();
+
+  G4PhotoElectricEffect* thePhotoElectricEffect = new G4PhotoElectricEffect();
+  thePhotoElectricEffect->SetModel(new G4PenelopePhotoElectricModel());
+  ph->RegisterProcess(thePhotoElectricEffect, particle);
+
+  G4ComptonScattering* theComptonScattering = new G4ComptonScattering();
+  theComptonScattering->SetModel(new G4PenelopeComptonModel());
+  ph->RegisterProcess(theComptonScattering, particle);
+
+  G4GammaConversion* theGammaConversion = new G4GammaConversion();
+  theGammaConversion->SetModel(new G4PenelopeGammaConversionModel());
+  ph->RegisterProcess(theGammaConversion, particle);
+
+  G4RayleighScattering* theRayleigh = new G4RayleighScattering();
+  theRayleigh->SetModel(new G4PenelopeRayleighModel());
+  ph->RegisterProcess(theRayleigh, particle);
+    
+
+  // Add standard EM Processes for e-
+  particle = G4Electron::Electron();
+
+  G4eMultipleScattering* msc = new G4eMultipleScattering();
+  ph->RegisterProcess(msc, particle);
+      
+  // Ionisation
+  G4eIonisation* eIoni = new G4eIonisation();
+  eIoni->SetEmModel(new G4PenelopeIonisationModel());
+  eIoni->SetFluctModel(new G4UniversalFluctuation() );
+  ph->RegisterProcess(eIoni, particle);
+      
+  // Bremsstrahlung
+  G4eBremsstrahlung* eBrem = new G4eBremsstrahlung();
+  eBrem->SetEmModel(new G4PenelopeBremsstrahlungModel());
+  ph->RegisterProcess(eBrem, particle);
+
+  // Add standard EM Processes for e+
+  particle = G4Positron::Positron();
+
+  ph->RegisterProcess(msc, particle);
+      
+  // Ionisation
+  ph->RegisterProcess(eIoni, particle);
+      
+  // Bremsstrahlung
+  ph->RegisterProcess(eBrem, particle);
+
+  //Annihilation
+  G4eplusAnnihilation* eAnni = new G4eplusAnnihilation();
+  eAnni->SetModel(new G4PenelopeAnnihilationModel());
+  ph->RegisterProcess(eAnni, particle);
+
+#endif
 
 }
 
