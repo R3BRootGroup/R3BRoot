b6cffeb4c543305c7c52aa07dc7f4cc44528d4aa
josesrs <josesrs>
2012-10-25 22:47:35 +0000

    Macros for Gamma radiation with phoswich endcap IEM-Madrid
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@17643 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 98c9b4710c1e10d82044321d0d5dad7b2046aecf

---
+++ macros/r3b/califa/eventDisplay_Protons_50MeV.C
@@ -0,0 +1,69 @@
+//Adapted For protons in phoswich endcap IEM - MAdrid CSIC. J Sanchez del Rio Saez (jose.sanchez@csic.es && josesrs@gmail.com)
+
+
+eventDisplay_Protons_50MeV()
+{
+   // ----  Load libraries   -------------------------------------------------
+  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
+  basiclibs();
+  gSystem->Load("libGeoBase");
+  gSystem->Load("libParBase");
+  gSystem->Load("libBase");
+  gSystem->Load("libMCStack");
+  gSystem->Load("libField");
+  gSystem->Load("libGen");
+
+  //----  Load R3B specific libraries ---------------------------------------
+  gSystem->Load("libR3Bbase");
+  gSystem->Load("libR3BPassive");
+  gSystem->Load("libR3BGen");
+  gSystem->Load("libR3BData");
+  gSystem->Load("libR3BCal");
+  gSystem->Load("libR3BCalo");
+  gSystem->Load("libR3BDch");
+  gSystem->Load("libR3BGfi");
+  gSystem->Load("libR3BLand");
+  gSystem->Load("libR3BmTof");
+  gSystem->Load("libR3BTof");
+  gSystem->Load("libR3BTra");
+  
+  
+  //----  Load vis. specific libraries ---------------------------------------
+  gSystem->Load("libEve");
+  gSystem->Load("libEventDisplay");
+
+                                     
+  // -----   Reconstruction run   -------------------------------------------
+  FairRunAna *fRun= new FairRunAna();
+ 
+  TFile* file = new TFile("r3bpar_Protons_50MeV.root");
+  file->Get("FairBaseParSet"); 
+
+ // -----   Runtime database   ---------------------------------------------
+
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parIn = new FairParRootFileIo();
+  parIn->open("r3bpar_Protons_50MeV.root");
+  rtdb->setFirstInput(parIn);
+  rtdb->print();
+
+
+ //-------
+ 
+  fRun->SetInputFile("r3bsim_Protons_50MeV.root");
+
+  fRun->SetOutputFile("test.root");
+  //fRun->LoadGeometry();
+
+  FairEventManager *fMan= new FairEventManager();
+  FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
+  //FairMCPointDraw *LandPoints =   new FairMCPointDraw ("LandPoint",kOrange,  kFullSquare);
+
+                                                               
+  fMan->AddTask(Track);
+  //fMan->AddTask(LandPoints);
+    
+  fMan->Init();                     
+  
+
+}
