6479e18b16c8d712cd47bd598b670d64a1ec8e41
fwamers <fwamers>
2012-10-29 16:10:59 +0000

    Average interaction time and depth fixed.
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@17678 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: ec7d4508cb27f43758fc27c8075caea59085b762

--- tracker/R3BTra.h
+++ tracker/R3BTra.h
@@ -127,7 +127,11 @@ class R3BTra : public R3BDetector
     Int_t          fsector;            //!  volume id
     TLorentzVector fPosIn, fPosOut;    //!  position
     TLorentzVector fMomIn, fMomOut;    //!  momentum
+    Double32_t     fTime_in;              //!  time when entering active volume
+    Double32_t     fTime_out;              //!  time when exiting active volume
     Double32_t     fTime;              //!  time
+    Double32_t     fLength_in;            //!  length when entering active volume
+    Double32_t     fLength_out;            //!  length when exiting active volume
     Double32_t     fLength;            //!  length
     Double32_t     fELoss;             //!  energy loss
     Double32_t     fCutE;              //!  Sec. Energy Cut-Off
