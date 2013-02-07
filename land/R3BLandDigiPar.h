22e7a0520ca9a462ce89a19ee5c610a45db24cbf
kresan <kresan>
2013-02-07 11:03:47 +0000

    save geometry file name
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@18480 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 6c2e429c381d7afccff1f70aad43d4be5b7bf261

--- land/R3BLandDigiPar.h
+++ land/R3BLandDigiPar.h
@@ -30,6 +30,7 @@ class R3BLandDigiPar : public FairParGenericSet
 	const Double_t GetPaddleDepth() { return  paddleDepth;}
 	const Double_t GetPaddleSpacing() { return  paddleSpacing; }
 	const Double_t GetPaddleWrapping() { return  paddleWrapping; }
+	const TString  GetGeometryFileName() { return fGeometryFileName; }
 	void SetMaxPaddle(Int_t paddle ){nMaxPaddle=paddle;}
 	void SetMaxPlane(Int_t pla){nMaxPlane=pla;}
 	void SetPaddleLength(Double_t length){paddleLength=length;}
@@ -37,6 +38,7 @@ class R3BLandDigiPar : public FairParGenericSet
 	void SetPaddleDepth(Double_t depth){paddleDepth=depth;}
 	void SetPaddleSpacing(Double_t space){paddleSpacing=space;}
 	void SetPaddleWrapping(Double_t wrap){paddleWrapping=wrap;}
+	void SetGeometryFileName(TString name) { fGeometryFileName = name; }
 
   private:
   // Digi. Parameters
@@ -47,6 +49,7 @@ class R3BLandDigiPar : public FairParGenericSet
   Double_t paddleDepth;
   Double_t paddleSpacing;
   Double_t paddleWrapping;
+  TString  fGeometryFileName;
 
    ClassDef(R3BLandDigiPar,2); //
 };
