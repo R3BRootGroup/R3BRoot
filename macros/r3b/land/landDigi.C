67e904c51e76629d6114d24aee832615e54d5fdb
kresan <kresan>
2013-06-06 12:54:55 +0000

    remove explicit loading of libraries
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@20149 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 3e3910f43d2ff3297a6e4c48b9e95294502285c2

--- macros/r3b/land/landDigi.C
+++ macros/r3b/land/landDigi.C
@@ -10,32 +10,8 @@ void landDigi(){
   TString outFile ="/data.local2/mheil/fairroot_results/comp_RPC/land_digi_2x2x2.5m_35m_131Sn_1n_1000AMeV_100keV.root";
 //  TString outFile     = "land_digi.root";
 
-  
-  // ----  Load libraries   -------------------------------------------------
-  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
-  basiclibs();
-
-  gSystem->Load("libGenVector");
-
-  gSystem->Load("libGeoBase");
-  gSystem->Load("libParBase");
-
-
-  gSystem->Load("libBase");
-  gSystem->Load("libMCStack");
-  gSystem->Load("libField");
-  gSystem->Load("libGen");
-
-  //----  Load R3B specific libraries ---------------------------------------
-  gSystem->Load("libR3Bbase");
-  gSystem->Load("libR3BGen");
-  gSystem->Load("libR3BPassive");
-  gSystem->Load("libR3BData");
-  gSystem->Load("libR3BLand");
-
-
-
-
+ 
+ 
   // -----   Timer   --------------------------------------------------------
   TStopwatch timer;
   timer.Start();
