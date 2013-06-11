719d32c3e9f3f9a7ea42483ca5d78e9c6648909b
kresan <kresan>
2013-06-11 08:28:45 +0000

    remove loading of libraries
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@20206 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 16fa2f54214e9a40d6bf9a733f118c5f71e324a9

--- macros/r3b/proton_sim/TofDigi.C
+++ macros/r3b/proton_sim/TofDigi.C
@@ -9,34 +9,6 @@ void TofDigi(){
 
 
   
-  // ----  Load libraries   -------------------------------------------------
-  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
-  basiclibs();
-
-  gSystem->Load("libGenVector");
-
-  gSystem->Load("libGeoBase");
-  gSystem->Load("libParBase");
-
-
-  gSystem->Load("libBase");
-  gSystem->Load("libMCStack");
-  gSystem->Load("libField");
-  gSystem->Load("libGen");
-
-  //----  Load R3B specific libraries ---------------------------------------
-  gSystem->Load("libR3Bbase");
-  gSystem->Load("libR3BGen");
-  gSystem->Load("libR3BPassive");
-  gSystem->Load("libR3BData");
-  gSystem->Load("libR3BDch");
-  
-  gSystem->Load("libR3BTof");
-  gSystem->Load("libR3BmTof");
-  gSystem->Load("libR3BTra");
-  gSystem->Load("libR3BGfi");
-
-
   // -----   Timer   --------------------------------------------------------
   TStopwatch timer;
   timer.Start();
