07da4c1dbde69bf644c9196b03d8665942cc3e32
kresan <kresan>
2013-06-11 08:35:22 +0000

    remove loading of libraries
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@20208 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 39576f49a4def4d4c51a8b8901f445f9f113b424

--- macros/r3b/unpack/landUnpack.C
+++ macros/r3b/unpack/landUnpack.C
@@ -1,30 +1,9 @@
 
 
 {
-  // ----- Load libraries ------------------------------------------------------
-  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
-  basiclibs();
-  gSystem->Load("libGenVector");
-  gSystem->Load("libGeoBase");
-  gSystem->Load("libFairDB");
-  gSystem->Load("libParBase");
-  gSystem->Load("libBase");
-  gSystem->Load("libMCStack");
-  gSystem->Load("libField");
-  gSystem->Load("libGen");
-  gSystem->Load("libR3Bbase");
-  gSystem->Load("libR3BGen");
-  gSystem->Load("libR3BPassive");
-  gSystem->Load("libR3BData");
-  gSystem->Load("libR3BLand");
-  gSystem->Load("libMbsAPI");
-  gSystem->Load("libUnpack");
   // ---------------------------------------------------------------------------
-
-
-  // ---------------------------------------------------------------------------
-  FairRemoteSource* source  = new FairRemoteSource("lxi047");
-  //FairLmdSource* source  = new FairLmdSource("/Users/kresan/r323_3607.lmd");
+  //FairRemoteSource* source  = new FairRemoteSource("lxi047");
+  FairLmdSource* source  = new FairLmdSource("/Users/kresan/r323_3607.lmd");
 
   char *strCalDir = "/Users/kresan/r3b/geo/r3broot/calfiles/land/";
   Int_t type = 94;
