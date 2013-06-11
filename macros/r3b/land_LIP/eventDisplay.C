ed3f71824aa527521d79c881b9af7862bc5b5118
kresan <kresan>
2013-06-11 09:19:51 +0000

    remove loading of libraries
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@20216 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 82b60f497259fa891190f934015d3a49fd13e911

--- macros/r3b/land_LIP/eventDisplay.C
+++ macros/r3b/land_LIP/eventDisplay.C
@@ -1,35 +1,5 @@
 eventDisplay()
 {
-   // ----  Load libraries   -------------------------------------------------
-  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
-  basiclibs();
-  gSystem->Load("libGeoBase");
-  gSystem->Load("libParBase");
-  gSystem->Load("libBase");
-  gSystem->Load("libMCStack");
-  gSystem->Load("libField");
-  gSystem->Load("libGen");
-
-  //----  Load R3B specific libraries ---------------------------------------
-  gSystem->Load("libR3Bbase");
-  gSystem->Load("libR3BPassive");
-  gSystem->Load("libR3BGen");
-  gSystem->Load("libR3BData");
-  gSystem->Load("libR3BCal");
-  gSystem->Load("libR3BCalo");
-  gSystem->Load("libR3BDch");
-  gSystem->Load("libR3BGfi");
-  gSystem->Load("libR3BLand");
-  gSystem->Load("libR3BmTof");
-  gSystem->Load("libR3BTof");
-  gSystem->Load("libR3BTra");
-  
-  
-  //----  Load vis. specific libraries ---------------------------------------
-  gSystem->Load("libEve");
-  gSystem->Load("libEventDisplay");
-
-
   // -----   Reconstruction run   -------------------------------------------
   FairRunAna *fRun= new FairRunAna();
  
