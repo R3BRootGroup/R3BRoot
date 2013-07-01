8ba7050b0fea0a990eca11ee78c732d468d87d5d
kresan <kresan>
2013-07-01 12:21:55 +0000

    switch to main r3ball.C
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@20565 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: b5131643c84b34c4d4ebdec6e5134ea96bb3c84e

--- macros/r3b/land/r3blandsim.C
+++ macros/r3b/land/r3blandsim.C
@@ -1,45 +1,57 @@
+
 Int_t r3blandsim()
 {
-  // Load the Main Simulation macro
-  gROOT->LoadMacro("r3ball.C");
+  TString dir = getenv("VMCWORKDIR");
+  TString InFile = dir + TString("/input/signal.1500AMeV.dat");
+  TString OutFile = "r3bsim.root";
+  TString ParFile = "r3bpar.root";
   
-  TString fMC ="TGeant3";
-  TString fGene="ascii";
-  Bool_t fUserPList= kTRUE;
-
+  // Load the Main Simulation macro
+  TString macro_r3ball = getenv("VMCWORKDIR");
+  macro_r3ball += "/macros/r3b/r3ball.C";
+  gROOT->LoadMacro(macro_r3ball.Data());
+  
+  TString fMC = "TGeant3";
+  TString fGene = "ascii";
+  Bool_t fUserPList = kTRUE;
+  
   // Target type
-  TString target="LiH";
-
+  TString target = "LiH";
+  
   TObjString det1("TARGET");
   TObjString det2("ALADIN");
-//   TObjString det2("GLAD");
-  TObjString det3("CRYSTALBALL");
-  TObjString det4("DCH");
-//   TObjString det5("LAND");
-  TObjString det5("SCINTNEULAND");
-
+  TObjString det3("GLAD");
+  TObjString det4("CRYSTALBALL");
+  TObjString det5("DCH");
+  TObjString det6("LAND");
+  TObjString det7("SCINTNEULAND");
+  
   TObjArray fDetList;
   fDetList.Add(&det1);
   fDetList.Add(&det2);
-  fDetList.Add(&det3);
+//  fDetList.Add(&det3);
   fDetList.Add(&det4);
   fDetList.Add(&det5);
-
+//  fDetList.Add(&det6);
+  fDetList.Add(&det7);
+  
   Int_t nEvents = 10;
-
-  Bool_t fEventDisplay = kTRUE;
-
+  
+  Bool_t fEventDisplay = kFALSE;
+  
   // Magnet Field definition
   Bool_t fR3BMagnet = kTRUE;
-
+  
   // Main Sim function call
   r3ball(nEvents,
-	 fDetList,
-	 target,
-	 fEventDisplay,
-	 fMC,
-	 fGene,
-	 fUserPList,
-	 fR3BMagnet);
+         fDetList,
+         target,
+         fEventDisplay,
+         fMC,
+         fGene,
+         fUserPList,
+         fR3BMagnet,
+         OutFile,
+         ParFile,
+         InFile);
 }
-
