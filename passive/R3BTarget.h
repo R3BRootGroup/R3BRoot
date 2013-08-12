c45875410f91039bd3713b64d76b130f980b2d91
kresan <kresan>
2013-08-12 08:03:43 +0000

    remove hardcoded geometry
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@21020 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: ce5369c0f1ba37c83472d37e5664873c7185c4e5

--- passive/R3BTarget.h
+++ passive/R3BTarget.h
@@ -5,31 +5,21 @@
 #include <TString.h>
 
 
-
-
 class R3BTarget : public R3BModule {
-  public:
-    R3BTarget(const char * name, const char *Title="R3B Target");
-    R3BTarget();
-    virtual ~R3BTarget();
-    virtual void ConstructGeometry();
-    void ConstructGeometryOld();
-    virtual Bool_t CheckIfSensitive(std::string name);
+public:
+  R3BTarget(const char * name, const char *Title="R3B Target");
+  R3BTarget();
+  virtual ~R3BTarget();
 
- protected:
-     TString *fTargetName; //!
-     void ConstructGeometry1();
-     void ConstructGeometry2();
-     void ConstructGeometry3();
-     void ConstructGeometry4();
-     void ConstructGeometry5();
-     void ConstructGeometry6();
-     void ConstructGeometry7();
-
-
-    ClassDef(R3BTarget,1)
+  virtual void ConstructGeometry();
   
+  virtual Bool_t CheckIfSensitive(std::string name);
+  
+protected:
+  TString *fTargetName; //!
+  
+public:
+  ClassDef(R3BTarget,1)
 };
 
 #endif //R3BTarget_H
-
