2bfc4223a012afe07d28a901f354665ee07075f6
kresan <kresan>
2013-08-12 08:01:36 +0000

    remove hardcoded geometry
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@21018 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 93a69f52da11ba3d7d9f3725c9cb8bf0905b1c1b

--- passive/R3BGladMagnet.h
+++ passive/R3BGladMagnet.h
@@ -10,31 +10,17 @@
 #include "TGeoMatrix.h"
 
 class R3BGladMagnet : public R3BModule {
-
-private:
-   TGeoCombiTrans *gLobalPos;  //!
-
+  
 public:
-    R3BGladMagnet(const char * name, const char *Title="R3BGlad Magnet");
-    R3BGladMagnet();
-    virtual ~R3BGladMagnet();
-    void ConstructGeometry();
-    void ConstructGeometryOld();
+  R3BGladMagnet(const char * name, const char *Title="R3BGlad Magnet");
+  R3BGladMagnet();
+  virtual ~R3BGladMagnet();
 
-    void ConstructSubPartEcrans(TGeoVolume* vol);
-    void ConstructDemiEcransTh(TGeoVolume* vol);
-    void ConstructEnceinteI(TGeoVolume* pWorld);
-    void ConstructEnceinteE(TGeoVolume* pWorld);
-    void ConstructFonfE(TGeoVolume* pWorld);
-    void ConstructFondS(TGeoVolume* pWorld);
-    void ConstructGToles(TGeoVolume* pWorld);
-
-    Bool_t CheckIfSensitive(std::string name);
-    ClassDef(R3BGladMagnet,1) //R3BGladMagnet
-
-
-
-
+  void ConstructGeometry();
+  
+  Bool_t CheckIfSensitive(std::string name);
+  
+  ClassDef(R3BGladMagnet,1) //R3BGladMagnet
 };
 
 #endif //R3BGladMAGNET_H
