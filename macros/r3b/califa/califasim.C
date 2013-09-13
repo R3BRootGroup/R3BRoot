01d2ae50628f7f23638d28dd934c6fd914b70949
kresan <kresan>
2013-09-13 09:24:29 +0000

    adopt to new califa/r3ball.C
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@21695 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: d9e5c4d7b8fba6656f3a956120a6b0c036967594

--- macros/r3b/califa/califasim.C
+++ macros/r3b/califa/califasim.C
@@ -4,129 +4,106 @@
 // Steering macros to main r3ball.C
 // Author: Hector Alvarez <hector.alvarez@usc.es>
 //
-// Last Update: 25/04/2012
+// Last Update: 13/09/2013
 // Comments:
 //
 //
 //--------------------------------------------------------------------
 
 
-Int_t califasim(){
+Int_t califasim()
+{
+  // Load the Main Simulation macro
+  TString macro_r3ball= getenv("VMCWORKDIR");
+  macro_r3ball+="/macros/r3b/califa/r3ball.C";
+  gROOT->LoadMacro(macro_r3ball.Data());
+  
+  
+  // Output files
+  TString OutFile = "r3bsim.root";
+  TString ParFile = "r3bpar.root";
+  
+  
+  //-------------------------------------------------
+  // Monte Carlo type     |    fMC        (TString)
+  //-------------------------------------------------
+  //   Geant3:                 "TGeant3"
+  //   Geant4:                 "TGeant4"
+  //   Fluka :                 "TFluka"
+  TString fMC ="TGeant4";
+  
+  
+  //-------------------------------------------------
+  // Primaries generation
+  // Event Generator Type |   fGene       (TString)
+  //-------------------------------------------------
+  // Box generator:             "box"
+  // CALIFA generator:          "gammas"
+  // R3B spec. generator:       "r3b"
+  TString fGene="gammas";
+  
+  
+  //-------------------------------------------------
+  // Secondaries  generation (G4 only)
+  // R3B Spec. PhysicList |     fUserPList (Bool_t)
+  // ----------------------------------------------
+  //     VMC Standard           kFALSE
+  //     R3B Special            kTRUE;
+  Bool_t fUserPList= kTRUE;
+  
+  
+  // Target type
+  TString target1="LeadTarget";
+  TString target2="Para";
+  TString target3="Para45";
+  TString target4="LiH";
+  
+  
+  //-------------------------------------------------
+  //- Geometrical Setup Definition
+  //-  Non Sensitiv        |    fDetName (String)
+  //-------------------------------------------------
+  //   Target:                  TARGET
+  //   Magnet:                  ALADIN
+  //                            GLAD
+  //-------------------------------------------------
+  //-  Sensitiv            |    fDetName
+  //-------------------------------------------------
+  //   Calorimeter:             CALIFA
+  //                            CRYSTALBALL
+  //
+  //   Tof:                     TOF
+  //                            MTOF
+  //
+  //   Tracker:                 DCH
+  //                            TRACKER
+  //                            GFI
+  //
+  //   Neutron:                 LAND
+  TMap detGeo;
+  detGeo.Add(new TObjString("CALIFA"), new TObjString("califa_v13_811.geo.root"));
 
-   // Load the Main Simulation macro
-   TString macro_r3ball= getenv("VMCWORKDIR");
-   macro_r3ball+="/macros/r3b/califa/r3ball.C";
-   gROOT->LoadMacro(macro_r3ball.Data());
-
-   //-------------------------------------------------
-   // Monte Carlo type     |    fMC        (TString)
-   //-------------------------------------------------
-   //   Geant3:                 "TGeant3"
-   //   Geant4:                 "TGeant4"
-   //   Fluka :                 "TFluka"
-
-   TString fMC ="TGeant4";
-
-   //-------------------------------------------------
-   // Primaries generation
-   // Event Generator Type |   fGene       (TString)
-   //-------------------------------------------------
-   // Box generator:             "box"
-   // CALIFA generator:          "gammas"
-   // R3B spec. generator:       "r3b"
-
-   TString fGene="gammas";
-
-   //-------------------------------------------------
-   // Secondaries  generation (G4 only)
-   // R3B Spec. PhysicList |     fUserPList (Bool_t)
-   // ----------------------------------------------
-   //     VMC Standard           kFALSE
-   //     R3B Special            kTRUE;
-
-   Bool_t fUserPList= kTRUE;
-
-   // Target type
-   TString target1="LeadTarget";
-   TString target2="Para";
-   TString target3="Para45";
-   TString target4="LiH";
-
-   //-------------------------------------------------
-   //- Geometrical Setup Definition
-   //-  Non Sensitiv        |    fDetName (String)
-   //-------------------------------------------------
-   //   Target:                  TARGET
-   //   Magnet:                  ALADIN
-   //                            GLAD
-   //-------------------------------------------------
-   //-  Sensitiv            |    fDetName
-   //-------------------------------------------------
-   //   Calorimeter:             CALIFA
-   //                            CRYSTALBALL
-   //
-   //   Tof:                     TOF
-   //                            MTOF
-   //
-   //   Tracker:                 DCH
-   //                            TRACKER
-   //                            GFI
-   //
-   //   Neutron:                 LAND
-
-    TObjString det0("TARGET");
-    TObjString det1("ALADIN");
-    TObjString det2("GLAD");
-    TObjString det3("CALIFA");
-    TObjString det4("CRYSTALBALL");
-    TObjString det5("TOF");
-    TObjString det6("MTOF");
-    TObjString det7("DCH");
-    TObjString det8("TRACKER");
-    TObjString det9("GFI");
-    TObjString det10("LAND");
-
-    TObjArray fDetList;
-    //fDetList.Add(&det0);
-    //fDetList.Add(&det2);
-    fDetList.Add(&det3);
-    //fDetList.Add(&det5);
-    //fDetList.Add(&det6);
-    //fDetList.Add(&det7);
-    //fDetList.Add(&det8);
-    //fDetList.Add(&det9);
-    //fDetList.Add(&det10);
-
-
-   //-------------------------------------------------
-   //- N# of Sim. Events   |    nEvents     (Int_t)
-   //-------------------------------------------------
-
-   Int_t nEvents = 1;
-
-   //-------------------------------------------------
-   //- EventDisplay        |    fEventDisplay (Bool_t)
-   //-------------------------------------------------
-   //   connected:              kTRUE
-   //   not connected:          kFALSE
-
-   Bool_t fEventDisplay=kTRUE;
-
-    // Magnet Field definition
+  
+  //-------------------------------------------------
+  //- N# of Sim. Events   |    nEvents     (Int_t)
+  //-------------------------------------------------
+  Int_t nEvents = 1;
+  
+  
+  //-------------------------------------------------
+  //- EventDisplay        |    fEventDisplay (Bool_t)
+  //-------------------------------------------------
+  //   connected:              kTRUE
+  //   not connected:          kFALSE
+  Bool_t fEventDisplay=kTRUE;
+  
+  
+  // Magnet Field definition
 	//Bool_t fR3BMagnet = kTRUE;
 	Bool_t fR3BMagnet = kFALSE;
+
 	
-
-   // Main Sim function call
-   r3ball(  nEvents,
-            fDetList,
-            target2,
-		    fEventDisplay,
-	        fMC,
-	        fGene,
-	        fUserPList,
-            fR3BMagnet
-          );      
-
+  // Main Sim function call
+  r3ball(nEvents, detGeo, target2, fEventDisplay, fMC, fGene, fUserPList,
+         fR3BMagnet, kFALSE, 2000., OutFile, ParFile);
 }
-
