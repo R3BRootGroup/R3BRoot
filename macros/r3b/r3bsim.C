abaf041f5deeb2cffeeba3a91f6d8c4fe06ddcb3
labiche <labiche>
2013-12-11 11:55:11 +0000

    modified r3bsim.C for v14
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@23133 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 2605de7afe63687fbc70404fb9742f91e368130e

--- macros/r3b/r3bsim.C
+++ macros/r3b/r3bsim.C
@@ -84,16 +84,19 @@ Int_t r3bsim()
   detGeo.Add(new TObjString("ALADIN"),        new TObjString("aladin_v13a.geo.root"));
 //  detGeo.Add(new TObjString("GLAD"),          new TObjString("glad_v13a.geo.root"));
   detGeo.Add(new TObjString("CRYSTALBALL"),   new TObjString("cal_v13a.geo.root"));
+//  detGeo.Add(new TObjString("CALIFA"),        new TObjString("califa_v14a.geo.root"));
 //  detGeo.Add(new TObjString("CALIFA"),        new TObjString("califa_v13_811.geo.root"));
   detGeo.Add(new TObjString("TOF"),           new TObjString("tof_v13a.geo.root"));
   detGeo.Add(new TObjString("MTOF"),          new TObjString("mtof_v13a.geo.root"));
   detGeo.Add(new TObjString("DCH"),           new TObjString("dch_v13a.geo.root"));
   detGeo.Add(new TObjString("TRACKER"),       new TObjString("tra_v13vac.geo.root"));
+//  detGeo.Add(new TObjString("STaRTrack"),     new TObjString("startra_v14a.geo.root"));
 //  detGeo.Add(new TObjString("STaRTrack"),     new TObjString("startra_v13a.geo.root"));
   detGeo.Add(new TObjString("GFI"),           new TObjString("gfi_v13a.geo.root"));
   detGeo.Add(new TObjString("LAND"),          new TObjString("land_v12a_10m.geo.root"));
 //  detGeo.Add(new TObjString("SCINTNEULAND"),  new TObjString("neuland_v12a_14m.geo.root"));
-  detGeo.Add(new TObjString("VACVESSELCOOL"), new TObjString("vacvessel_v13a.geo.root"));
+//  detGeo.Add(new TObjString("VACVESSELCOOL"), new TObjString("vacvessel_v14a.geo.root"));   // to be used only with the R3B tracker: "STaRTrack", not "TRACKER"
+//  detGeo.Add(new TObjString("VACVESSELCOOL"), new TObjString("vacvessel_v13a.geo.root"));
 //  detGeo.Add(new TObjString("MFI"),           new TObjString("mfi_v13a.geo.root"));
 //  detGeo.Add(new TObjString("PSP"),           new TObjString("psp_v13a.geo.root"));
 
