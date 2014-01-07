fab687005b9752baa9d8e4d6890a5869c6d88e84
kresan <kresan>
2014-01-07 12:53:58 +0000

    added macros for s406 simulations
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@23400 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: afd50f8ad3655c180a6dc83071a14437f0f659c5

---
+++ macros/r3b/land/s406/eventDisplay.C
@@ -0,0 +1,21 @@
+
+{
+  FairRunAna *fRun= new FairRunAna();
+ 
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo*  parIo1 = new FairParRootFileIo();
+  parIo1->open("/Users/kresan/neuland/r3bpar.s406.255AMeV.root");
+  rtdb->setFirstInput(parIo1);
+  rtdb->print();
+
+  fRun->SetInputFile("/Users/kresan/neuland/r3bsim.s406.255AMeV.root");
+  fRun->SetOutputFile("test.root");
+
+  R3BEventManager *fMan = new R3BEventManager();
+  R3BMCTracks *Track = new R3BMCTracks("Monte-Carlo Tracks");
+  FairMCPointDraw *LandPoints = new FairMCPointDraw("LandPoint", kOrange,  kFullSquare);
+//  fMan->AddTask(Track);
+//  fMan->AddTask(LandPoints);
+  
+  fMan->Init();  
+}
