ec152519204cb7e040c7191fc1c3155479b59cca
mcherciu <mcherciu>
2014-01-15 13:41:27 +0000

    Changes accordingly with R3BLandUnpacker upload
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@23461 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: a023e0c22fa440db171f7fff3906b1eeb8e0bc22

--- unpack/land/R3BLandRawHit.h
+++ unpack/land/R3BLandRawHit.h
@@ -1,37 +1,53 @@
-// -----------------------------------------------------------------------------
-// -----                                                                   -----
-// -----                            R3BLandRawHit                          -----
-// -----                    Created 23.04.2013 by D.Kresan                 -----
-// -----------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------
+// -----                                                                             -----
+// -----                           R3BLandUnpack                                     -----
+// -----                           Version 0.1                                       -----
+// -----         Adapted by M.I. Cherciu @ 01.2014 after Y.Gonzalez code             -----
+// -----                                                                             -----
+// ---------------------------------------------------------------------------------------
 
 #ifndef R3BLANDRAWHIT_H
 #define R3BLANDRAWHIT_H
 
 #include "TObject.h"
 
-
 class R3BLandRawHit : public TObject {
- public:
+public:
+  // Default Constructor
   R3BLandRawHit();
-  R3BLandRawHit(Int_t padId, Double_t qdc1, Double_t qdc2);
-  virtual ~R3BLandRawHit();
+  
+  /** Standard Constructor
+   *@param Tac_addr   tac mod address
+   *@param Tac_ch     tac channel no
+   *@param Cal        calibration bit
+   *@param Cntrl_data cntrl data
+   *@param Tac_data   tac data
+   *@param Qdc_data   qdc data
+   **/
+  R3BLandRawHit(UShort_t Tac_addr, UShort_t Tac_ch, UShort_t Cal, UShort_t Cntrl_data, UShort_t Tac_data, UShort_t Qdc_data);
+  
+  //Destructor
+  ~R3BLandRawHit() { }
+  
+  //Getters
+//  inline const UShort_t& GetTacAddr()    const { return ftaddr;  }
+//  inline const UShort_t& GetTacCh()      const { return ftch;    }
+//  inline const ULong_t&  GetCal()        const { return fcal;    }
+//  inline const ULong_t&  GetCntrlData()  const { return fcntdata;}
+//  inline const ULong_t&  GetTacData()    const { return ftdata;  }
 
- protected:
-  Int_t fPadId;
-  Double32_t fQdc1;
-  Double32_t fQdc2;
+  UShort_t ftaddr;    //... tac mod address
+  UShort_t ftch;      //... tac channel no
+  UShort_t fcal;      //... calibration bit
+  UShort_t fcntdata;  //... cntrl data
+  UShort_t ftdata;    //... tac data
+  UShort_t fqdata;
 
- public:
-  inline Int_t    GetPadId() const { return fPadId; }
-  inline Double_t GetQdc1()  const { return fQdc1;  }
-  inline Double_t GetQdc2()  const { return fQdc2;  }
-
-  ClassDef(R3BLandRawHit, 1)
+  inline const UShort_t&  GetQdcData()    const { return fqdata;  }
+  
+  
+public:
+  ClassDef(R3BLandRawHit,1)
 };
 
-
 #endif
-
-
-
-
