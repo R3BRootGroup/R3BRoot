604838822d98c2365dd4554fd8e6e0970c30e26b
pcabanelas <pcabanelas>
2014-03-19 14:32:55 +0000

    Updated macro
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@24162 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 53478d3271dcaaafc468dc79285f2854b21bad2c

--- macros/r3b/califa/r3bevtvis.C
+++ macros/r3b/califa/r3bevtvis.C
@@ -1,46 +1,11 @@
 r3bevtvis()
 {
-   // ----  Load libraries   -------------------------------------------------
-  gROOT->LoadMacro("$VMCWORKDIR/gconfig/basiclibs.C");
-  basiclibs();
-  gSystem->Load("libGeoBase");
-  gSystem->Load("libFairDB");
-  gSystem->Load("libParBase");
-  gSystem->Load("libBase");
-  gSystem->Load("libMCStack");
-  gSystem->Load("libGen");
-
-  // Load FairRoot base Event Display Libs
-  gSystem->Load("libEve");
-  gSystem->Load("libEventDisplay");
-
-  //----  Load R3B specific libraries ---------------------------------------
-  gSystem->Load("libR3Bbase");
-  gSystem->Load("libR3BGen");
-  gSystem->Load("libR3BPassive");
-  gSystem->Load("libR3BData");
-  gSystem->Load("libR3BXBall");
-  gSystem->Load("libR3BCalo");
-  gSystem->Load("libR3BDch");
-  gSystem->Load("libR3BGfi");
-  gSystem->Load("libR3BLand");
-  gSystem->Load("libR3BmTof");
-  gSystem->Load("libR3BTof");
-  gSystem->Load("libR3BTra");
-
-  //----  Load R3B vis. specific libraries --------------------------  
-  gSystem->Load("libR3BEvtVis");
-
-  // -----   Reconstruction run   -------------------------------------------
   FairRunAna *fRun= new FairRunAna();
-  TFile* file = new TFile("r3bpar.root");
-  file->Get("FairBaseParSet"); 
   
-  // -----   Runtime database   ---------------------------------------------
   FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
-  FairParRootFileIo* parIn = new FairParRootFileIo();
-  parIn->open("r3bpar.root");
-  rtdb->setFirstInput(parIn);
+  FairParRootFileIo*  parIo1 = new FairParRootFileIo();
+  parIo1->open("r3bpar.root");
+  rtdb->setFirstInput(parIo1);
   rtdb->print();
   
   fRun->SetInputFile("r3bsim.root");
@@ -48,11 +13,14 @@ r3bevtvis()
   
   R3BEventManager *fMan= new R3BEventManager();
   R3BMCTracks *Track =  new R3BMCTracks ("Monte-Carlo Tracks");
+
   R3BCaloEventDisplay *CaloEvtVis = new R3BCaloEventDisplay("R3BCaloEventDisplay");
   R3BCaloHitEventDisplay *CaloHitEvtVis = new R3BCaloHitEventDisplay("R3BCaloHitEventDisplay");
-  
+
   fMan->AddTask(CaloEvtVis);
   fMan->AddTask(CaloHitEvtVis);  //Make sure that you have CaloHit branch in the file
+
   fMan->AddTask(Track);
+  
   fMan->Init();
 }
