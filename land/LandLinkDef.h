5988a5188f7ca8222fc8d52bb07ae9fce3d62c0b
kresan <kresan>
2014-04-16 09:05:47 +0000

    do not yet include r3bdb
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@24505 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: b3f5983edab5cad3eae97e1cc99923b34acfc704

--- land/LandLinkDef.h
+++ land/LandLinkDef.h
@@ -32,8 +32,8 @@
 #pragma link C++ class R3BLandMapping+;
 #pragma link C++ class R3BLandRawHitMapped+;
 #pragma link C++ class R3BLandUnpack+;
-#pragma link C++ class R3BLandTcalFill+;
-#pragma link C++ class R3BLandParamRead+;
+//#pragma link C++ class R3BLandTcalFill+;
+//#pragma link C++ class R3BLandParamRead+;
 
 #endif
 
