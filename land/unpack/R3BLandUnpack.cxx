9c34cb064a9ddec853c38c63a055b96a49ce8de6
kresan <kresan>
2014-06-06 10:25:17 +0000

    invert Tacquila time, change UShort to UInt, ignore obsolete trigger, INFO --> DEBUG
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@25278 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 363b99539d983330a72c3e903af59555a8a1a288

--- land/unpack/R3BLandUnpack.cxx
+++ land/unpack/R3BLandUnpack.cxx
@@ -71,10 +71,10 @@ Bool_t R3BLandUnpack::DoUnpack(Int_t* data, Int_t size)
         n17 = 0;
 
         UInt_t* p1 = (UInt_t*)(data + l_i);
-        UShort_t l_sam_id = (p1[0] & 0xf0000000) >> 28; // identifies the sam
-        UShort_t l_gtb_id = (p1[0] & 0x0f000000) >> 24; // 0 or 1, identifies which of the 2 cables of the sam
-        UShort_t l_lec = (p1[0] & 0x00f00000) >> 20;
-        UShort_t l_da_siz = (p1[0] & 0x000001ff);
+        UInt_t l_sam_id = (p1[0] & 0xf0000000) >> 28; // identifies the sam
+        UInt_t l_gtb_id = (p1[0] & 0x0f000000) >> 24; // 0 or 1, identifies which of the 2 cables of the sam
+        UInt_t l_lec = (p1[0] & 0x00f00000) >> 20;
+        UInt_t l_da_siz = (p1[0] & 0x000001ff);
 
         LOG(DEBUG) << "R3BLandUnpack : SAM:" << l_sam_id << ",  GTB:" << l_gtb_id << ",  lec:" << l_lec << ",  size:" << l_da_siz << FairLogger::endl;
 
@@ -84,17 +84,15 @@ Bool_t R3BLandUnpack::DoUnpack(Int_t* data, Int_t size)
 
         for (Int_t i1 = 0; i1 < l_da_siz; i1 += 2)
         {
-            UShort_t tac_addr;
-            UShort_t tac_ch;
-            UShort_t cal;
-            UShort_t clock;
-            UShort_t tac_data;
-            UShort_t qdc_data;
+            UInt_t tac_addr;
+            UInt_t tac_ch;
+            UInt_t clock;
+            UInt_t tac_data;
+            UInt_t qdc_data;
             tac_addr = (p1[i1] & 0xf8000000) >> 27;
             tac_ch = (p1[i1] & 0x07c00000) >> 22;
-            cal = (p1[i1] & 0x003C0000) >> 18;
-            clock = (p1[i1] & 0x0003f000) >> 12;
-            tac_data = (p1[i1] & 0x00000fff);
+            clock = 63 - ((p1[i1] & 0x0003f000) >> 12);
+            tac_data = 4095 - ((p1[i1] & 0x00000fff));
             qdc_data = (p1[i1 + 1] & 0x00000fff);
             l_i += 2;
 
@@ -104,14 +102,14 @@ Bool_t R3BLandUnpack::DoUnpack(Int_t* data, Int_t size)
             }
             LOG(DEBUG) << "R3BLandUnpack : TAC ADDR IS " << tac_addr << ",  TAC CH IS " << tac_ch << ",  TAC Data IS " << tac_data << ",  QDC Data IS " << qdc_data
             << FairLogger::endl;
-            new ((*fRawData)[fNHits]) R3BLandRawHit(l_sam_id, l_gtb_id, tac_addr, tac_ch, cal, clock, tac_data, qdc_data);
+            new ((*fRawData)[fNHits]) R3BLandRawHit(l_sam_id, l_gtb_id, tac_addr, tac_ch, 0, clock, tac_data, qdc_data);
             fNHits++;
         }
 
         LOG(DEBUG) << "R3BLandUnpack : n17=" << n17 << FairLogger::endl;
     }
 
-    LOG(INFO) << "R3BLandUnpack : Number of hits in LAND: " << fNHits << FairLogger::endl;
+    LOG(DEBUG) << "R3BLandUnpack : Number of hits in LAND: " << fNHits << FairLogger::endl;
     return kTRUE;
 }
 
