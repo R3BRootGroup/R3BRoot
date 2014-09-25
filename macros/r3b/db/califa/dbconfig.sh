170724720555ecbf2a7b347b67a8a2552637652d
ygonroz <ygonroz>
2014-09-25 10:03:51 +0000

    Changed dbconfig
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@26075 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 5f5f6f1b959fbc754c14b24ae6a44bbd91d3829e

--- macros/r3b/db/califa/dbconfig.sh
+++ macros/r3b/db/califa/dbconfig.sh
@@ -13,29 +13,26 @@ export FAIRDB_LOGFILE_DB=""
 if [ $1 = "local_mysql" ]
 then
 # Local MySQL server
-export FAIRDB_TSQL_URL="mysql://localhost/r3b"
-export FAIRDB_TSQL_USER="scott"
-export FAIRDB_TSQL_PSWD="tiger"
+export FAIRDB_TSQL_URL="xxx"
+export FAIRDB_TSQL_USER="xxx"
+export FAIRDB_TSQL_PSWD="xxx"
 fi
 
 if [ $1 = "local_pgsql" ]
 then
 # Local PgSQL server
-export PATH=$PATH:$HOME/Work/postgresql/bin
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/Work/postgresql/lib
-export PGSQL=$HOME/Work/postgresql
-export FAIRDB_TSQL_URL="pgsql://localhost:5432/r3b"
-export FAIRDB_TSQL_USER="ygonroz"
-export FAIRDB_TSQL_PSWD="44849937USC"
+export FAIRDB_TSQL_URL="xxx"
+export FAIRDB_TSQL_USER="xxx"
+export FAIRDB_TSQL_PSWD="xxx"
 fi
 
 
 if [ $1  = "local_sqlite" ]
 then
 # SQLite
-export FAIRDB_TSQL_URL="sqlite://test.sqlite"
-export FAIRDB_TSQL_USER="test"
-export FAIRDB_TSQL_PSWD="test"
+export FAIRDB_TSQL_URL="xxx"
+export FAIRDB_TSQL_USER="xxx"
+export FAIRDB_TSQL_PSWD="xxx"
 fi
 
 echo $1 " session configured as: "
