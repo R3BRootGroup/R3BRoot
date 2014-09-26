d8f2cce0b86ff6db79cba8be621756bbe217dbcc
denis <denis>
2014-09-26 15:42:50 +0000

    modifs for initialisation with ascii in CaloCalPar
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@26090 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 705f5c971dbf512ba381aab6e97ccaf27b7b627f

---
+++ macros/r3b/db/califa/califa_test.C
@@ -0,0 +1,47 @@
+Int_t  califa_test()
+{
+  // Create a Runtime Database singleton.
+  FairRuntimeDb* db = FairRuntimeDb::instance();
+
+   // Create in memory the relevant container
+  R3BCaloCalPar* par = (R3BCaloCalPar*)(db->getContainer("CaloCalPar"));  
+
+  // Set the SQL IO as first input
+  FairParAsciiFileIo* inp = new FairParAsciiFileIo();
+
+  TString filename ="ducals.par";
+  inp->open(filename.Data(),"in");
+
+  db->setFirstInput(inp);
+
+// Generate a unique RunID
+  FairRunIdGenerator runID;
+  UInt_t runId =  runID.generateId();
+  db->initContainers(runId);
+
+
+  // Get the container after initialisation
+  // from the RuntimeDB
+  R3BCaloCalPar* par = (R3BCaloCalPar*)(db->getContainer("CaloCalPar"));
+
+  // Dump the Parameters
+  cout << endl;
+  cout << "-I- Reading Parameter data from Ascii File: \n" << filename.Data() << endl;
+  cout << endl;
+
+  par->Print();
+  cout << endl;
+
+  // Convert in ROOT format 
+  par->setChanged();
+  Bool_t kParameterMerged = kTRUE;
+  FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+  parOut->open("califa_cal_par.root");
+  db->setOutput(parOut);
+  db->saveOutput();
+  db->print();
+  // ------------------------------------------------------------------------
+
+  if (db) delete db;
+  return 0;
+}
