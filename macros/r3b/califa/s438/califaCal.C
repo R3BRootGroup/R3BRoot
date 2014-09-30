7d9fc0d2df0d26922501d28ff7023bdfa5c572b7
hapol <hapol>
2014-09-30 11:30:49 +0000

    Updating the calibration macro with the RunTimeDB properly initialized.
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@26130 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 7a550f2c4183efa2bd12ce248c69144f364d0575

--- macros/r3b/califa/s438/califaCal.C
+++ macros/r3b/califa/s438/califaCal.C
@@ -2,21 +2,28 @@
 //
 //   ----- General Macro for R3B CALIFA Analysis
 //         Author: Hector Alvarez <hector.alvarez@usc.es>
-//         Last Update: 25/09/14
+//         Last Update: 30/09/14
 //         Comments:
-//			Runs the CALIFA Hit Finder. Outputs a root file with 
-//			a collection (TClonesArray) of R3BCaloHits
+//	       	Runs the CALIFA calibration. Outputs a root file with 
+//     		a collection (TClonesArray) of R3BCaloCrystalHits
 //
 //  -------------------------------------------------------------------------
 //
 //   Usage: 
 //        > root -l califaCal.C
+//        ROOT> .L califaCal.C
+//        ROOT>  califaCal(inputFile,outputFile,parRootFile,nevents)
 //                         
-//
+//   where
+//    inputFile is the input root file with the R3BCalorRawHits 
+//    outputFile is the output root file where the R3BCaloCrystalHits will 
+//               be created
+//    parRootFile is the parameters ROOT file 
+//    nevents is the number of events to calibrate (0 if all)
 //  -------------------------------------------------------------------------
 
-void califaCal(Int_t nEvents = -1) {
-  
+void califaCal(TString inputFile, TString outputFile, 
+	       TString parRootFile, Int_t nEvents = 0) {
   // -----   Timer   --------------------------------------------------------
   TStopwatch timer;
   timer.Start();
@@ -24,19 +31,46 @@ void califaCal(Int_t nEvents = -1) {
   
   // -----   Create analysis run   ----------------------------------------
   FairRunAna* fRun = new FairRunAna();
-  fRun->SetInputFile("./data/na22_low_range_30mev_raw.root");
-  fRun->SetOutputFile("./data/na22_low_range_30mev_cal.root");
+  fRun->SetInputFile(inputFile);
+  fRun->SetOutputFile(outputFile);
   
+  // -----   Runtime database   ---------------------------------------------
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo* parRootIn; 
+  FairParAsciiFileIo* parAsciiIn; 
+  
+  // if(parAsciiFile) {
+  //   parAsciiIn = new FairParAsciiFileIo();
+  //   parAsciiIn->open(parAsciiFile);
+  //   rtdb->setFirstInput(parAsciiIn);
+  //   if(parRootFile) {
+  //     parRootIn = new FairParRootFileIo();
+  //     parRootIn->open(parRootFile);
+  //     rtdb->setSecondInput(parRootIn);
+  //   }
+  // }
+  //else if(parRootFile) {
+  parRootIn = new FairParRootFileIo();
+  parRootIn->open(parRootFile);
+  rtdb->setFirstInput(parRootIn);
+    //}
+  //rtdb->setOutput(parIn);
+  //rtdb->saveOutput();
+  rtdb->print();
+  
+  /*TList *lPars = rtdb->getListOfContainers();
+  for (Int_t i=0;i<lPars->GetEntries();i++) {
+    FairParSet* apar = (FairParSet*) lPars->At(i);
+    apar->Print();
+  }    
+  R3BCaloCalPar* par = (R3BCaloCalPar*)rtdb->getContainer("CaloCalPar");  
+  if(par) par->Print();
+  else cout << "NO PAR!!!!" << endl << endl;
 
-    // -----   Runtime database   ---------------------------------------------
-  //FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
-  //R3BCaloDUCalPar* duPar = (R3BCaloDUCalPar*)rtdb->getContainer("CaloDUCalPar");
-
-  //FairParAsciiFileIo* parIn = new FairParAsciiFileIo();
-  //parIn->open("/Users/hapol/CODES/r3broot/macros/r3b/califa/params.txt");
-  //rtdb->setFirstInput(parIn);
-  //rtdb->print();
-
+  R3BCaloDUCalPar* du =  par->GetDUCalParAt(0);
+  if(du) du->Print();
+  else cout << "NO DU!!!!" << endl << endl;
+  */
 
   //Crystal calibration
   R3BCaloCal* cal = new R3BCaloCal();
@@ -46,7 +80,7 @@ void califaCal(Int_t nEvents = -1) {
   fRun->Init();       
   FairLogger::GetLogger()->SetLogScreenLevel("INFO");
              
-  fRun->Run(0, 10000);
+  fRun->Run(0,nEvents);
   
   // -----   Finish   -------------------------------------------------------
   timer.Stop();
