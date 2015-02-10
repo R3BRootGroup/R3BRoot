91ce7501070085b39e1231da6f29c5290313962b
kresan <kresan>
2015-02-10 14:00:26 +0000

    store calibration bit
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@27043 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 1ea6238c253ed7381081219ddabbb513085a14f9

--- land/unpack/R3BLandUnpack.cxx
+++ land/unpack/R3BLandUnpack.cxx
@@ -86,11 +86,13 @@ Bool_t R3BLandUnpack::DoUnpack(Int_t* data, Int_t size)
         {
             UInt_t tac_addr;
             UInt_t tac_ch;
+            UInt_t cal;
             UInt_t clock;
             UInt_t tac_data;
             UInt_t qdc_data;
             tac_addr = (p1[i1] & 0xf8000000) >> 27;
             tac_ch = (p1[i1] & 0x07c00000) >> 22;
+            cal = (p1[i1] & 0x003C0000) >> 18;
             clock = 63 - ((p1[i1] & 0x0003f000) >> 12);
             tac_data = 4095 - ((p1[i1] & 0x00000fff));
             qdc_data = (p1[i1 + 1] & 0x00000fff);
@@ -102,7 +104,7 @@ Bool_t R3BLandUnpack::DoUnpack(Int_t* data, Int_t size)
             }
             LOG(DEBUG) << "R3BLandUnpack : TAC ADDR IS " << tac_addr << ",  TAC CH IS " << tac_ch << ",  TAC Data IS " << tac_data << ",  QDC Data IS " << qdc_data
             << FairLogger::endl;
-            new ((*fRawData)[fNHits]) R3BLandRawHit(l_sam_id, l_gtb_id, tac_addr, tac_ch, 0, clock, tac_data, qdc_data);
+            new ((*fRawData)[fNHits]) R3BLandRawHit(l_sam_id, l_gtb_id, tac_addr, tac_ch, cal, clock, tac_data, qdc_data);
             fNHits++;
         }
 
