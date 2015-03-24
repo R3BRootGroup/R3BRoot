5b54f4aeb0c0534a849e6a72fa5546e3a15b63e1
kresan <kresan>
2015-03-24 09:31:23 +0000

    moved R3BLandRawHit to r3bdata; added histogramming for s438b
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@27277 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 0c8d4b2a4bfd8a5e3daffa0b830799a35161c19b

--- land/LandLinkDef.h
+++ land/LandLinkDef.h
@@ -28,7 +28,7 @@
 #pragma link C++ class R3BNeutHit+;
 
 #pragma link C++ class R3BLandRawAna+;
-#pragma link C++ class R3BLandRawHit+;
+#pragma link C++ class R3BLandRawAnaS438b+;
 #pragma link C++ class R3BLandMapping+;
 #pragma link C++ class R3BLandRawHitMapped+;
 #pragma link C++ class R3BLandUnpack+;
