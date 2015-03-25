c3bbee9e3e1d9ce4ad6389b4980f8d736186168c
uhlig <uhlig>
2015-03-25 14:56:22 +0000

    Add FairRoot version to label which is shown on CDash.
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@27300 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 2a2b097d51b06be59bb798a868d87f25ddbe8244

--- Dart.sh
+++ Dart.sh
@@ -74,7 +74,7 @@ else
   GCC_VERSION=$($CXX -dumpversion)
 fi
 
-export LABEL1=${LINUX_FLAVOUR}-$SYSTEM-$COMPILER$GCC_VERSION-fairsoft_$FAIRSOFT_VERSION
+export LABEL1=${LINUX_FLAVOUR}-$SYSTEM-$COMPILER$GCC_VERSION-fairsoft_$FAIRSOFT_VERSION-fairroot_$FAIRROOT_VERSION
 export LABEL=$(echo $LABEL1 | sed -e 's#/#_#g')
 
 # get the number of processors
