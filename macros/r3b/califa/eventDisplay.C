01fa05e4d6fcef2a5a5b33b1e8376f18b29c43ea
pcabanelas <pcabanelas>
2015-07-30 09:36:11 +0000

    Bug fixed
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@28265 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: 2be3db90a9b00e2edc859ed22eaefeb02f8f6da0

--- macros/r3b/califa/eventDisplay.C
+++ macros/r3b/califa/eventDisplay.C
@@ -1,31 +1,27 @@
 eventDisplay()
 {
-  // -----   Reconstruction run   -------------------------------------------
   FairRunAna *fRun= new FairRunAna();
   
-  TFile* file = new TFile("r3bpar.root");
-  file->Get("FairBaseParSet"); 
-  
-  // -----   Runtime database   ---------------------------------------------
   FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
-  FairParRootFileIo* parIn = new FairParRootFileIo();
-  parIn->open("r3bpar.root");
-  rtdb->setFirstInput(parIn);
+  FairParRootFileIo*  parIo1 = new FairParRootFileIo();
+  parIo1->open("r3bpar.root");
+  rtdb->setFirstInput(parIo1);
   rtdb->print();
-
-
+  
   fRun->SetInputFile("r3bsim.root");
-  
   fRun->SetOutputFile("test.root");
-  //fRun->LoadGeometry();
   
-  FairEventManager *fMan= new FairEventManager();
-  FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
-  //FairMCPointDraw *LandPoints =   new FairMCPointDraw ("LandPoint",kOrange,  kFullSquare);
-  
-  
+  R3BEventManager *fMan= new R3BEventManager();
+  R3BMCTracks *Track =  new R3BMCTracks ("Monte-Carlo Tracks");
+
+  R3BCaloEventDisplay *CaloEvtVis = new R3BCaloEventDisplay("R3BCaloEventDisplay");
+  R3BCaloHitEventDisplay *CaloHitEvtVis = new R3BCaloHitEventDisplay("R3BCaloHitEventDisplay");
+  CaloEvtVis->SelectGeometryVersion(10);
+  fMan->AddTask(CaloEvtVis);
+  fMan->AddTask(CaloHitEvtVis);
+
   fMan->AddTask(Track);
-  //fMan->AddTask(LandPoints);
+  
   
   fMan->Init();
 }
