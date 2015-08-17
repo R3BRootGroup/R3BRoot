4ebd80072bb669bc1ed85da23267f7d26a144726
janmayer <janmayer>
2015-08-17 13:01:18 +0000

    Removed old NeuLandDigitizer
    git-svn-id: https://subversion.gsi.de/fairroot/r3broot/trunk@28363 0381ead4-6506-0410-b988-94b70fbc4730
    
    Former-commit-id: af181bef035833d3bc55ea21e644b006ce76314d

--- land/LandLinkDef.h
+++ land/LandLinkDef.h
@@ -3,7 +3,7 @@
 #pragma link off all globals;
 #pragma link off all classes;
 #pragma link off all functions;
- 
+
 #pragma link C++ class R3BGeoLand+;
 #pragma link C++ class R3BGeoLandPar;
 #pragma link C++ class R3BLand+;
@@ -11,7 +11,6 @@
 #pragma link C++ class R3BNeuLandv1+;
 #pragma link C++ class R3BLandDigitizer+;
 #pragma link C++ class R3BLandDigitizerQA+;
-#pragma link C++ class R3BNeuLandDigitizer+;
 #pragma link C++ class R3BLandDigiPar+;
 #pragma link C++ class R3BLandData+;
 #pragma link C++ class R3BNeutronTracker+;
