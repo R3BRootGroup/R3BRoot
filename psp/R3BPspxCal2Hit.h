d5e36dd89a2cca670e8dbe72b68b8cf665db645b
Dmytro Kresan <D.Kresan@gsi.de>
2016-06-16 09:40:32 +0200

    Fix ROOT 5 dictionary problem with data members.

--- psp/R3BPspxCal2Hit.h
+++ psp/R3BPspxCal2Hit.h
@@ -12,6 +12,14 @@
 
 #include "FairTask.h"
 
+const static UInt_t ndetectors = 5;
+const static UInt_t nstrips = 16;
+const static UInt_t length_array_detector = ndetectors+1;
+const static UInt_t length_array_strip= nstrips+1;
+
+const static Double_t detector_length=5.; // in cm
+const static Double_t strip_width=detector_length/nstrips; // in cm
+
 class TClonesArray;
 class R3BEventHeader;
 
@@ -37,17 +45,10 @@ class R3BPspxCal2Hit : public FairTask
     TClonesArray* fCalibration; // holding slopes and offsets
     
     // calibration parameters, might be recreated in another way soon
-    const static UInt_t ndetectors = 5;
-    const static UInt_t nstrips = 16;
-    const static UInt_t length_array_detector = ndetectors+1;
-    const static UInt_t length_array_strip= nstrips+1;
     Float_t offset[length_array_detector][length_array_strip];
     Float_t slope[length_array_detector][length_array_strip];
     Int_t sign_x[length_array_detector];
     Int_t sign_y[length_array_detector];
-    
-    constexpr static Double_t detector_length=5.; // in cm
-    constexpr static Double_t strip_width=detector_length/nstrips; // in cm
     
     void InitCalibration();
 
