5ffb50db97bb4376c801b89717ef250480862d9b
Dmytro Kresan <D.Kresan@gsi.de>
2016-11-17 10:06:15 +0100

    Introduce track propagator using Runge Kutta method. Example of propagation to mTOF.

---
+++ macros/r3b/tracking/eventDisplay.C
@@ -0,0 +1,23 @@
+void eventDisplay()
+{
+  FairRunAna *fRun= new FairRunAna();
+  
+  FairRuntimeDb* rtdb = fRun->GetRuntimeDb();
+  FairParRootFileIo*  parIo1 = new FairParRootFileIo();
+  parIo1->open("par.root");
+  rtdb->setFirstInput(parIo1);
+  rtdb->print();
+  
+  fRun->SetInputFile("sim.root");
+  fRun->SetOutputFile("test.root");
+  
+  FairEventManager *fMan= new FairEventManager();
+  FairMCTracks *Track =  new FairMCTracks ("Monte-Carlo Tracks");
+  FairMCPointDraw *LandPoints =   new FairMCPointDraw ("LandPoint",kOrange,  kFullSquare);
+  
+  fMan->AddTask(Track);
+  
+  fMan->AddTask(LandPoints);
+  
+  fMan->Init();
+}
--- mtof/R3BmTof.cxx
+++ mtof/R3BmTof.cxx
@@ -10,6 +10,7 @@
 #include "R3BmTofPoint.h"
 #include "R3BGeomTofPar.h"
 #include "R3BMCStack.h"
+#include "R3BTGeoPar.h"
 
 #include "FairGeoInterface.h"
 #include "FairGeoLoader.h"
@@ -89,6 +90,21 @@ void R3BmTof::Initialize()
 
     LOG(INFO) << "R3BmTof: initialisation" << FairLogger::endl;
     LOG(DEBUG) << "R3BmTof: Sci. Vol. (McId) " << gMC->VolId("mTOFLog") << FairLogger::endl;
+
+
+    fTGeoPar = (R3BTGeoPar*) FairRuntimeDb::instance()->getContainer("mTofGeoPar");
+
+    // Position and rotation
+    TGeoNode* main_vol = gGeoManager->GetTopVolume()->FindNode("mTOF_0");
+    TGeoMatrix *matr = main_vol->GetMatrix();
+    fTGeoPar->SetPosXYZ(matr->GetTranslation()[0], matr->GetTranslation()[1], matr->GetTranslation()[2]);
+    fTGeoPar->SetRotXYZ(0., -TMath::Abs(TMath::ASin(matr->GetRotationMatrix()[2]) * TMath::RadToDeg()), 0.);
+
+    // Dimensions
+    TGeoBBox *box = (TGeoBBox*) gGeoManager->GetVolume(gMC->VolId("mTOFLog"))->GetShape();
+    fTGeoPar->SetDimXYZ(box->GetDX(), box->GetDY(), box->GetDZ());
+
+    fTGeoPar->setChanged();
 }
 
 void R3BmTof::SetSpecialPhysicsCuts()
