91ce606717e5a4d043f5346fe46bd0ded6684b53
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2017-01-12 13:34:44 +0100

    Cleaned gconfig, removed obsolete plists

--- gconfig/g3Config.C
+++ gconfig/g3Config.C
@@ -1,56 +1,44 @@
-// $Id: g3Config.C,v 1.1.1.1 2005/06/23 07:14:09 dbertini Exp $
-//
-// Configuration macro for Geant3 VirtualMC 
+// Configuration macro for Geant3 VirtualMC
 
 void Config()
 {
-  FairRunSim *fRun = FairRunSim::Instance();
-  TString* gModel = fRun->GetGeoModel();
-  TGeant3* geant3 = NULL;
-  if ( strncmp(gModel->Data(),"TGeo",4) == 0 ) {
-	geant3
-	  = new  TGeant3TGeo("C++ Interface to Geant3");
-	cout << "-I- G3Config: Geant3 with TGeo has been created."
-		 << endl;
-  }else{
-	geant3
-	  = new  TGeant3("C++ Interface to Geant3");
-	cout << "-I- G3Config: Geant3 native has been created."
-		 << endl;
-  }
-  // create R3B  Specific Stack
-  R3BStack *st = new R3BStack();
-  // Stack debug mode
-  st->SetDebug(kFALSE);
-//  st->SetDebug(kTRUE);
-  st->SetMinPoints(1);
-  st->StoreSecondaries(kTRUE);
-  
-  geant3->SetStack( st ) ;
-  
-  // ******* GEANT3  specific configuration for simulated Runs  *******
-  geant3->SetTRIG(1);         //Number of events to be processed
-  geant3->SetSWIT(4, 10);
-  geant3->SetDEBU(0, 0, 1);
-  
-  //<DB> Command for debugging purpose
-//  geant3->SetSWIT(2, 2);
-//  geant3->SetDEBU(1, 100, 2); 
-  
-  geant3->SetRAYL(1);
-  geant3->SetSTRA(1);
-  geant3->SetAUTO(1);         //Select automatic STMIN etc... calc. (AUTO 1) or manual (AUTO 0)
-  geant3->SetABAN(0);         //Restore 3.16 behaviour for abandoned tracks
-  geant3->SetOPTI(2);         //Select optimisation level for GEANT geometry searches (0,1,2)
-  geant3->SetERAN(5.e-7);
-  geant3->SetCKOV(1);     // cerenkov photons
-  //    geant3->SetHADR(5);     // gcalor
-  // set common stuff 
-  TString configm(gSystem->Getenv("VMCWORKDIR"));
-  TString cuts = configm + "/gconfig/SetCuts.C";
-  cout << "Physics cuts with script \n "<<  cuts.Data() << endl;
-  Int_t cut=gROOT->LoadMacro(cuts.Data());
-  if(cut==0)gInterpreter->ProcessLine("SetCuts()"); 
+    cout << "[g3Config]: Creating TGeant3 VirtualMC ..." << endl;
+
+    TGeant3* geant3 = new TGeant3TGeo("C++ Interface to Geant3");
+
+    R3BStack* stack = new R3BStack();
+    stack->SetDebug(kFALSE);
+    stack->StoreSecondaries(kTRUE);
+    stack->SetMinPoints(1);
+    geant3->SetStack(stack);
+
+    // ******* GEANT3  specific configuration for simulated Runs  *******
+    geant3->SetTRIG(1); // Number of events to be processed
+    geant3->SetSWIT(4, 10);
+    geant3->SetDEBU(0, 0, 1);
+
+    //<DB> Command for debugging purpose
+    //  geant3->SetSWIT(2, 2);
+    //  geant3->SetDEBU(1, 100, 2);
+
+    geant3->SetRAYL(1);
+    geant3->SetSTRA(1);
+    geant3->SetAUTO(1); // Select automatic STMIN etc... calc. (AUTO 1) or manual (AUTO 0)
+    geant3->SetABAN(0); // Restore 3.16 behaviour for abandoned tracks
+    geant3->SetOPTI(2); // Select optimisation level for GEANT geometry searches (0,1,2)
+    geant3->SetERAN(5.e-7);
+    geant3->SetCKOV(1); // cerenkov photons
+
+    gMC->SetProcess("PAIR", 1); /** pair production*/
+    gMC->SetProcess("COMP", 1); /**Compton scattering*/
+    gMC->SetProcess("PHOT", 1); /** photo electric effect */
+    gMC->SetProcess("PFIS", 0); /**photofission*/
+    gMC->SetProcess("DRAY", 0); /**delta-ray*/
+    gMC->SetProcess("ANNI", 1); /**annihilation*/
+    gMC->SetProcess("BREM", 1); /**bremsstrahlung*/
+    gMC->SetProcess("HADR", 5); /**hadronic process*/
+    gMC->SetProcess("MUNU", 1); /**muon nuclear interaction*/
+    gMC->SetProcess("DCAY", 1); /**decay*/
+    gMC->SetProcess("LOSS", 2); /**energy loss (, 3)?*/
+    gMC->SetProcess("MULS", 1); /**multiple scattering*/
 }
-
-
