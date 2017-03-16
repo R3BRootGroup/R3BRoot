20e66110600672422046a274dd4b67d79b6da099
Dmytro Kresan <d.kresan@gsi.de>
2017-03-16 08:43:42 +0100

    Temporary fix: ROOT has problem to generate dictionary if unique pointer is used. Avoid new I/O scheme in LinkDef.

--- neuland/digitizing/NeulandDigitizingLinkDef.h
+++ neuland/digitizing/NeulandDigitizingLinkDef.h
@@ -5,7 +5,7 @@
 #pragma link off all functions;
 
 #pragma link C++ class Neuland::DigitizingEngine+;
-#pragma link C++ class Neuland::DigitizingTacQuila+;
+#pragma link C++ class Neuland::DigitizingTacQuila;
 #pragma link C++ class R3BNeulandDigiMon+;
 #pragma link C++ class R3BNeulandDigitizer+;
 
