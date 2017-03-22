7c36c9e069a1c58251d777c564222b4d36ecb9b5
Jan Mayer <jan.mayer@ikp.uni-koeln.de>
2017-03-22 16:07:48 +0100

    Fixes test failure with root5 and build issues with new FairMQ

--- neuland/reconstruction/NeulandReconstructionLinkDef.h
+++ neuland/reconstruction/NeulandReconstructionLinkDef.h
@@ -2,7 +2,7 @@
 
 #pragma link C++ class Neuland::Reconstruction2DCalibr+;
 #pragma link C++ class Neuland::ReconstructionEGate+;
-#pragma link C++ class Neuland::ReconstructionNeuralNetworkMultiplicity+;
+// #pragma link C++ class Neuland::ReconstructionNeuralNetworkMultiplicity+;
 #pragma link C++ class R3BNeulandNeutronReconstruction+;
 #pragma link C++ class R3BNeulandNeutronReconstructionMon+;
 
