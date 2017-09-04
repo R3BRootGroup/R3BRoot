393da5693ff84ddc83b2d06b90de32b525a063ca
Dmytro Kresan <d.kresan@gsi.de>
2017-09-04 09:46:47 +0200

    Remove obsolete classes in r3bgen directory.

--- r3bgen/R3BGenLinkDef.h
+++ r3bgen/R3BGenLinkDef.h
@@ -6,7 +6,6 @@
 #pragma link off all classes;
 #pragma link off all functions;
 
-#pragma link C++ class  R3BPrimaryGenerator+;
 #pragma link C++ class  R3BSpecificGenerator+;
 #pragma link C++ class  R3BReadKinematics+;
 #pragma link C++ class  R3BCDGenerator+;
@@ -18,8 +17,6 @@
 #pragma link C++ class  R3BCALIFATestGenerator+;
 #pragma link C++ class  R3BCosmicGenerator+;
 #pragma link C++ class  R3Bp2pGenerator+;
-#pragma link C++ class  R3BAsciiIQMDGen+;
-#pragma link C++ class  R3BAsciiUrQMDGen+;
 #pragma link C++ class  R3BIonGenerator+;
 #pragma link C++ class  R3BGammaGenerator+;
 
