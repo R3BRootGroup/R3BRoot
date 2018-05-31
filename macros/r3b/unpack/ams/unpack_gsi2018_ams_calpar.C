67b18e605f8732d5c91d07b3992b9e121a57fa33
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2018-05-31 23:16:34 +0200

    macros for unpack of ams detectors

---
+++ macros/r3b/unpack/ams/unpack_gsi2018_ams_calpar.C
@@ -0,0 +1,107 @@
+/* Additional info:
+ * To generate the header file used for the R3BUcesbSource (ext_h101.h), use:
+ *
+ * $UCESB_DIR/upexps/FIXME --ntuple=UNPACK:TRIGGER,UNPACK:EVENTNO,RAW\
+ *     STRUCT_HH,ext_h101.h
+ *
+ * Put this header file into the 'r3bsource' directory and recompile.
+ * */
+
+typedef struct EXT_STR_h101_t {//FIXME for ams
+  EXT_STR_h101_unpack_t unpack;
+  EXT_STR_h101_CALIFA_t califa;
+} EXT_STR_h101;
+
+void unpack_gsi2018_ams_calpar() {
+  TStopwatch timer;
+  timer.Start();
+  
+  //const Int_t nev = -1; /* number of events to read, -1 - until CTRL+C */
+  const Int_t nev = -1; /* number of events to read, -1 - until CTRL+C */
+  
+  /* Create source using ucesb for input ------------------ */
+  
+  TString filename = "~/lmd/data_0003.lmd";
+  TString outputFileName = "data_0001.root";
+  
+  TString ntuple_options = "UNPACK:EVENTNO,UNPACK:TRIGGER,RAW";
+  TString ucesb_dir = getenv("UCESB_DIR");
+  
+  TString ucesb_path = ucesb_dir + "/../upexps/califaKrakow17/califa";//FIXME
+  
+  EXT_STR_h101 ucesb_struct;
+  
+  R3BUcesbSource* source = new R3BUcesbSource(filename, ntuple_options,
+					      ucesb_path, &ucesb_struct, sizeof(ucesb_struct));
+  source->SetMaxEvents(nev);
+  
+  source->AddReader(new R3BUnpackReader((EXT_STR_h101_unpack*)&ucesb_struct,
+					offsetof(EXT_STR_h101, unpack)));
+  source->AddReader(new R3BAmsReader((EXT_STR_h101_CALIFA*)&ucesb_struct.califa,
+					     offsetof(EXT_STR_h101, califa)));//FIXME
+
+  
+  /* Create online run ------------------------------------ */
+  FairRunOnline* run = new FairRunOnline(source);
+  run->SetRunId(1513078509);
+  run->SetOutputFile(outputFileName);
+
+  
+  /* Add analysis task ------------------------------------ */
+  R3BAmsMapped2StripCalPar* CalPar = new R3BAmsMapped2StripCalPar();
+  CalPar->SetNumDetectors(1); // Number of ams detectors 
+  CalPar->SetNumStrips(1024); // 1024 strips per ams detector
+  CalPar->SetNumStripsK(384); // 384 strips for the K-side
+  CalPar->SetNumStripsS(640); // 640 strips for the S-side
+  CalPar->SetMinStadistics(1000);
+  CalPar->SetCalRange_left(0);  
+  CalPar->SetCalRange_right(2000);
+  CalPar->SetCalRange_bins(500);
+  run->AddTask(CalPar);
+
+
+  /* Initialize ------------------------------------------- */
+  run->Init();
+  FairLogger::GetLogger()->SetLogScreenLevel("INFO");
+  //FairLogger::GetLogger()->SetLogScreenLevel("WARNING");
+  //FairLogger::GetLogger()->SetLogScreenLevel("DEBUG");
+  /* ------------------------------------------------------ */
+  
+
+  /* Runtime data base ------------------------------------ */
+  FairRuntimeDb* rtdb = run->GetRuntimeDb();
+
+  //Choose Root or Ascii file	
+  //1-Root file with the Calibartion Parameters
+  Bool_t kParameterMerged = kTRUE;
+  FairParRootFileIo* parOut = new FairParRootFileIo(kParameterMerged);
+  parOut->open("Ams_CalibParam.root");
+  rtdb->setOutput(parOut);
+  rtdb->print();
+  
+  //2-Ascii file with the Calibartion Parameters
+  /*FairParAsciiFileIo* parIo1 = new FairParAsciiFileIo();
+  parIo1->open("Ams_CalibParam.par","out");
+  rtdb->setOutput(parIo1);
+  rtdb->print();*/
+
+
+  /* Run -------------------------------------------------- */
+  run->Run((nev < 0) ? nev : 0, (nev < 0) ? 0 : nev);
+
+
+  /* Save parameters (if needed) -------------------------- */
+  rtdb->saveOutput();
+
+
+  /* Finish ----------------------------------------------- */
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  std::cout << std::endl << std::endl;
+  std::cout << "Macro finished succesfully." << std::endl;
+  std::cout << "Output file is " << outputFileName << std::endl;
+  std::cout << "Real time " << rtime << " s, CPU time " << ctime << " s"
+            << std::endl << std::endl;
+  gApplication->Terminate();
+}
---
+++ macros/r3b/unpack/ams/unpack_gsi2018_ams_mapped2cal.C
@@ -0,0 +1,88 @@
+/* Additional info:
+ * To generate the header file used for the R3BUcesbSource (ext_h101.h), use:
+ *
+ * $UCESB_DIR/upexps/FIXME --ntuple=UNPACK:TRIGGER,UNPACK:EVENTNO,RAW\
+ *     STRUCT_HH,ext_h101.h
+ *
+ * Put this header file into the 'r3bsource' directory and recompile.
+ * */
+
+ typedef struct EXT_STR_h101_t {//FIXME for ams
+   EXT_STR_h101_unpack_t unpack;
+   EXT_STR_h101_CALIFA_t califa;
+ } EXT_STR_h101;
+
+void unpack_gsi2018_ams_mapped2cal() {
+  TStopwatch timer;
+  timer.Start();
+
+  //const Int_t nev = -1; /* number of events to read, -1 - until CTRL+C */
+  const Int_t nev = -1; /* number of events to read, -1 - until CTRL+C */
+  
+  /* Create source using ucesb for input ------------------ */
+  
+  TString filename = "~/lmd/data_0003.lmd";
+  TString outputFileName = "data_0001.root";
+  
+  TString ntuple_options = "UNPACK:EVENTNO,UNPACK:TRIGGER,RAW";
+  TString ucesb_dir = getenv("UCESB_DIR");
+  
+  TString ucesb_path = ucesb_dir + "/../upexps/califaKrakow17/califa";//FIXME
+  
+  EXT_STR_h101 ucesb_struct;
+  
+  R3BUcesbSource* source = new R3BUcesbSource(filename, ntuple_options,
+					      ucesb_path, &ucesb_struct, sizeof(ucesb_struct));
+  source->SetMaxEvents(nev);
+  
+  source->AddReader(new R3BUnpackReader((EXT_STR_h101_unpack*)&ucesb_struct,
+					offsetof(EXT_STR_h101, unpack)));
+  source->AddReader(new R3BAmsReader((EXT_STR_h101_CALIFA*)&ucesb_struct.califa,
+					     offsetof(EXT_STR_h101, califa)));
+
+  
+  /* Create online run ------------------------------------ */
+  FairRunOnline* run = new FairRunOnline(source);
+  run->SetRunId(1513078509);
+  run->SetOutputFile(outputFileName);
+  
+  /* Runtime data base ------------------------------------ */
+  FairRuntimeDb* rtdb = run->GetRuntimeDb();
+
+  /* Add analysis task ------------------------------------ */  
+  R3BAmsMapped2StripCal* Map2Cal = new R3BAmsMapped2StripCal();
+  run->AddTask(Map2Cal);
+
+
+  FairParRootFileIo* parIo1 = new FairParRootFileIo();
+  parIo1->open("Ams_CalibParam.root","in");
+  rtdb->setFirstInput(parIo1);
+  rtdb->print();
+
+
+  /* Initialize ------------------------------------------- */
+  run->Init();
+  //    FairLogger::GetLogger()->SetLogScreenLevel("WARNING");
+  //    FairLogger::GetLogger()->SetLogScreenLevel("DEBUG");
+  FairLogger::GetLogger()->SetLogScreenLevel("INFO");
+
+
+  /* Run -------------------------------------------------- */
+  run->Run((nev < 0) ? nev : 0, (nev < 0) ? 0 : nev);
+
+
+  /* Save parameters (if needed) -------------------------- */
+  //rtdb->saveOutput();
+
+
+  /* Finish ----------------------------------------------- */
+  timer.Stop();
+  Double_t rtime = timer.RealTime();
+  Double_t ctime = timer.CpuTime();
+  std::cout << std::endl << std::endl;
+  std::cout << "Macro finished succesfully." << std::endl;
+  std::cout << "Output file is " << outputFileName << std::endl;
+  std::cout << "Real time " << rtime << " s, CPU time " << ctime << " s"
+            << std::endl << std::endl;
+  gApplication->Terminate();
+}
