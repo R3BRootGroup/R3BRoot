6b91f46605c827d48f387fe8fdeab96f0a84b3f3
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2021-11-02 15:59:35 +0100

    Added initialization of fNofLosDetectors to R3BOnlineSpectraLosVsSci2
    Added white rabbit for r3b-music

---
+++ r3bsource/R3BWhiterabbitMusicReader.cxx
@@ -0,0 +1,127 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+#include "FairLogger.h"
+#include "FairRootManager.h"
+
+#include "R3BEventHeader.h"
+#include "R3BWRData.h"
+#include "R3BWhiterabbitMusicReader.h"
+#include "TClonesArray.h"
+
+extern "C"
+{
+#include "ext_data_client.h"
+#include "ext_h101_wrmusic.h"
+}
+
+R3BWhiterabbitMusicReader::R3BWhiterabbitMusicReader(EXT_STR_h101_WRMUSIC* data, size_t offset, UInt_t whiterabbit_id)
+    : R3BReader("R3BWhiterabbitMusicReader")
+    , fNEvent(0)
+    , fData(data)
+    , fOffset(offset)
+    , fOnline(kFALSE)
+    , fWhiterabbitId(whiterabbit_id)
+    , fEventHeader(nullptr)
+    , fArray(new TClonesArray("R3BWRMasterData"))
+{
+}
+
+R3BWhiterabbitMusicReader::~R3BWhiterabbitMusicReader()
+{
+    if (fArray)
+        delete fArray;
+    if (fEventHeader)
+        delete fEventHeader;
+}
+
+Bool_t R3BWhiterabbitMusicReader::Init(ext_data_struct_info* a_struct_info)
+{
+    Int_t ok;
+    LOG(INFO) << "R3BWhiterabbitMusicReader::Init";
+    EXT_STR_h101_WRMUSIC_ITEMS_INFO(ok, *a_struct_info, fOffset, EXT_STR_h101_WRMUSIC, 0);
+
+    if (!ok)
+    {
+        LOG(ERROR) << "R3BWhiterabbitMusicReader::Failed to setup structure information.";
+        return kFALSE;
+    }
+
+    // Look for the R3BEventHeader
+    FairRootManager* frm = FairRootManager::Instance();
+    fEventHeader = (R3BEventHeader*)frm->GetObject("EventHeader.");
+    if (!fEventHeader)
+    {
+        LOG(WARNING) << "R3BWhiterabbitMusicReader::Init() EventHeader. not found";
+    }
+    else
+        LOG(INFO) << "R3BWhiterabbitMusicReader::Init() R3BEventHeader found";
+
+    // Register output array in tree
+    FairRootManager::Instance()->Register("WRMusicData", "WRMusic", fArray, !fOnline);
+
+    fArray->Clear();
+    fData->TIMESTAMP_MUSIC_ID = 0;
+
+    return kTRUE;
+}
+
+Bool_t R3BWhiterabbitMusicReader::Read()
+{
+    if (!fData->TIMESTAMP_MUSIC_ID)
+    {
+        return kTRUE;
+    }
+    /*printf("ams %08x %08x %08x %08x %08x \n",
+    fData->TIMESTAMP_MUSIC_ID,
+    fData->TIMESTAMP_MUSIC_WR_T4, fData->TIMESTAMP_MUSIC_WR_T3,
+    fData->TIMESTAMP_MUSIC_WR_T2, fData->TIMESTAMP_MUSIC_WR_T1);*/
+
+    if (fWhiterabbitId != fData->TIMESTAMP_MUSIC_ID)
+    {
+        char strMessage[1000];
+        snprintf(strMessage,
+                 sizeof strMessage,
+                 "Event %u: Whiterabbit ID mismatch: expected 0x%x, got 0x%x.\n",
+                 fEventHeader->GetEventno(),
+                 fWhiterabbitId,
+                 fData->TIMESTAMP_MUSIC_ID);
+        LOG(error) << strMessage;
+    }
+
+    if (fEventHeader != nullptr)
+    {
+        uint64_t timestamp = ((uint64_t)fData->TIMESTAMP_MUSIC_WR_T4 << 48) |
+                             ((uint64_t)fData->TIMESTAMP_MUSIC_WR_T3 << 32) |
+                             ((uint64_t)fData->TIMESTAMP_MUSIC_WR_T2 << 16) | (uint64_t)fData->TIMESTAMP_MUSIC_WR_T1;
+        // fEventHeader->SetTimeStamp(timestamp);
+        fNEvent = fEventHeader->GetEventno();
+        new ((*fArray)[fArray->GetEntriesFast()]) R3BWRData(timestamp);
+    }
+    else
+    {
+        fNEvent++;
+    }
+
+    fData->TIMESTAMP_MUSIC_ID = 0;
+    return kTRUE;
+}
+
+void R3BWhiterabbitMusicReader::Reset()
+{
+    // Reset the output array
+    fArray->Clear();
+    fNEvent = 0;
+}
+
+ClassImp(R3BWhiterabbitMusicReader);
---
+++ r3bsource/ext/ext_h101_wrmusic.h
@@ -0,0 +1,88 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+/********************************************************
+ *
+ * Structure for ext_data_fetch_event() filling.
+ *
+ * Do not edit - automatically generated.
+ */
+
+#ifndef __GUARD_H101_WRMUSIC_EXT_H101_WRMUSIC_H__
+#define __GUARD_H101_WRMUSIC_EXT_H101_WRMUSIC_H__
+
+#ifndef __CINT__
+# include <stdint.h>
+#else
+/* For CINT (old version trouble with stdint.h): */
+# ifndef uint32_t
+typedef unsigned int uint32_t;
+typedef          int  int32_t;
+# endif
+#endif
+#ifndef EXT_STRUCT_CTRL
+# define EXT_STRUCT_CTRL(x)
+#endif
+
+/********************************************************
+ *
+ * Plain structure (layout as ntuple/root file):
+ */
+
+typedef struct EXT_STR_h101_WRMUSIC_t
+{
+  uint32_t TIMESTAMP_MUSIC_ID /* [0,65535] */;
+  uint32_t TIMESTAMP_MUSIC_WR_T1 /* [0,65535] */;
+  uint32_t TIMESTAMP_MUSIC_WR_T2 /* [0,65535] */;
+  uint32_t TIMESTAMP_MUSIC_WR_T3 /* [0,65535] */;
+  uint32_t TIMESTAMP_MUSIC_WR_T4 /* [0,65535] */;
+
+} EXT_STR_h101_WRMUSIC;
+
+/********************************************************
+ *
+ * Structure with multiple levels of arrays (partially)
+ * recovered (recommended):
+ */
+
+typedef struct EXT_STR_h101_WRMUSIC_onion_t
+{
+  uint32_t TIMESTAMP_MUSIC_ID;
+  uint32_t TIMESTAMP_MUSIC_WR_T[4];
+
+} EXT_STR_h101_WRMUSIC_onion;
+
+/*******************************************************/
+
+#define EXT_STR_h101_WRMUSIC_ITEMS_INFO(ok,si,offset,struct_t,printerr) do { \
+  ok = 1; \
+  EXT_STR_ITEM_INFO_LIM(ok,si,offset,struct_t,printerr,\
+                     TIMESTAMP_MUSIC_ID,              UINT32,\
+                    "TIMESTAMP_MUSIC_ID",65535); \
+  EXT_STR_ITEM_INFO_LIM(ok,si,offset,struct_t,printerr,\
+                     TIMESTAMP_MUSIC_WR_T1,           UINT32,\
+                    "TIMESTAMP_MUSIC_WR_T1",65535); \
+  EXT_STR_ITEM_INFO_LIM(ok,si,offset,struct_t,printerr,\
+                     TIMESTAMP_MUSIC_WR_T2,           UINT32,\
+                    "TIMESTAMP_MUSIC_WR_T2",65535); \
+  EXT_STR_ITEM_INFO_LIM(ok,si,offset,struct_t,printerr,\
+                     TIMESTAMP_MUSIC_WR_T3,           UINT32,\
+                    "TIMESTAMP_MUSIC_WR_T3",65535); \
+  EXT_STR_ITEM_INFO_LIM(ok,si,offset,struct_t,printerr,\
+                     TIMESTAMP_MUSIC_WR_T4,           UINT32,\
+                    "TIMESTAMP_MUSIC_WR_T4",65535); \
+  \
+} while (0);
+#endif/*__GUARD_H101_WRMUSIC_EXT_H101_WRMUSIC_H__*/
+
+/*******************************************************/
