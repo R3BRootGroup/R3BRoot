31b0bbad328e94c56809559064f34222238c646b
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2022-01-24 00:19:31 +0100

    Added R3BTsplinePar class to manage tspline parameters

---
+++ r3bbase/R3BTsplinePar.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+#ifndef R3BTSPLINEPAR_H
+#define R3BTSPLINEPAR_H
+
+#include "TObject.h"
+#include "TSpline3.h"
+#include <Rtypes.h>
+
+class FairParamList;
+
+class R3BTsplinePar : public TObject
+{
+  public:
+    /** Default constructor **/
+    R3BTsplinePar(const TString& name = "Spline");
+
+    /** Destructor **/
+    virtual ~R3BTsplinePar() {}
+
+    /** Method to print values of parameters **/
+    void print();
+
+    /** Method to store all parameters using FairRuntimeDB **/
+    virtual void putParams(FairParamList* list);
+
+    /** Method to retrieve all parameters using FairRuntimeDB**/
+    TSpline3* getParams(FairParamList* list);
+
+    /** Accessor functions **/
+    TString& GetName() { return fSplineName; }
+    TSpline3* GetSpline() { return fSpline; }
+
+    void SetName(TString& n) { fSplineName = n; }
+    void SetMaxPoints(UInt_t p) { fMaxPoints = p; }
+    void SetSpline(TSpline3* c) { fSpline = c; }
+
+  private:
+    UInt_t fMaxPoints;
+    TSpline3* fSpline;
+    TString fSplineName;
+
+  public:
+    ClassDef(R3BTsplinePar, 0);
+};
+
+#endif // R3BTSPLINEPAR_H
