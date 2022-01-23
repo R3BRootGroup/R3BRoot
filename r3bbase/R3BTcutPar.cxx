9a86b31531c42d235b3e5c6c9b28dc5c2fe90092
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2022-01-23 21:13:36 +0100

    Added online classes for Rpc and a class R3BTcutPar to manage the tcut parameters
    Removed unused FairParamList in R3BTcutPar

---
+++ r3bbase/R3BTcutPar.cxx
@@ -0,0 +1,93 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+// --------------------------------------------------------------
+// -----                   R3BTcutPar                       -----
+// -----   Created Jan 23th 2022 by J.L. Rodriguez-Sanchez  -----
+// --------------------------------------------------------------
+
+#include "R3BTcutPar.h"
+#include "R3BLogger.h"
+
+#include "FairLogger.h"
+#include "FairParamList.h"
+
+#include "TArrayF.h"
+#include "TMath.h"
+
+// -----   Default constructor   -----------------------------------------------
+R3BTcutPar::R3BTcutPar(const TString& cutname)
+    : fCutName(cutname)
+    , fMaxPoints(100)
+{
+    fCut = new TCutG(fCutName, 1);
+}
+
+// ----  Method putParams ------------------------------------------------------
+void R3BTcutPar::putParams(FairParamList* list)
+{
+    R3BLOG(INFO, "called for " << fCut->GetN() << " points");
+
+    TArrayF* p = new TArrayF(2);
+
+    for (int n = 0; n < fCut->GetN(); n++)
+    {
+        Double_t a[2];
+        fCut->GetPoint(n, a[0], a[1]);
+
+        TString name = fCutName + "Point" + n;
+        p->AddAt(a[0], 0);
+        p->AddAt(a[1], 1);
+        list->add(name, *p);
+    }
+}
+
+// ----  Method getParams ------------------------------------------------------
+TCutG* R3BTcutPar::getParams(FairParamList* list)
+{
+    R3BLOG(INFO, "called");
+
+    TArrayF* p = new TArrayF(2);
+    for (int n = 0; n < fMaxPoints; n++)
+    {
+        TString name = fCutName + "Point" + n;
+        if (list->fill(name, p))
+        {
+            fCut->SetPoint(n, p->GetAt(0), p->GetAt(1));
+        }
+    }
+    return fCut;
+}
+
+// ----  Method print ----------------------------------------------------------
+void R3BTcutPar::print()
+{
+    if (fCut)
+    {
+        R3BLOG(INFO, "for " << fCut->GetName());
+
+        TArrayF* p = new TArrayF(2);
+        for (int n = 0; n < fCut->GetN(); n++)
+        {
+            Double_t a[2];
+            fCut->GetPoint(n, a[0], a[1]);
+            LOG(INFO) << "CutPoint" << n << ": " << a[0] << " ; " << a[1];
+        }
+    }
+    else
+    {
+        R3BLOG(WARNING, "Cut parameters not found");
+    }
+}
+
+ClassImp(R3BTcutPar);
---
+++ r3bbase/R3BTcutPar.h
@@ -0,0 +1,58 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+#ifndef R3BTCUTPAR_H
+#define R3BTCUTPAR_H
+
+#include "TCutG.h"
+#include "TObject.h"
+#include <Rtypes.h>
+
+class FairParamList;
+
+class R3BTcutPar : public TObject
+{
+  public:
+    /** Default constructor **/
+    R3BTcutPar(const TString& cutname = "Cut");
+
+    /** Destructor **/
+    virtual ~R3BTcutPar() {}
+
+    /** Method to print values of parameters **/
+    void print();
+
+    /** Method to store all parameters using FairRuntimeDB **/
+    virtual void putParams(FairParamList* list);
+
+    /** Method to retrieve all parameters using FairRuntimeDB**/
+    TCutG* getParams(FairParamList* list);
+
+    /** Accessor functions **/
+    TString& GetName() { return fCutName; }
+    TCutG* GetCut() { return fCut; }
+
+    void SetName(TString& n) { fCutName = n; }
+    void SetMaxPoints(UInt_t p) { fMaxPoints = p; }
+    void SetCut(TCutG* c) { fCut = c; }
+
+  private:
+    UInt_t fMaxPoints;
+    TCutG* fCut;
+    TString fCutName;
+
+  public:
+    ClassDef(R3BTcutPar, 0);
+};
+
+#endif // R3BTCUTPAR_H
