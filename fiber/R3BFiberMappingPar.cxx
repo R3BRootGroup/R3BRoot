37ff064bee63e0cc3d9c3acacc5113a3db6ad2d0
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2022-04-21 10:20:51 +0200

    Adding R3BFiberMappingPar class to manage the mapping parameters for trigger times

---
+++ fiber/R3BFiberMappingPar.cxx
@@ -0,0 +1,154 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+// -------------------------------------------------------------
+// -----        R3BFiberMappingPar source file             -----
+// -----    Created 18/03/22 by J.L. Rodriguez-Sanchez     -----
+// -------------------------------------------------------------
+
+#include "R3BFiberMappingPar.h"
+
+#include "FairParamList.h"
+#include "R3BLogger.h"
+
+#include "TMath.h"
+#include "TString.h"
+
+// ---- Standard Constructor ---------------------------------------------------
+R3BFiberMappingPar::R3BFiberMappingPar(const TString& name, const TString& title, const TString& context)
+    : FairParGenericSet(name, title, context)
+    , fNumPlanes(4)
+    , fNumPaddles(44)
+    , fNumPmts(2)
+{
+    for (Int_t p = 0; p < fNumPmts; p++)
+    {
+        fTrigmap[p].resize(fNumPlanes);
+    }
+    for (Int_t plane = 0; plane < fNumPlanes; plane++)
+        for (Int_t p = 0; p < fNumPmts; p++)
+        {
+            fTrigmap[p][plane] = new TArrayI(fNumPaddles);
+            for (Int_t paddle = 0; paddle < fNumPaddles; paddle++)
+                fTrigmap[p][plane]->AddAt(0, paddle);
+        }
+}
+
+// ----  Destructor ------------------------------------------------------------
+R3BFiberMappingPar::~R3BFiberMappingPar()
+{
+    clear();
+    for (Int_t plane = 0; plane < fNumPlanes; plane++)
+        for (Int_t p = 0; p < fNumPmts; p++)
+        {
+            if (fTrigmap[p][plane])
+                delete fTrigmap[p][plane];
+        }
+}
+
+// ----  Method clear ----------------------------------------------------------
+void R3BFiberMappingPar::clear()
+{
+    status = kFALSE;
+    resetInputVersions();
+}
+
+// ----  Method putParams ------------------------------------------------------
+void R3BFiberMappingPar::putParams(FairParamList* list)
+{
+    R3BLOG(INFO, "called");
+    if (!list)
+    {
+        R3BLOG(FATAL, "FairParamList not found");
+        return;
+    }
+    list->add("tofdPlanesPar", fNumPlanes);
+    list->add("tofdPaddlesPar", fNumPaddles);
+
+    R3BLOG(INFO, "Nb of planes: " << fNumPlanes);
+    R3BLOG(INFO, "Nb of paddles: " << fNumPaddles);
+
+    for (Int_t p = 0; p < fNumPmts; p++)
+    {
+        fTrigmap[p].resize(fNumPlanes);
+    }
+    char name[300];
+    for (Int_t plane = 0; plane < fNumPlanes; plane++)
+        for (Int_t p = 0; p < fNumPmts; p++)
+        {
+            fTrigmap[p][plane]->Set(fNumPaddles);
+            sprintf(name, "tofdplane%dPmt%dPar", plane + 1, p + 1);
+            list->add(name, *fTrigmap[p][plane]);
+        }
+}
+
+// ----  Method getParams ------------------------------------------------------
+Bool_t R3BFiberMappingPar::getParams(FairParamList* list)
+{
+    R3BLOG(INFO, "called");
+    if (!list)
+    {
+        R3BLOG(FATAL, "FairParamList not found");
+        return kFALSE;
+    }
+    if (!list->fill("tofdPlanesPar", &fNumPlanes))
+    {
+        R3BLOG(INFO, "Could not initialize tofdPlanesPar");
+        return kFALSE;
+    }
+    if (!list->fill("tofdPaddlesPar", &fNumPaddles))
+    {
+        R3BLOG(INFO, "Could not initialize tofdPaddlesPar");
+        return kFALSE;
+    }
+
+    for (Int_t p = 0; p < fNumPmts; p++)
+    {
+        fTrigmap[p].resize(fNumPlanes);
+    }
+    char name[300];
+    for (Int_t plane = 0; plane < fNumPlanes; plane++)
+        for (Int_t p = 0; p < fNumPmts; p++)
+        {
+            fTrigmap[p][plane]->Set(fNumPaddles);
+            sprintf(name, "tofdplane%dPmt%dPar", plane + 1, p + 1);
+
+            if (!(list->fill(name, fTrigmap[p][plane])))
+            {
+                R3BLOG(ERROR, "Could not initialize " << name);
+                return kFALSE;
+            }
+        }
+
+    return kTRUE;
+}
+
+// ----  Method print ----------------------------------------------------------
+void R3BFiberMappingPar::print() { printParams(); }
+
+// ----  Method printParams ----------------------------------------------------
+void R3BFiberMappingPar::printParams()
+{
+    R3BLOG(INFO, "Mapping params for TofD: Num of planes: " << fNumPlanes << " and paddles: " << fNumPaddles);
+
+    for (Int_t plane = 0; plane < fNumPlanes; plane++)
+        for (Int_t p = 0; p < fNumPmts; p++)
+            for (Int_t paddle = 0; paddle < fNumPaddles; paddle++)
+            {
+                R3BLOG(INFO,
+                       "Plane: " << plane + 1 << ", pmt: " << p + 1 << ", paddle: " << paddle + 1
+                                 << ", value: " << fTrigmap[p][plane]->GetAt(paddle));
+            }
+}
+
+ClassImp(R3BFiberMappingPar);
