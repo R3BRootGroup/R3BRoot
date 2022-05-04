e08d4f45dca64c5287737c304e69f9f9c973cafc
Audrey Chatillon <audrey.chatillon@gmail.com>
2022-05-04 22:45:16 +0200

    Music LIGHT IONS : Mapped 2 Cal step
    R3BMusliContFact class
    
    fix a typo in method SetPosParams of R3BTwimCalPar
    
    fix typo in getParams in R3BTwimCalPar class
    
    R3BMusliMapped2Cal class

---
+++ musli/calibration/R3BMusliMapped2Cal.h
@@ -0,0 +1,92 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+#ifndef R3BMusliMapped2Cal_H
+#define R3BMusliMapped2Cal_H
+
+#include "TArrayF.h"
+
+#include "FairTask.h"
+#include "R3BMusliCalData.h"
+#include "R3BMusliMappedData.h"
+
+#define MAX_MULT_MUSLI 100
+#define MAX_NB_SIGNALS_MAP 18
+
+class TClonesArray;
+class R3BMusliCalPar;
+
+class R3BMusliMapped2Cal : public FairTask
+{
+
+  public:
+    /** Default constructor **/
+    R3BMusliMapped2Cal();
+
+    /** Standard constructor **/
+    R3BMusliMapped2Cal(const char* name, Int_t iVerbose = 1);
+
+    /** Destructor **/
+    virtual ~R3BMusliMapped2Cal();
+
+    /** Virtual method Exec **/
+    virtual void Exec(Option_t* option);
+
+    /** Virtual method Reset **/
+    virtual void Reset();
+
+    virtual void SetParContainers();
+
+    // Fair specific
+    /** Virtual method Init **/
+    virtual InitStatus Init();
+
+    /** Virtual method ReInit **/
+    virtual InitStatus ReInit();
+
+    /** Accessor to select online mode **/
+    void SetOnline(Bool_t option) { fOnline = option; }
+
+  private:
+    void SetParameters();
+
+    Int_t mult_signalmap[MAX_NB_SIGNALS_MAP];
+    UInt_t type[MAX_MULT_MUSLI][MAX_NB_SIGNALS_MAP];
+    UInt_t index[MAX_MULT_MUSLI][MAX_NB_SIGNALS_MAP];
+    Double_t energy[MAX_MULT_MUSLI][MAX_NB_SIGNALS_MAP];
+    Double_t time[MAX_MULT_MUSLI][MAX_NB_SIGNALS_MAP];
+
+    Int_t fNumSignals;
+    Int_t fNumGroupsAnodes;
+    Int_t fNumParamsEneFit;
+    Int_t fNumParamsPosFit;
+    Int_t fMaxMult;
+    TArrayF* fEneCalParams;
+    TArrayF* fPosCalParams;
+
+    R3BMusliCalPar* fCal_Par;         /**< Parameter container. > */
+    TClonesArray* fMusliMappedDataCA; /**< Array with Musli Mapped-input data. >*/
+    TClonesArray* fMusliCalDataCA;    /**< Array with Musli Cal-output data. >*/
+
+    Bool_t fOnline; // Don't store data for online
+
+    /** Private method AddCalData **/
+    //** Adds a MusliCalData to the anodeCalCollection
+    R3BMusliCalData* AddCalData(UInt_t nb_anodes, UInt_t index, Double_t dt, Double_t e);
+
+  public:
+    // Class definition
+    ClassDef(R3BMusliMapped2Cal, 1)
+};
+
+#endif
---
+++ musli/pars/R3BMusliContFact.cxx
@@ -0,0 +1,89 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019 Members of R3B Collaboration                          *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+//
+//  R3BMusliContFact
+//
+//  Factory for the parameter containers in libR3BMusli
+//
+
+#include "R3BMusliContFact.h"
+
+#include "FairLogger.h"
+#include "FairParAsciiFileIo.h"
+#include "FairParRootFileIo.h"
+#include "FairRuntimeDb.h"
+
+#include "R3BMusliCalPar.h"
+//#include "R3BMusliHitPar.h"
+//#include "R3BTGeoPar.h"
+
+#include "TClass.h"
+
+static R3BMusliContFact gR3BMusliContFact;
+
+R3BMusliContFact::R3BMusliContFact()
+{
+    // Constructor (called when the library is loaded)
+    fName = "R3BMusliContFact";
+    fTitle = "Factory for parameter containers in libR3BMusli";
+    setAllContainers();
+    FairRuntimeDb::instance()->addContFactory(this);
+}
+
+void R3BMusliContFact::setAllContainers()
+{
+    // Creates the Container objects with all accepted contexts and adds them to
+    // the list of containers for the STS library.
+
+    FairContainer* p1 = new FairContainer("musliCalPar", "MUSLI Cal Parameters", "MusliCalParContext");
+    p1->addContext("MusliCalParContext");
+
+    containers->Add(p1);
+
+    // FairContainer* p2 = new FairContainer("musliHitPar", "MUSLI Hit Parameters", "MusliHitParContext");
+    // p2->addContext("MusliHitParContext");
+
+    // containers->Add(p2);
+
+    // FairContainer* p3 = new FairContainer("MusliGeoPar", "Musli geometry parameters", "GeometryParameterContext");
+    // p3->addContext("GeometryParameterContext");
+
+    // containers->Add(p3);
+}
+
+FairParSet* R3BMusliContFact::createContainer(FairContainer* c)
+{
+    // Trals the constructor of the corresponding parameter container.
+    // For an actual context, which is not an empty string and not the default context
+    // of this container, the name is concatinated with the context.
+
+    const char* name = c->GetName();
+    LOG(INFO) << "R3BMusliContFact: Create container name: " << name;
+    FairParSet* p = 0;
+    if (strcmp(name, "musliCalPar") == 0)
+    {
+        p = new R3BMusliCalPar(c->getConcatName().Data(), c->GetTitle(), c->getContext());
+    }
+    // else if (strcmp(name, "musliHitPar") == 0)
+    //{
+    //    p = new R3BMusliHitPar(c->getConcatName().Data(), c->GetTitle(), c->getContext());
+    //}
+    // else if (strcmp(name, "MusliGeoPar") == 0)
+    //{
+    //    p = new R3BTGeoPar(c->getConcatName().Data(), c->GetTitle(), c->getContext());
+    //}
+    return p;
+}
+
+ClassImp(R3BMusliContFact);
