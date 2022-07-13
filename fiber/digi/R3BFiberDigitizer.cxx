25af368d92c118f304b0b0c1f1d5b838b61f873a
MartinaFeijoo <martina.feijoo.fontan@usc.es>
2022-07-13 17:56:50 +0200

    delete old exec
    clang format

--- fiber/digi/R3BFiberDigitizer.cxx
+++ fiber/digi/R3BFiberDigitizer.cxx
@@ -19,9 +19,11 @@
 #include "R3BFiberDigitizer.h"
 #include "FairRootManager.h"
 #include "FairRuntimeDb.h"
+#include "R3BFibPoint.h"
 #include "R3BLogger.h"
-#include "TClonesArray.h"
+#include "R3BMCTrack.h"
 #include "R3BTGeoPar.h"
+#include "TClonesArray.h"
 #include "TH1F.h"
 #include "TH2F.h"
 #include "TMath.h"
@@ -29,8 +31,6 @@
 #include "TVector3.h"
 #include <iostream>
 #include <string>
-#include "R3BFibPoint.h"
-#include "R3BMCTrack.h"
 
 R3BFiberDigitizer::R3BFiberDigitizer(const TString& name)
     : FairTask("R3B" + name + "Digitizer")
@@ -77,26 +77,27 @@ void R3BFiberDigitizer::SetYPositionResolution(Double_t y) { ysigma = y; }
 void R3BFiberDigitizer::SetParContainers()
 {
     FairRuntimeDb* rtdb = FairRuntimeDb::instance();
-    fFiGeoPar = (R3BTGeoPar*)rtdb->getContainer(fName+"GeoPar");
+    fFiGeoPar = (R3BTGeoPar*)rtdb->getContainer(fName + "GeoPar");
     if (!fFiGeoPar)
     {
-        LOG(ERROR) << "R3BFiberDigitizer::SetParContainers() : Could not get access to "+fName+"GeoPar container.";
+        LOG(ERROR) << "R3BFiberDigitizer::SetParContainers() : Could not get access to " + fName + "GeoPar container.";
         return;
     }
     else
-        LOG(INFO) << "R3BFiberDigitizer::SetParContainers() : Container "+fName+"GeoPar found.";
+        LOG(INFO) << "R3BFiberDigitizer::SetParContainers() : Container " + fName + "GeoPar found.";
 }
 
 void R3BFiberDigitizer::SetParameter()
 {
-    if (fFiGeoPar){
-      ysigma = fFiGeoPar->GetSigmaY();
-      xsigma = fFiGeoPar->GetSigmaX();
-      fRot.RotateX(-fFiGeoPar->GetRotX() * TMath::DegToRad());
-      fRot.RotateY(-fFiGeoPar->GetRotY() * TMath::DegToRad());
-      fRot.RotateZ(-fFiGeoPar->GetRotZ() * TMath::DegToRad());
+    if (fFiGeoPar)
+    {
+        ysigma = fFiGeoPar->GetSigmaY();
+        xsigma = fFiGeoPar->GetSigmaX();
+        fRot.RotateX(-fFiGeoPar->GetRotX() * TMath::DegToRad());
+        fRot.RotateY(-fFiGeoPar->GetRotY() * TMath::DegToRad());
+        fRot.RotateZ(-fFiGeoPar->GetRotZ() * TMath::DegToRad());
 
-      fTrans.SetXYZ(fFiGeoPar->GetPosX(), fFiGeoPar->GetPosY(), fFiGeoPar->GetPosZ());
+        fTrans.SetXYZ(fFiGeoPar->GetPosX(), fFiGeoPar->GetPosY(), fFiGeoPar->GetPosZ());
     }
 }
 
@@ -125,128 +126,6 @@ InitStatus R3BFiberDigitizer::Init()
 
 void R3BFiberDigitizer::Exec(Option_t* opt)
 {
-    /*Reset();
-
-    auto Digitize = [this](TClonesArray* Points, TClonesArray* Hits, Int_t NumOfFibers) {
-        Int_t entryNum = Points->GetEntries();
-
-        if (!entryNum)
-            return;
-
-        // creating the storage for energy and time for each PMT
-        std::vector<Double_t>* energy = new std::vector<Double_t>[NumOfFibers];
-        std::vector<Double_t>* time = new std::vector<Double_t>[NumOfFibers];
-        std::vector<Double_t>* y = new std::vector<Double_t>[NumOfFibers];
-
-        for (Int_t i = 0; i < NumOfFibers; ++i)
-        {
-            time[i].push_back(99999);
-            energy[i].push_back(0);
-            y[i].push_back(0);
-        }
-
-        struct TempHit
-        {
-            Int_t fiberID;
-            Double_t Energy;
-            Double_t Time;
-            Double_t Y;
-
-            TempHit(Int_t id, Double_t e, Double_t t, Double_t y)
-                : fiberID(id)
-                , Energy(e)
-                , Time(t)
-                , Y(y)
-            {
-            }
-        };
-
-        // ordering the hits in time
-
-        std::vector<TempHit> TempHits;
-
-        for (Int_t i = 0; i < entryNum; ++i)
-        {
-            R3BFibPoint* data_element = (R3BFibPoint*)Points->At(i);
-
-            TempHits.push_back(TempHit(data_element->GetDetectorID(),
-                                       data_element->GetEnergyLoss(),
-                                       data_element->GetTime(),
-                                       data_element->GetYIn()));
-        }
-
-        std::sort(TempHits.begin(), TempHits.end(), [](const TempHit& lhs, const TempHit& rhs) {
-            return lhs.Time < rhs.Time;
-        });
-
-        // registering the hits in the PMTs
-
-        for (TempHit& Hit : TempHits)
-        {
-            if (Hit.Energy < 0.0000001)
-            {
-                continue;
-            }
-
-            Int_t fiberID = Hit.fiberID;
-
-            if (Hit.Time - time[fiberID].back() < 30)
-            {
-
-                energy[fiberID].back() += Hit.Energy;
-
-                y[fiberID].back() = (time[fiberID].back() > Hit.Time) ? Hit.Y : y[fiberID].back();
-
-                time[fiberID].back() = (time[fiberID].back() > Hit.Time) ? Hit.Time : time[fiberID].back();
-            }
-            else
-            {
-                energy[fiberID].push_back(Hit.Energy);
-                time[fiberID].push_back(Hit.Time);
-                y[fiberID].push_back(Hit.Y);
-            }
-        }
-
-        // creating the final hits
-
-        for (Int_t i = 0; i < NumOfFibers; ++i)
-        {
-            for (Double_t& energyl : energy[i])
-            {
-                if (energyl > 0.0000001)
-                {
-                    Float_t xpos =
-                        -detector_width / 2. + fiber_thickness / 2. + (i + (i * air_layer)) * fiber_thickness;
-                    // cout << "Test: Det = 30" << " x = " << xpos << " fiber = " << i << endl;
-
-                    new ((*Hits)[Hits->GetEntries()])
-                        R3BBunchedFiberHitData(1,
-                                               xpos,
-                                               prnd->Gaus((y[i].at(&energyl - energy[i].data())), ysigma),
-                                               prnd->Gaus(energyl, esigma),
-                                               prnd->Gaus(time[i].at(&energyl - energy[i].data()), tsigma),
-                                               i,
-                                               0.,
-                                               0.,
-                                               0.,
-                                               0.);
-                }
-            }
-        }
-
-        delete[] energy;
-        delete[] time;
-        delete[] y;
-    };
-
-    // running the digitizer for the Fi detectors
-
-    if (fFiPoints)
-    {
-        Digitize(fFiPoints, fFiHits, fiber_nbr);
-    }
-    */
-
     Reset();
     // Reading the Input -- Point Data --
     Int_t nHits = fFiPoints->GetEntriesFast();
@@ -259,8 +138,10 @@ void R3BFiberDigitizer::Exec(Option_t* opt)
     Int_t TrackId = 0, PID = 0, mother = -1;
     Double_t x = 0., y = 0., z = 0., time = 0.;
     TVector3 vpos;
+
     for (Int_t i = 0; i < nHits; i++)
     {
+
         pointData[i] = (R3BFibPoint*)(fFiPoints->At(i));
         TrackId = pointData[i]->GetTrackID();
 
@@ -270,25 +151,24 @@ void R3BFiberDigitizer::Exec(Option_t* opt)
 
         if (PID > 1000401000) // Z=40 and A=100
         {
-            x = (pointData[i]->GetXIn() + pointData[i]->GetXOut())/2.;
-            y = (pointData[i]->GetYIn() + pointData[i]->GetYOut())/2.;
-            z = (pointData[i]->GetZIn() + pointData[i]->GetZOut())/2.;
+            x = (pointData[i]->GetXIn() + pointData[i]->GetXOut()) / 2.;
+            y = (pointData[i]->GetYIn() + pointData[i]->GetYOut()) / 2.;
+            z = (pointData[i]->GetZIn() + pointData[i]->GetZOut()) / 2.;
             vpos.SetXYZ(x, y, z);
 
             vpos = fRot * (vpos - fTrans);
-            //vpos = fRot * (vpos);
-            //time = pointData[i]->GetTime() + rand->Gaus(0., fsigma_t);
+            // vpos = fRot * (vpos);
+            // time = pointData[i]->GetTime() + rand->Gaus(0., fsigma_t);
 
-            fiber = (int) std::round(vpos.X() / fiber_thickness);
+            fiber = (int)std::round(vpos.X() / fiber_thickness);
             // Add hit data
-            AddHitData(1, vpos.X()*10.+rand->Gaus(0., 0.1), vpos.Y()*10.+rand->Gaus(0., 0.1),
-                       1, 1, fiber, 1,1,1,1);
+            AddHitData(
+                1, vpos.X() * 10. + rand->Gaus(0., 0.1), vpos.Y() * 10. + rand->Gaus(0., 0.1), 1, 1, fiber, 1, 1, 1, 1);
         }
     }
     if (pointData)
         delete[] pointData;
     return;
-
 }
 
 // -----   Public method ReInit   ----------------------------------------------
@@ -302,6 +182,25 @@ void R3BFiberDigitizer::Reset()
 {
     if (fFiHits)
         fFiHits->Clear();
+}
+
+// -----   Private method AddR3BHitData  -------------------------------------------
+R3BBunchedFiberHitData* R3BFiberDigitizer::AddHitData(Int_t detId,
+                                                      Double_t x,
+                                                      Double_t y,
+                                                      Double_t eloss,
+                                                      Double_t time,
+                                                      Int_t a_fiber_id,
+                                                      Double_t a_mapmt_time_ns,
+                                                      Double_t a_spmt_time_ns,
+                                                      Double_t a_mapmt_tot_ns,
+                                                      Double_t a_spmt_tot_ns)
+{
+    // It fills the R3BBunchedFiberHitData
+    TClonesArray& clref = *fFiHits;
+    Int_t size = clref.GetEntriesFast();
+    return new (clref[size]) R3BBunchedFiberHitData(
+        detId, x, y, eloss, time, a_fiber_id, a_mapmt_time_ns, a_spmt_time_ns, a_mapmt_tot_ns, a_spmt_tot_ns);
 }
 
 ClassImp(R3BFiberDigitizer);
