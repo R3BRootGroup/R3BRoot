f64690efc8a3da3b88f95e08e4d05c07f5bc034a
jose-luis-rs <joseluis.rodriguez.sanchez@usc.es>
2022-11-07 19:53:48 +0100

    Adding ASYEOS framework

--- r3bdata/R3BTrack.h
+++ r3bdata/R3BTrack.h
@@ -25,7 +25,17 @@ class R3BTrack : public TObject
 {
   public:
     R3BTrack();
-    R3BTrack(Double_t x, Double_t y, Double_t z, Double_t px, Double_t py, Double_t pz, Int_t q, Int_t AoZ, Double_t chix, Double_t chiy, Int_t quality);
+    R3BTrack(Double_t x,
+             Double_t y,
+             Double_t z,
+             Double_t px,
+             Double_t py,
+             Double_t pz,
+             Int_t q,
+             Int_t AoZ,
+             Double_t chix,
+             Double_t chiy,
+             Int_t quality);
     virtual ~R3BTrack();
 
     inline const Double_t& GetX() const { return fX; }
@@ -35,7 +45,7 @@ class R3BTrack : public TObject
     inline const Double_t& GetPy() const { return fPy; }
     inline const Double_t& GetPz() const { return fPz; }
     inline const Double_t& GetQ() const { return fQ; }
-    inline const Double_t& GetAoZ() const { return fAoZ; }    
+    inline const Double_t& GetAoZ() const { return fAoZ; }
     inline const Double_t& GetChix() const { return fChix; }
     inline const Double_t& GetChiy() const { return fChiy; }
     inline const Int_t& GetQuality() const { return fQuality; }
@@ -48,9 +58,9 @@ class R3BTrack : public TObject
     Double_t fPy;
     Double_t fPz;
     Double_t fQ;
-	Double_t fAoZ;
+    Double_t fAoZ;
     Double_t fChix;
-    Double_t fChiy;    
+    Double_t fChiy;
     Int_t fQuality;
 
   public:
