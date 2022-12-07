ea047baea6c9f10b707b2458c997a3d23d54e348
Philipp Klenze <pklenze@ph.tum.de>
2022-12-07 20:17:23 +0100

    Fixed warnings found with gcc-10. Enabled Werror for builds. Applied clang-format Added a python script to display the relevant lines from the log. Fixed message.

--- califa/calibration/R3BCalifaMapped2CrystalCal.cxx
+++ califa/calibration/R3BCalifaMapped2CrystalCal.cxx
@@ -10,7 +10,6 @@
  * granted to it by virtue of its status as an Intergovernmental Organization *
  * or submit itself to any jurisdiction.                                      *
  ******************************************************************************/
-
 #include "TClonesArray.h"
 #include "TMath.h"
 #include "TRandom.h"
