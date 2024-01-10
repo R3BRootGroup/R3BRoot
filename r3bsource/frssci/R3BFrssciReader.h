d85271dd312deed09900670561e30282c343e8b2
Audrey Chatillon <audrey.chatillon@gmail.com>
2024-01-10 17:25:34 +0100

    Create FrssciMappedData Create r3BFrssciReader clang-format frssci files
    clang-format-15 -i for frssci files
    
    debug tf / tc mismatch
    
    remove auto item from the reader
    
    Add online spectra for Frssci at MAPPED LEVEL
    
    coherent type of variables
    
    cosmetics
    
    clang
    
    cosmetics
    
    cosmetics
    
    fix bug
    
    comment fh2_multMap_RvsL[i]->Fill( to try to pass the ckang-tidy test
    
    try to fix the clang-tidy requirement

---
+++ r3bsource/frssci/R3BFrssciReader.h
@@ -0,0 +1,51 @@
+#ifndef R3BFRSSCIREADER_H
+#define R3BFRSSCIREADER_H
+
+#include "R3BReader.h"
+#include "TClonesArray.h"
+
+#include <Rtypes.h>
+
+struct EXT_STR_h101_FRSSCI_t;
+typedef struct EXT_STR_h101_FRSSCI_t EXT_STR_h101_FRSSCI;
+
+class R3BFrssciReader : public R3BReader
+{
+  public:
+    // Standard constructor
+    R3BFrssciReader(EXT_STR_h101_FRSSCI*, size_t);
+    R3BFrssciReader(EXT_STR_h101_FRSSCI*, size_t, UShort_t);
+
+    // Destructor
+    virtual ~R3BFrssciReader();
+
+    // Setup structure information
+    virtual Bool_t Init(ext_data_struct_info*) override;
+
+    // Read data from full event structure
+    virtual Bool_t R3BRead() override;
+
+    // Reset
+    virtual void Reset() override;
+
+    // Accessor to select online mode
+    void SetOnline(Bool_t option) { fOnline = option; }
+
+  private:
+    // Reader specific data structure from ucesb
+    EXT_STR_h101_FRSSCI* fData;
+    // Data offset
+    size_t fOffset;
+    // Don't store data for online
+    Bool_t fOnline;
+    // Output array of type R3BFrssciMapped
+    TClonesArray* fArray;
+
+    UInt_t fNumEntries;
+    UShort_t fNumSci;
+
+  public:
+    ClassDefOverride(R3BFrssciReader, 0);
+};
+
+#endif // R3BFRSSCIREADER_H
