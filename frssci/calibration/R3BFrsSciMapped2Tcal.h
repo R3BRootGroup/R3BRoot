fc13e434527f71e60676c0b27f516902c21d90e6
Audrey Chatillon <audrey.chatillon@gmail.com>
2024-01-20 10:51:43 +0100

    add FrsSci task to fill Tcal level
    debug a copy/paste bug

---
+++ frssci/calibration/R3BFrsSciMapped2Tcal.h
@@ -0,0 +1,72 @@
+/******************************************************************************
+ *   Copyright (C) 2019 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
+ *   Copyright (C) 2019-2023 Members of R3B Collaboration                     *
+ *                                                                            *
+ *             This software is distributed under the terms of the            *
+ *                 GNU General Public Licence (GPL) version 3,                *
+ *                    copied verbatim in the file "LICENSE".                  *
+ *                                                                            *
+ * In applying this license GSI does not waive the privileges and immunities  *
+ * granted to it by virtue of its status as an Intergovernmental Organization *
+ * or submit itself to any jurisdiction.                                      *
+ ******************************************************************************/
+
+// *** *************************************************************** *** //
+// ***                  R3BFrsSciMapped2Tcal                           *** //
+// ***    convert Mapped data to tcal data                             *** //
+// ***    from the fine and coarse times, calculate a raw time in ns   *** //
+// *** *************************************************************** *** //
+
+#ifndef R3BFRSSCI_MAPPED2TCAL_H
+#define R3BFRSSCI_MAPPED2TCAL_H 1
+
+#include "FairTask.h"
+#include "R3BFrsSciTcalData.h"
+#include "R3BFrsSciTcalPar.h"
+#include "TClonesArray.h"
+#include "TRandom3.h"
+
+class TRandom3;
+class R3BFrsSciTcalData;
+
+class R3BFrsSciMapped2Tcal : public FairTask
+{
+  public:
+    // --- Default constructor --- //
+    R3BFrsSciMapped2Tcal();
+
+    // --- Standard constructor --- //
+    R3BFrsSciMapped2Tcal(const char* name, Int_t iVerbose = 1);
+
+    // --- Destructor --- //
+    virtual ~R3BFrsSciMapped2Tcal();
+
+    virtual void Exec(Option_t* option);
+
+    virtual void SetParContainers();
+
+    virtual InitStatus Init();
+
+    virtual InitStatus ReInit();
+
+    virtual void Reset();
+
+    void SetOnline(Bool_t option) { fOnline = option; }
+
+  private:
+    UInt_t fNevent;
+    TClonesArray* fMapped;      // input data - FrsSci
+    R3BFrsSciTcalPar* fTcalPar; // tcal parameters container - FrsSci
+    TClonesArray* fTcal;        // output data
+    Bool_t fOnline;             // Don't store data for online
+
+    TRandom3 rand;
+
+    Double_t CalculateTimeNs(UShort_t det, UShort_t pmt, UInt_t tf, UInt_t tc);
+    R3BFrsSciTcalData* AddTcalData(UShort_t det, UShort_t pmt, Double_t tns);
+
+  public:
+    ClassDef(R3BFrsSciMapped2Tcal, 1)
+};
+
+#endif // R3BFRSSCI_MAPPED2TCAL_H
---
+++ frssci/calibration/R3BFrsSciMapped2TcalPar.h
@@ -0,0 +1,74 @@
+#ifndef __R3BFRSSCIMAPPED2TCALPAR_H__
+#define __R3BFRSSCIMAPPED2TCALPAR_H__ 1
+
+#include "FairTask.h"
+#include "TH1F.h"
+
+class TClonesArray;
+class R3BSofTcalPar;
+
+class R3BFrsSciMapped2TcalPar : public FairTask
+{
+  public:
+    /** Default constructor **/
+    R3BFrsSciMapped2TcalPar();
+
+    /** Standard constructor **/
+    R3BFrsSciMapped2TcalPar(const char* name, Int_t iVerbose = 1);
+
+    /** Destructor **/
+    virtual ~R3BFrsSciMapped2TcalPar();
+
+    /** Virtual method Init **/
+    virtual InitStatus Init();
+
+    /** Virtual method Exec **/
+    virtual void Exec(Option_t* opt);
+
+    /** Virtual method FinishEvent **/
+    virtual void FinishEvent();
+
+    /** Virtual method FinishTask **/
+    virtual void FinishTask();
+
+    /** Virtual method Reset **/
+    virtual void Reset();
+
+    /** Virtual method ReInit **/
+    virtual InitStatus ReInit();
+
+    /** Virtual method calculate the Vftx Tcal Parameters **/
+    virtual void CalculateVftxTcalParams();
+
+    void SetOutputFile(const char* outFile);
+
+    const UShort_t GetNumDets() { return fNumDets; }
+    const UShort_t GetNumPmts() { return fNumPmts; }
+    const UShort_t GetNumPars() { return fNumPars; }
+    const UInt_t GetMinStat() { return fMinStat; }
+
+    void SetNumDets(UShort_t nDets) { fNumDets = nDets; }
+    void SetNumPmts(UShort_t nPmts) { fNumPmts = nPmts; }
+    void SetNumPars(UShort_t nPars) { fNumPars = nPars; }
+    void SetMinStat(UInt_t minstat) { fMinStat = minstat; }
+
+  private:
+    Int_t fMinStatistics; // Minimum statistics to proceed to the calibration
+
+    TClonesArray* fMapped; // Array with input mapped data
+    UShort_t fNumDets;
+    UShort_t fNumPmts;
+    UShort_t fNumPars;
+    UInt_t fMinStat;
+    R3BSofSciTcalPar* fTcalPar; // Tcal Parameters
+
+    // histograms
+    TH1F** fh_TimeFineBin;
+    TH1D** fh_TimeFineNs;
+    char* fOutputFile;
+
+  public:
+    ClassDef(R3BFrsSciMapped2TcalPar, 0);
+};
+
+#endif //__R3BFRSSCIMAPPED2TCALPAR_H__
