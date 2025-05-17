c475d6f3efdda81790a40f5c9577c2c9f8b10b67
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2025-05-17 19:33:40 +0200

    feat(r3bdata/footData):Added Z-charge variable for foot hit data

--- r3bdata/footData/R3BFootHitData.h
+++ r3bdata/footData/R3BFootHitData.h
@@ -12,8 +12,8 @@
  ******************************************************************************/
 
 // ------------------------------------------------------------------
-// -----            R3BFootHitData header file                  -----
-// -----       Created 16/07/21  by J.L. Rodriguez-Sanchez      -----
+// -----              R3BFootHitData header file                -----
+// -----       Created 16/07/21 by J.L. Rodriguez-Sanchez       -----
 // ------------------------------------------------------------------
 
 #pragma once
@@ -32,11 +32,12 @@ class R3BFootHitData : public TObject
      *@param fDetId     Detector unique identifier
      *@param fNbHit     Number of hits
      *@param fMulStrip  Number of strips with signals
-     *@param fPos         Position in mm, local coordinate (detector frame)
+     *@param fPos       Position in mm, local coordinate (detector frame)
      *@param fTheta     Master: Angle theta [rad] (lab frame)
      *@param fPhi       Master: Angle Phi [rad] (lab frame)
      *@param fEnergy    Total energy deposited by the hit ([GeV] in sim)
      *@param fEta       Difference between the integer part of the cluster position and the cluster position
+     *@param fZCharge   Ion atomic charge
      **/
     explicit R3BFootHitData(uint8_t detid,
                             uint16_t nbhit,
@@ -44,7 +45,8 @@ class R3BFootHitData : public TObject
                             TVector3 master,
                             double energy,
                             uint16_t mulstrip = 0,
-                            double eta = 0);
+                            double eta = 0,
+                            double zcharge = 0);
 
     /** Copy constructor **/
     R3BFootHitData(const R3BFootHitData&);
@@ -64,6 +66,7 @@ class R3BFootHitData : public TObject
     [[nodiscard]] inline const TVector3 GetPosLab() const { return fmaster; }
     [[nodiscard]] inline const double& GetEnergy() const { return fEnergy; }
     [[nodiscard]] inline const double& GetEta() const { return fEta; }
+    [[nodiscard]] inline const double& GetZCharge() const { return fZCharge; }
 
   protected:
     uint8_t fDetId = 0;
@@ -75,7 +78,8 @@ class R3BFootHitData : public TObject
     double fEnergy = std::nan("");
     uint16_t fMulStrip = 0;
     double fEta = 0;
+    double fZCharge = std::nan("");
 
   public:
-    ClassDefOverride(R3BFootHitData, 1)
+    ClassDefOverride(R3BFootHitData, 2); // NOLINT
 };
