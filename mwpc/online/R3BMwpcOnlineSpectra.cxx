68b23a2f455589469a3dd71f99243efb081f0c4c
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2025-05-29 09:59:45 +0200

    feat(mwpc/online):Added trigger and tpat conditions for mwpc online

--- mwpc/online/R3BMwpcOnlineSpectra.cxx
+++ mwpc/online/R3BMwpcOnlineSpectra.cxx
@@ -78,6 +78,10 @@ InitStatus R3BMwpcOnlineSpectra::Init()
     FairRunOnline* run = FairRunOnline::Instance();
     run->GetHttpServer()->Register("", this);
 
+    header = dynamic_cast<R3BEventHeader*>(mgr->GetObject("EventHeader."));
+    R3BLOG_IF(warn, header == nullptr, "EventHeader. not found");
+    R3BLOG_IF(info, header, "EventHeader. found");
+
     // get access to mapped data of mwpcs
     fMapItemsMwpc = dynamic_cast<TClonesArray*>(mgr->GetObject(fNameDet + "MappedData"));
     R3BLOG_IF(fatal, fMapItemsMwpc == nullptr, fNameDet + "MappedData not found");
@@ -439,6 +443,26 @@ void R3BMwpcOnlineSpectra::Reset_Histo()
 
 void R3BMwpcOnlineSpectra::Exec(Option_t* /*option*/)
 {
+
+    // Check for requested trigger (Todo: should be done globablly / somewhere else)
+    if ((fTrigger >= 0) && (header != nullptr) && (header->GetTrigger() != fTrigger))
+        return;
+
+    if (fTpat1 >= 0 && fTpat2 >= 0 && (header))
+    {
+        // fTpat = 1-16; fTpat_bit = 0-15
+        Int_t fTpat_bit1 = fTpat1 - 1;
+        Int_t fTpat_bit2 = fTpat2 - 1;
+        Int_t tpatbin = 0;
+        for (int i = 0; i < 16; i++)
+        {
+            tpatbin = (header->GetTpat() & (1 << i));
+            if (tpatbin != 0 && (i < fTpat_bit1 || i > fTpat_bit2))
+            {
+                return;
+            }
+        }
+    }
 
     if (fMapItemsMwpc && fMapItemsMwpc->GetEntriesFast() > 0)
     {
