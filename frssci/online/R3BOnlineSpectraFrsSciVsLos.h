fde2bafe59d1faa124674bf149fac7afad372cac
Audrey Chatillon <audrey.chatillon@gmail.com>
2025-06-03 13:42:49 +0200

    add A/Q raw spectra in FrsSciVsLos online

--- frssci/online/R3BOnlineSpectraFrsSciVsLos.h
+++ frssci/online/R3BOnlineSpectraFrsSciVsLos.h
@@ -19,6 +19,7 @@
 
 class TClonesArray;
 class R3BEventHeader;
+class R3BIncomingIDPar;
 
 /**
  * This taks reads SCI data and plots online histograms
@@ -82,6 +83,8 @@ class R3BOnlineSpectraFrsSciVsLos : public FairTask
     /** Virtual method Reset **/
     virtual void Reset() {}
 
+    virtual void SetParContainers();
+
     void SetFrsSciNbDets(UShort_t ndets) { fFrsSciNbDets = ndets; }
     void SetFrsSciNbPmts(UShort_t npmts) { fFrsSciNbPmts = npmts; }
 
@@ -89,16 +92,24 @@ class R3BOnlineSpectraFrsSciVsLos : public FairTask
     R3BEventHeader* fHeader;    /**< Event header  */
     TClonesArray* fFrsSci_Tcal; /**< Array with R3BFrsSciTcalData */
 
+    void SetParameters();
+    R3BIncomingIDPar* fIncomingID_Par; // Parameter container
+    TArrayF* fTof2InvV_p0;
+    TArrayF* fTof2InvV_p1;
+    TArrayF* fBrho0_S2toCC;
+    TArrayF* fDispersionS2;
+
     Int_t fNEvents; /**< Event counter.     */
     UShort_t fFrsSciNbDets;
     UShort_t fFrsSciNbPmts;
 
     // Canvas Tof
     TCanvas* cTcal_TofRaw;
+    TCanvas* cAoQ;
 
     // Histograms TofRaw
     TH1D** fh1_Tcal1Hit_TofRaw; // [fNumFrsSci] START=FrsSci, STOP=LOS
-    TH1D** fh1_Tcal1Hit_TofCal; // [fNumFrsSci] START=FrsSci, STOP=LOS
+    TH1D** fh1_Tcal1Hit_AoQraw; // [fNumFrsSci] with Brho from FrsSci to Los
 
   public:
     ClassDef(R3BOnlineSpectraFrsSciVsLos, 1)
