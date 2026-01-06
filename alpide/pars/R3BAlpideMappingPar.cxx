32402ca75df694c72a17261c138c29bccad21ba1
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2026-01-06 14:02:09 +0100

    refactor(alpide):Update R3BAlpideMappingPar and use by R3BAlpideMapped2Cal
    Change the use of parameters to std::vector
    
    Change mapping par to manage big arrays
    
    Minor change
    
    Minor change
    
    Change to decimal numbers
    
    Change to decimal numbers
    
    Final change
    
    Forgot 1-base in the array
    
    Change to debug mode

--- alpide/pars/R3BAlpideMappingPar.cxx
+++ alpide/pars/R3BAlpideMappingPar.cxx
@@ -1,6 +1,6 @@
 /******************************************************************************
  *   Copyright (C) 2022 GSI Helmholtzzentrum für Schwerionenforschung GmbH    *
- *   Copyright (C) 2022-2023 Members of R3B Collaboration                     *
+ *   Copyright (C) 2022-2026 Members of R3B Collaboration                     *
  *                                                                            *
  *             This software is distributed under the terms of the            *
  *                 GNU General Public Licence (GPL) version 3,                *
@@ -16,46 +16,24 @@
 // -----      Created 11/02/22 by J.L. Rodriguez-Sanchez    -----
 // --------------------------------------------------------------
 
-#include "FairLogger.h"
-#include "FairParamList.h"
-
 #include "R3BAlpideMappingPar.h"
 #include "R3BLogger.h"
+
+#include <FairLogger.h>
+#include <FairParamList.h>
+
+#include <TArrayD.h>
+#include <sstream>
+#include <string>
 
 // ---- Standard Constructor ---------------------------------------------------
 R3BAlpideMappingPar::R3BAlpideMappingPar(const char* name, const char* title, const char* context)
     : FairParGenericSet(name, title, context)
-    , fNbSensors(363)
-    , fGeoVersion(2024)
-    , fAlpideCols(DAlpideCols)
-    , fAlpideRows(DAlpideRows)
 {
-    for (Int_t c = 0; c < fAlpideCols; c++)
-        for (Int_t r = 0; r < fAlpideRows; r++)
-        {
-            fIn_use[c][r].resize(fNbSensors);
-            for (Int_t s = 0; s < fNbSensors; s++)
-            {
-                fIn_use[c][r][s] = 1;
-            }
-        }
 }
 
 // ----  Destructor ------------------------------------------------------------
 R3BAlpideMappingPar::~R3BAlpideMappingPar() { clear(); }
-
-// ----  Method SetNbSensors ---------------------------------------------------
-void R3BAlpideMappingPar::SetNbSensors(Int_t n)
-{
-    fNbSensors = n;
-    for (Int_t c = 0; c < fAlpideCols; c++)
-        for (Int_t r = 0; r < fAlpideRows; r++)
-        {
-            fIn_use[c][r].resize(fNbSensors);
-            for (Int_t s = 0; s < fNbSensors; s++)
-                fIn_use[c][r][s] = 1;
-        }
-}
 
 // ----  Method clear ----------------------------------------------------------
 void R3BAlpideMappingPar::clear()
@@ -80,16 +58,15 @@ void R3BAlpideMappingPar::putParams(FairParamList* list)
     list->add("NbSensorsPar", fNbSensors);
     R3BLOG(info, "Nb of ALPIDE sensors: " << fNbSensors);
 
-    Int_t defzero = 0;
-    char name[300];
-    for (int s = 1; s <= fNbSensors; s++)
-        for (int c = 1; c <= fAlpideCols; c++)
-            for (int r = 1; r <= fAlpideRows; r++)
-                if (fIn_use[c - 1][r - 1][s - 1] == 0)
-                {
-                    sprintf(name, "Sensor%dCol%dRow%dPar", s, c, r);
-                    list->add(name, defzero);
-                }
+    list->add("NbMaskPixelsPar", fNbMaskPixels);
+    R3BLOG(info, "Nb of masking pixels: " << fNbMaskPixels);
+
+    TArrayD fMask_pixels(fNbMaskPixels);
+    for (auto pixel = 0; pixel < fNbMaskPixels; pixel++)
+    {
+        fMask_pixels[pixel] = fMask_sensors[pixel];
+    }
+    list->add("MaskPixelPar", fMask_pixels);
 }
 
 // ----  Method getParams ------------------------------------------------------
@@ -122,49 +99,36 @@ Bool_t R3BAlpideMappingPar::getParams(FairParamList* list)
         R3BLOG(info, "Nb of ALPIDE sensors: " << fNbSensors);
     }
 
-    char name[300];
-    for (int s = 1; s <= fNbSensors; s++)
-        for (Int_t c = 1; c <= fAlpideCols; c++)
-            for (Int_t r = 1; r <= fAlpideRows; r++)
-            {
-                sprintf(name, "Sensor%dCol%dRow%dPar", s, c, r);
-                Int_t value = 1;
-                auto check = fillParams(name, &value, list);
-                if (check)
-                {
-                    fIn_use[c - 1][r - 1][s - 1] = 0;
-                }
-            }
-    return kTRUE;
-}
-
-Bool_t R3BAlpideMappingPar::fillParams(const Text_t* name, Int_t* values, FairParamList* paramList, const Int_t nValues)
-{
-    // Copies the data from the list object into the parameter array of type Int_t.
-    // The function returns an error, if the array size of the list object is not equal
-    // to nValues.
-    if (values == 0)
+    if (!list->fill("NbMaskPixelsPar", &fNbMaskPixels))
     {
+        R3BLOG(error, "Could not initialize NbMaskPixelsPar");
         return kFALSE;
     }
-    auto o = paramList->find(name);
-    if (o && strcmp(o->getParamType(), "Int_t") == 0)
+    else
     {
-        Int_t l = o->getLength();
-        Int_t n = o->getNumParams();
+        R3BLOG(info, "Nb of masking pixels: " << fNbMaskPixels);
+    }
 
-        if (n == nValues)
+    if (fNbMaskPixels > 0)
+    {
+        TArrayD fMask_pixels(fNbMaskPixels);
+        if (!(list->fill("MaskPixelPar", &fMask_pixels)))
         {
-            memcpy(values, o->getParamValue(), l);
-            return kTRUE;
-        }
-        else
-        {
-            R3BLOG(error, "Different array sizes for parameter " << name);
+            R3BLOG(error, "Could not initialize MaskPixelPar");
             return kFALSE;
         }
+
+        for (auto pixel = 0; pixel < fNbMaskPixels; pixel++)
+        {
+            fMask_sensors.push_back(fMask_pixels[pixel]);
+
+            auto sen = this->GetSensorId(fMask_sensors[pixel]);
+            auto col = this->GetCol(fMask_sensors[pixel]);
+            auto row = this->GetRow(fMask_sensors[pixel]);
+            LOG(info) << "Skipping Sensor: " << sen << " Col: " << col << " Row: " << row;
+        }
     }
-    return kFALSE;
+    return kTRUE;
 }
 
 // ----  Method print ----------------------------------------------------------
@@ -174,15 +138,15 @@ void R3BAlpideMappingPar::print() { printParams(); }
 void R3BAlpideMappingPar::printParams()
 {
     R3BLOG(info, "Nb Sensors: " << fNbSensors);
-    for (Int_t s = 0; s < fNbSensors; s++)
+    R3BLOG(info, "Geo Version: " << fGeoVersion);
+    R3BLOG(info, "Nb masked pixels: " << fNbMaskPixels);
+
+    for (auto pixel = 0; pixel < fNbMaskPixels; pixel++)
     {
-        R3BLOG(info, "ALPIDE sensor: " << s + 1);
-        for (Int_t c = 0; c < fAlpideCols; c++)
-            for (Int_t r = 0; r < fAlpideRows; r++)
-            {
-                if (fIn_use[c][r][s] == 0)
-                    LOG(info) << "Pixel column: " << c + 1 << ", row: " << r + 1 << " is skipped";
-            }
+        auto sen = this->GetSensorId(fMask_sensors[pixel]);
+        auto col = this->GetCol(fMask_sensors[pixel]);
+        auto row = this->GetRow(fMask_sensors[pixel]);
+        LOG(info) << "Skipping Sensor: " << sen << " Col: " << col << " Row: " << row;
     }
 }
 
