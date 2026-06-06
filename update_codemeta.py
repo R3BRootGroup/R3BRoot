d62d384aa1aef2b8f738ea7c3e16a6b2eb9e596a
jose-luis-rs <j.l.rodriguez.sanchez@udc.es>
2026-06-06 10:51:08 +0200

    Update Copyright date

--- update_codemeta.py
+++ update_codemeta.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-# Copyright (C) 2019-2025 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
+# Copyright (C) 2019-2026 GSI Helmholtzzentrum fuer Schwerionenforschung GmbH
 #
 # SPDX-License-Identifier: LGPL-3.0-or-later
 
